ac655f652f1f3038049ee6dd55df488c53c4f830
Benedikt Ritter <benedikt.ritter@codecentric.de>
2017-11-18 14:57:08 +0100

    Add equals, hashCode and toString to AbstractValue

--- plc4j/api/src/main/java/org/apache/plc4x/java/types/AbstractValue.java
+++ plc4j/api/src/main/java/org/apache/plc4x/java/types/AbstractValue.java
@@ -18,6 +18,8 @@ under the License.
 */
 package org.apache.plc4x.java.types;
 
+import java.util.Objects;
+
 public abstract class AbstractValue<T> implements Value<T> {
 
     private T value;
@@ -31,4 +33,23 @@ public abstract class AbstractValue<T> implements Value<T> {
         this.value = value;
     }
 
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final AbstractValue<?> that = (AbstractValue<?>) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+            "value=" + value +
+            '}';
+    }
 }
