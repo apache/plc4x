329f99e6ca73b53979d36321a2938847ef124d10
Dale LaBossiere <dlaboss@us.ibm.com>
2017-11-29 12:49:55 -0500

    migrate all the nuts and bolts into the PlcConnectionAdapter

--- integrations/apache-edgent/src/main/java/org/apache/plc4x/edgent/wip/PlcConnectionAdapter.java
+++ integrations/apache-edgent/src/main/java/org/apache/plc4x/edgent/wip/PlcConnectionAdapter.java
@@ -1,41 +1,84 @@
 package org.apache.plc4x.edgent.wip;
 
+import java.util.Calendar;
+
+import org.apache.edgent.function.Consumer;
+import org.apache.edgent.function.Function;
+import org.apache.edgent.function.Supplier;
 import org.apache.plc4x.java.PlcDriverManager;
 import org.apache.plc4x.java.api.connection.PlcConnection;
+import org.apache.plc4x.java.api.connection.PlcReader;
+import org.apache.plc4x.java.api.connection.PlcWriter;
 import org.apache.plc4x.java.api.exceptions.PlcException;
+import org.apache.plc4x.java.api.messages.BooleanPlcReadRequest;
+import org.apache.plc4x.java.api.messages.BooleanPlcWriteRequest;
+import org.apache.plc4x.java.api.messages.BytePlcReadRequest;
+import org.apache.plc4x.java.api.messages.BytePlcWriteRequest;
+import org.apache.plc4x.java.api.messages.CalendarPlcReadRequest;
+import org.apache.plc4x.java.api.messages.CalendarPlcWriteRequest;
+import org.apache.plc4x.java.api.messages.FloatPlcReadRequest;
+import org.apache.plc4x.java.api.messages.FloatPlcWriteRequest;
+import org.apache.plc4x.java.api.messages.IntegerPlcReadRequest;
+import org.apache.plc4x.java.api.messages.IntegerPlcWriteRequest;
+import org.apache.plc4x.java.api.messages.PlcReadRequest;
+import org.apache.plc4x.java.api.messages.PlcWriteRequest;
+import org.apache.plc4x.java.api.messages.StringPlcReadRequest;
+import org.apache.plc4x.java.api.messages.StringPlcWriteRequest;
+import org.apache.plc4x.java.api.model.Address;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonObject;
 
 /**
- * PlcConnectionAdapter encapsulates a PlcConnection.
+ * PlcConnectionAdapter encapsulates a plc4x {@link PlcConnection}.
  * <p>
  * The idea here is to use PlcConnectionAdapter to enable our Edgent Supplier/Consumer
- * instances to be isolated from some of the details of / variability of 
+ * instances to be isolated from the details of / variability of 
  * PlcConnection mgmt and such.
  * <p>
- * The current usage presumption (in Supplier and Consumer factories) is that
- * a single PlcConnector instance can be used in multiple Supplier/Consumer instances.
- * IS THAT VALID?
+ * A PlcConnectionAdapter is subject to the constraints of the underlying
+ * PlcConnection for the device.
+ * <p>
+ * A single PlcConnectionAdaptor represents a single underlying PlcConnection/connection
+ * to a plc device.
+ * <p>
+ * Multiple PlcConnectionAdaptor instances, hence multiple PlcConnection instances,
+ * can be created for a single plc device subject to the underlying device's protocol's
+ * support for multiple connections from a single client.
+ * <p 
+ * A single PlcConnectionAdapter can be used by multiple threads concurrently
+ * (e.g., used by multiple PlcFunctions Consumers for {@code Topology.poll()} and/or 
+ * multiple Suppliers for {@code TStream.sink()}). 
+ * 
+ * @see PlcFunctions
  */
 public class PlcConnectionAdapter implements AutoCloseable{
+
+  private final static Logger logger = LoggerFactory.getLogger(PlcConnectionAdapter.class);
   
   private String plcConnectionUrl;
   private PlcConnection plcConnection;
   
-  // plcConnection must be connected
+  /*
+   * NOTES:
+   * - if we get to the point of the application needing some feedback (possibly control)
+   *   of read or write errors, my thinking is to enhance the PlcConnectionAdapter
+   *   to enable the app to register an error callback handler or such.
+   */
+  
   public PlcConnectionAdapter(PlcConnection plcConnection) {
     this.plcConnection = plcConnection;
   }
   
-  // getConnection() gets a PlcConnection and connects.
   public PlcConnectionAdapter(String plcConnectionUrl) {
     this.plcConnectionUrl = plcConnectionUrl;
   }
   
-  // returns connected PlcConnection
-  public PlcConnection getConnection() throws PlcException {
+  PlcConnection getConnection() throws PlcException {
     synchronized(this) {
       if (plcConnection == null) {
         plcConnection = new PlcDriverManager().getConnection(plcConnectionUrl);
-        plcConnection.connect();
       }
       return plcConnection;
     }
@@ -48,6 +91,129 @@ public class PlcConnectionAdapter implements AutoCloseable{
       if (plcConnection != null)
         plcConnection.close();
     }
+  }
+
+  <T> Supplier<T> newSupplier(String addressStr, Class<T> datatype) {
+    PlcConnectionAdapter.checkDatatype(datatype);
+    return new Supplier<T>() {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      public T get() {
+        PlcConnection connection = null;
+        Address address = null;
+        try {
+          connection = getConnection();
+          address = connection.parseAddress(addressStr);
+          PlcReader reader = connection.getReader().get();
+          PlcReadRequest<T> readRequest = PlcConnectionAdapter.newPlcReadRequest(datatype, address);
+          T value = reader.read(readRequest).get().getValue();
+          return value;
+        }
+        catch (Exception e) {
+          logger.error("reading from plc device {} {} failed", connection, address, e);
+          return null;
+        }
+      }
+      
+    };
+  }
+
+  <T> Consumer<T> newConsumer(String addressStr, Class<T> datatype) {
+    PlcConnectionAdapter.checkDatatype(datatype);
+    return new Consumer<T>() {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      public void accept(T arg0) {
+        PlcConnection connection = null;
+        Address address = null;
+        try {
+          connection = getConnection();
+          address = connection.parseAddress(addressStr);
+          PlcWriter writer = connection.getWriter().get();
+          PlcWriteRequest<T> writeReq = PlcConnectionAdapter.newPlcWriteRequest(address, arg0);
+          writer.write(writeReq).get();
+        }
+        catch (Exception e) {
+          logger.error("writing to plc device {} {} failed", connection, address, e);
+        }
+      }
+      
+    };
+  }
+  
+  <T> Consumer<JsonObject> newConsumer(Class<T> datatype, Function<JsonObject,String> addressFn, Function<JsonObject,T> valueFn) {
+    return new Consumer<JsonObject>() {
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      public void accept(JsonObject jo) {
+        PlcConnection connection = null;
+        Address address = null;
+        try {
+          connection = getConnection();
+          String addressStr = addressFn.apply(jo);
+          address = connection.parseAddress(addressStr);
+          T value = valueFn.apply(jo);
+          PlcWriter writer = connection.getWriter().get();
+          PlcWriteRequest<T> writeReq = newPlcWriteRequest(address, value);
+          writer.write(writeReq).get();
+        }
+        catch (Exception e) {
+          logger.error("writing to plc device {} {} failed", connection, address, e);
+        }
+      }
+      
+    };
+  }
+
+  private static void checkDatatype(Class<?> cls) {
+    if (cls == Boolean.class
+        || cls == Byte.class
+        || cls == Integer.class
+        || cls == Float.class
+        || cls == String.class
+        || cls == Calendar.class)
+      return;
+    throw new IllegalArgumentException("Not a legal plc data type: "+cls.getSimpleName());
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <T> PlcWriteRequest<T> newPlcWriteRequest(Address address, T value) {
+    Class<?> cls = value.getClass();
+    if (cls == Boolean.class)
+      return (PlcWriteRequest<T>) new BooleanPlcWriteRequest(address, (Boolean)value);
+    else if (cls == Byte.class)
+      return (PlcWriteRequest<T>) new BytePlcWriteRequest(address, (Byte)value);
+    else if (cls == Integer.class)
+      return (PlcWriteRequest<T>) new IntegerPlcWriteRequest(address, (Integer)value);
+    else if (cls == Float.class)
+      return (PlcWriteRequest<T>) new FloatPlcWriteRequest(address, (Float)value);
+    else if (cls == String.class)
+      return (PlcWriteRequest<T>) new StringPlcWriteRequest(address, (String)value);
+    else if (cls == Calendar.class)
+      return (PlcWriteRequest<T>) new CalendarPlcWriteRequest(address, (Calendar)value);
+    else
+      throw new IllegalArgumentException("Not a legal plc data type: "+cls.getSimpleName());
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <T> PlcReadRequest<T> newPlcReadRequest(Class<T> datatype, Address address) {
+    if (datatype == Boolean.class)
+      return (PlcReadRequest<T>) new BooleanPlcReadRequest(address);
+    else if (datatype == Byte.class)
+      return (PlcReadRequest<T>) new BytePlcReadRequest(address);
+    else if (datatype == Integer.class)
+      return (PlcReadRequest<T>) new IntegerPlcReadRequest(address);
+    else if (datatype == Float.class)
+      return (PlcReadRequest<T>) new FloatPlcReadRequest(address);
+    else if (datatype == String.class)
+      return (PlcReadRequest<T>) new StringPlcReadRequest(address);
+    else if (datatype == Calendar.class)
+      return (PlcReadRequest<T>) new CalendarPlcReadRequest(address);
+    else
+      throw new IllegalArgumentException("Not a legal plc data type: "+datatype.getSimpleName());
   }
 
 }
--- integrations/apache-edgent/src/main/java/org/apache/plc4x/edgent/wip/PlcFunctions.java
+++ integrations/apache-edgent/src/main/java/org/apache/plc4x/edgent/wip/PlcFunctions.java
@@ -5,37 +5,18 @@ import java.util.Calendar;
 import org.apache.edgent.function.Consumer;
 import org.apache.edgent.function.Function;
 import org.apache.edgent.function.Supplier;
-import org.apache.plc4x.java.api.connection.PlcConnection;
-import org.apache.plc4x.java.api.connection.PlcReader;
-import org.apache.plc4x.java.api.connection.PlcWriter;
-import org.apache.plc4x.java.api.messages.BooleanPlcReadRequest;
-import org.apache.plc4x.java.api.messages.BooleanPlcWriteRequest;
-import org.apache.plc4x.java.api.messages.BytePlcReadRequest;
-import org.apache.plc4x.java.api.messages.BytePlcWriteRequest;
-import org.apache.plc4x.java.api.messages.CalendarPlcReadRequest;
-import org.apache.plc4x.java.api.messages.CalendarPlcWriteRequest;
-import org.apache.plc4x.java.api.messages.FloatPlcReadRequest;
-import org.apache.plc4x.java.api.messages.FloatPlcWriteRequest;
-import org.apache.plc4x.java.api.messages.IntegerPlcReadRequest;
-import org.apache.plc4x.java.api.messages.IntegerPlcWriteRequest;
-import org.apache.plc4x.java.api.messages.PlcReadRequest;
-import org.apache.plc4x.java.api.messages.PlcWriteRequest;
-import org.apache.plc4x.java.api.messages.StringPlcReadRequest;
-import org.apache.plc4x.java.api.messages.StringPlcWriteRequest;
-import org.apache.plc4x.java.api.model.Address;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonObject;
 
 /**
- * WIP - A plc4x Apache Edgent Supplier and Consumer connector factory.
+ * WIP - A plc4x Apache Edgent {@link Supplier} and {@link Consumer} connector factory.
  * <p>
  * TODO:
  * Are there cases where a single logical poll would want to read from 
  * multiple addrs/data (of different types) from a device and bundle the values
  * into a single TStream tuple (e.g., a JsonObject)?  How would we support that?
  * Is there a similar need for writing to multiple addrs/values on a device?
+ * Ah... NOTE: plc4c "batch" requests are coming and will help to address this.
  * 
  * <p>
  * Sample use to read plc device data into an Edgent TStream:
@@ -70,8 +51,6 @@ import com.google.gson.JsonObject;
  */
 public class PlcFunctions {
 
-  private final static Logger logger = LoggerFactory.getLogger(PlcFunctions.class);
-
   /**
    * Create a new Edgent {@link Supplier} to read data from the 
    * plc device.
@@ -93,29 +72,7 @@ public class PlcFunctions {
    * @see org.apache.edgent.topology.Topology#poll(Supplier, long, java.util.concurrent.TimeUnit)
    */
   private static <T> Supplier<T> supplier(PlcConnectionAdapter adapter, String addressStr, Class<T> datatype) {
-    checkDatatype(datatype);
-    return new Supplier<T>() {
-      private static final long serialVersionUID = 1L;
-
-      @Override
-      public T get() {
-        PlcConnection connection = null;
-        Address address = null;
-        try {
-          connection = adapter.getConnection();
-          address = connection.parseAddress(addressStr);
-          PlcReader reader = connection.getReader().get();
-          PlcReadRequest<T> readRequest = newPlcReadRequest(datatype, address);
-          T value = reader.read(readRequest).get().getValue();
-          return value;
-        }
-        catch (Exception e) {
-          logger.error("reading from plc device {} {} failed", connection, address, e);
-          return null;
-        }
-      }
-      
-    };
+    return adapter.newSupplier(addressStr, datatype);
   }
 
   public static Supplier<Boolean> booleanSupplier(PlcConnectionAdapter adapter, String addressStr) {
@@ -137,54 +94,6 @@ public class PlcFunctions {
     return supplier(adapter, addressStr, Calendar.class);
   }
 
-  private static void checkDatatype(Class<?> cls) {
-    if (cls == Boolean.class
-        || cls == Byte.class
-        || cls == Integer.class
-        || cls == Float.class
-        || cls == String.class
-        || cls == Calendar.class)
-      return;
-    throw new IllegalArgumentException("Not a legal plc data type: "+cls.getSimpleName());
-  }
-  
-  @SuppressWarnings("unchecked")
-  private static <T> PlcReadRequest<T> newPlcReadRequest(Class<T> datatype, Address address) {
-    if (datatype == Boolean.class)
-      return (PlcReadRequest<T>) new BooleanPlcReadRequest(address);
-    else if (datatype == Byte.class)
-      return (PlcReadRequest<T>) new BytePlcReadRequest(address);
-    else if (datatype == Integer.class)
-      return (PlcReadRequest<T>) new IntegerPlcReadRequest(address);
-    else if (datatype == Float.class)
-      return (PlcReadRequest<T>) new FloatPlcReadRequest(address);
-    else if (datatype == String.class)
-      return (PlcReadRequest<T>) new StringPlcReadRequest(address);
-    else if (datatype == Calendar.class)
-      return (PlcReadRequest<T>) new CalendarPlcReadRequest(address);
-    else
-      throw new IllegalArgumentException("Not a legal plc data type: "+datatype.getSimpleName());
-  }
-  
-  @SuppressWarnings("unchecked")
-  private static <T> PlcWriteRequest<T> newPlcWriteRequest(Address address, T value) {
-    Class<?> cls = value.getClass();
-    if (cls == Boolean.class)
-      return (PlcWriteRequest<T>) new BooleanPlcWriteRequest(address, (Boolean)value);
-    else if (cls == Byte.class)
-      return (PlcWriteRequest<T>) new BytePlcWriteRequest(address, (Byte)value);
-    else if (cls == Integer.class)
-      return (PlcWriteRequest<T>) new IntegerPlcWriteRequest(address, (Integer)value);
-    else if (cls == Float.class)
-      return (PlcWriteRequest<T>) new FloatPlcWriteRequest(address, (Float)value);
-    else if (cls == String.class)
-      return (PlcWriteRequest<T>) new StringPlcWriteRequest(address, (String)value);
-    else if (cls == Calendar.class)
-      return (PlcWriteRequest<T>) new CalendarPlcWriteRequest(address, (Calendar)value);
-    else
-      throw new IllegalArgumentException("Not a legal plc data type: "+cls.getSimpleName());
-  }
-  
   /**
    * Create a new Edgent {@link Consumer} to write data to the 
    * plc device.
@@ -211,27 +120,7 @@ public class PlcFunctions {
    * @see org.apache.edgent.topology.TStream#sink(Consumer)
    */
   private static <T> Consumer<T> consumer(PlcConnectionAdapter adapter, String addressStr, Class<T> datatype) {
-    checkDatatype(datatype);
-    return new Consumer<T>() {
-      private static final long serialVersionUID = 1L;
-
-      @Override
-      public void accept(T arg0) {
-        PlcConnection connection = null;
-        Address address = null;
-        try {
-          connection = adapter.getConnection();
-          address = connection.parseAddress(addressStr);
-          PlcWriter writer = connection.getWriter().get();
-          PlcWriteRequest<T> writeReq = newPlcWriteRequest(address, arg0);
-          writer.write(writeReq).get();
-        }
-        catch (Exception e) {
-          logger.error("writing to plc device {} {} failed", connection, address, e);
-        }
-      }
-      
-    };
+    return adapter.newConsumer(addressStr, datatype);
   }
 
   public static Consumer<Boolean> booleanConsumer(PlcConnectionAdapter adapter, String addressStr) {
@@ -277,28 +166,7 @@ public class PlcFunctions {
    * @see org.apache.edgent.topology.TStream#sink(Consumer)
    */
   private static <T> Consumer<JsonObject> consumer(PlcConnectionAdapter adapter, Class<T> datatype, Function<JsonObject,String> addressFn, Function<JsonObject,T> valueFn) {
-    return new Consumer<JsonObject>() {
-      private static final long serialVersionUID = 1L;
-
-      @Override
-      public void accept(JsonObject jo) {
-        PlcConnection connection = null;
-        Address address = null;
-        try {
-          connection = adapter.getConnection();
-          String addressStr = addressFn.apply(jo);
-          address = connection.parseAddress(addressStr);
-          T value = valueFn.apply(jo);
-          PlcWriter writer = connection.getWriter().get();
-          PlcWriteRequest<T> writeReq = newPlcWriteRequest(address, value);
-          writer.write(writeReq).get();
-        }
-        catch (Exception e) {
-          logger.error("writing to plc device {} {} failed", connection, address, e);
-        }
-      }
-      
-    };
+    return adapter.newConsumer(datatype, addressFn, valueFn);
   }
 
   public static Consumer<JsonObject> booleanConsumer(PlcConnectionAdapter adapter, Function<JsonObject,String> addressFn, Function<JsonObject,Boolean> valueFn) {
