9f8446a8d435c012449df3a9eca58f3eecd8dcd9
Sebastian Rühl <sebastian.ruehl@codecentric.de>
2017-11-30 15:19:08 +0100

    simplify code

--- integrations/apache-camel/src/main/java/org/apache/plc4x/camel/PLC4XEndpoint.java
+++ integrations/apache-camel/src/main/java/org/apache/plc4x/camel/PLC4XEndpoint.java
@@ -1,8 +1,23 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
 package org.apache.plc4x.camel;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.apache.camel.Component;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
@@ -11,11 +26,9 @@ import org.apache.camel.impl.DefaultEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriPath;
+import org.apache.plc4x.java.PlcDriverManager;
 
 @UriEndpoint(scheme = "plc4x", title = "PLC4X", syntax = "plc4x:driver:address", label = "plc4x")
-@Setter
-@Getter
-@NoArgsConstructor
 public class PLC4XEndpoint extends DefaultEndpoint {
 
     /**
@@ -25,8 +38,11 @@ public class PLC4XEndpoint extends DefaultEndpoint {
     @Metadata(required = "true")
     String address;
 
+    final PlcDriverManager plcDriverManager;
+
     public PLC4XEndpoint(String endpointUri, Component component) {
         super(endpointUri, component);
+        plcDriverManager = new PlcDriverManager();
     }
 
     @Override
--- integrations/apache-camel/src/main/java/org/apache/plc4x/camel/PLC4XProducer.java
+++ integrations/apache-camel/src/main/java/org/apache/plc4x/camel/PLC4XProducer.java
@@ -1,3 +1,21 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
 package org.apache.plc4x.camel;
 
 import org.apache.camel.AsyncCallback;
@@ -6,21 +24,17 @@ import org.apache.camel.Message;
 import org.apache.camel.ShutdownRunningTask;
 import org.apache.camel.impl.DefaultAsyncProducer;
 import org.apache.camel.spi.ShutdownAware;
-import org.apache.plc4x.camel.util.StreamUtils;
-import org.apache.plc4x.java.PlcDriverManager;
 import org.apache.plc4x.java.api.connection.PlcConnection;
+import org.apache.plc4x.java.api.connection.PlcWriter;
 import org.apache.plc4x.java.api.exceptions.PlcException;
 import org.apache.plc4x.java.api.messages.GenericPlcWriteRequest;
-import org.apache.plc4x.java.api.messages.GenericPlcWriteResponse;
 import org.apache.plc4x.java.api.model.Address;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
 public class PLC4XProducer extends DefaultAsyncProducer implements ShutdownAware {
     private static final Logger LOG = LoggerFactory.getLogger(PLC4XProducer.class);
+
     private PLC4XEndpoint endpoint;
     private PlcConnection plcConnection;
 
@@ -28,8 +42,8 @@ public class PLC4XProducer extends DefaultAsyncProducer implements ShutdownAware
         super(endpoint);
         this.endpoint = endpoint;
         try {
-            plcConnection = new PlcDriverManager().getConnection(endpoint.getEndpointUri().replaceFirst("plc4x:/?/?", ""));
-            plcConnection.connect();
+            String plc4xURI = endpoint.getEndpointUri().replaceFirst("plc4x:/?/?", "");
+            plcConnection = endpoint.plcDriverManager.getConnection(plc4xURI);
         } catch (PlcException e) {
             throw new RuntimeException(e);
         }
@@ -42,24 +56,15 @@ public class PLC4XProducer extends DefaultAsyncProducer implements ShutdownAware
         Class<?> datatype = in.getHeader(Constants.DATATYPE_HEADER, Class.class);
         Object value = in.getBody(Object.class);
         GenericPlcWriteRequest plcSimpleWriteRequest = new GenericPlcWriteRequest(datatype, address, value);
-        StreamUtils.streamOf(plcConnection.getWriter())
-            .map(plcWriter -> plcWriter.write(plcSimpleWriteRequest))
-            .forEach(plcWriteResponseCompletableFuture -> {
-                try {
-                    // FIXME: If I omit the cast to CompletableFuture the java compiler complains
-                    GenericPlcWriteResponse response = (GenericPlcWriteResponse)
-                        ((CompletableFuture) plcWriteResponseCompletableFuture).get();
-                    if (exchange.getPattern().isOutCapable()) {
-                        Message out = exchange.getOut();
-                        out.copyFrom(exchange.getIn());
-                        out.setBody(response);
-                    } else {
-                        in.setBody(response);
-                    }
-                } catch (InterruptedException | ExecutionException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+        PlcWriter plcWriter = plcConnection.getWriter().orElseThrow(() -> new IllegalArgumentException("Writer for driver not found"));
+        Object response = plcWriter.write(plcSimpleWriteRequest).get();
+        if (exchange.getPattern().isOutCapable()) {
+            Message out = exchange.getOut();
+            out.copyFrom(exchange.getIn());
+            out.setBody(response);
+        } else {
+            in.setBody(response);
+        }
     }
 
     public boolean process(Exchange exchange, AsyncCallback callback) {
