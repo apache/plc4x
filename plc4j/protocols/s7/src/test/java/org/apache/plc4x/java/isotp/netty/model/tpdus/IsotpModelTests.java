3d1d1956f855f20f54c1ceb7e5fda9c1af765a43
Justin Mclean <jmclean@apache.org>
2017-12-28 10:31:08 +1100

    added parameter test

--- plc4j/protocols/s7/src/test/java/org/apache/plc4x/java/isotp/netty/model/tpdus/IsotpModelTests.java
+++ plc4j/protocols/s7/src/test/java/org/apache/plc4x/java/isotp/netty/model/tpdus/IsotpModelTests.java
@@ -21,14 +21,15 @@ package org.apache.plc4x.java.isotp.netty.model.tpdus;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import org.apache.plc4x.java.isotp.netty.model.params.CallingTsapParameter;
+import org.apache.plc4x.java.isotp.netty.model.params.ChecksumParameter;
 import org.apache.plc4x.java.isotp.netty.model.params.Parameter;
-import org.apache.plc4x.java.isotp.netty.model.types.DisconnectReason;
-import org.apache.plc4x.java.isotp.netty.model.types.ProtocolClass;
-import org.apache.plc4x.java.isotp.netty.model.types.RejectCause;
-import org.apache.plc4x.java.isotp.netty.model.types.TpduCode;
+import org.apache.plc4x.java.isotp.netty.model.params.TpduSizeParameter;
+import org.apache.plc4x.java.isotp.netty.model.types.*;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -53,6 +54,27 @@ class IsotpModelTests {
         assertTrue(tpdu.getRejectCause() == RejectCause.REASON_NOT_SPECIFIED);
         assertTrue(tpdu.getParameters().isEmpty(), "Unexpected parameters");
         assertTrue(tpdu.getUserData().readByte() == (byte) 0x7F, "Unexpected user data");
+    }
+
+    @Test
+    @Tag("Fast")
+    void errorTpduParameter() {
+        short destinationReference = 0x1;
+        RejectCause rejectCause = RejectCause.REASON_NOT_SPECIFIED;
+        ArrayList<Parameter> parameters = new ArrayList<>();
+        ByteBuf userData = Unpooled.buffer();
+
+        userData.writeByte(0x7F);
+
+        ErrorTpdu tpdu = new ErrorTpdu(destinationReference, rejectCause, parameters, userData);
+
+        parameters.add(new TpduSizeParameter(TpduSize.SIZE_1024));
+        parameters.add(new ChecksumParameter((byte)0xFF));
+
+        assertTrue(tpdu.getParameters().size() == 2, "Unexpected number of parameters");
+        assertTrue(tpdu.getParameters().containsAll(parameters), "Unexpected parameter");
+        assertTrue(tpdu.getParameter(ChecksumParameter.class) != null, "Checksum parameter should exist");
+        assertTrue(tpdu.getParameter(CallingTsapParameter.class) == null, "CallingTsapParameter parameter should not exist");
     }
 
     @Test
