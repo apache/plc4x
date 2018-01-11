b10c70de60000ccbe44e61e386ba96b3550ada10
Sebastian Rühl <sruehl@apache.org>
2018-01-11 17:53:28 +0100

    rename generic from REQUEST_TYPE to REQUEST_ITEM

--- plc4j/api/src/main/java/org/apache/plc4x/java/api/messages/items/ResponseItem.java
+++ plc4j/api/src/main/java/org/apache/plc4x/java/api/messages/items/ResponseItem.java
@@ -20,18 +20,18 @@ package org.apache.plc4x.java.api.messages.items;
 
 import org.apache.plc4x.java.api.types.ResponseCode;
 
-public abstract class ResponseItem<REQUEST_TYPE> {
+public abstract class ResponseItem<REQUEST_ITEM> {
 
-    private final REQUEST_TYPE requestItem;
+    private final REQUEST_ITEM requestItem;
 
     private final ResponseCode responseCode;
 
-    public ResponseItem(REQUEST_TYPE requestItem, ResponseCode responseCode) {
+    public ResponseItem(REQUEST_ITEM requestItem, ResponseCode responseCode) {
         this.requestItem = requestItem;
         this.responseCode = responseCode;
     }
 
-    public REQUEST_TYPE getRequestItem() {
+    public REQUEST_ITEM getRequestItem() {
         return requestItem;
     }
 
