6f76878e7233867b5ebb696dd1e474d70046e66c
Christofer Dutz <christofer.dutz@c-ware.de>
2018-02-12 15:01:33 +0100

    PLC4X-3 - Implement a testing framework for protocols
    - Added code for dumping pcap Files and moved the binary request data into binary test resource files

--- plc4j/protocols/s7/src/test/java/org/apache/plc4x/java/s7/connection/S7PlcTestConnection.java
+++ plc4j/protocols/s7/src/test/java/org/apache/plc4x/java/s7/connection/S7PlcTestConnection.java
@@ -21,12 +21,14 @@ package org.apache.plc4x.java.s7.connection;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.plc4x.java.base.connection.TestChannelFactory;
-import org.apache.plc4x.java.isotp.netty.model.types.*;
 import org.apache.plc4x.java.netty.events.S7ConnectionEvent;
-import org.apache.plc4x.java.s7.netty.S7Protocol;
-import org.apache.plc4x.java.s7.netty.model.types.MessageType;
-import org.apache.plc4x.java.s7.netty.model.types.ParameterType;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class S7PlcTestConnection extends S7PlcConnection {
 
@@ -34,43 +36,8 @@ public class S7PlcTestConnection extends S7PlcConnection {
         super(new TestChannelFactory(), rack, slot, params);
     }
 
-    @Override
-    protected void sendChannelCreatedEvent() {
-        EmbeddedChannel channel = (EmbeddedChannel) getChannel();
-
-        // Send an event to the pipeline telling the Protocol filters what's going on.
-        channel.pipeline().fireUserEventTriggered(new S7ConnectionEvent());
-
-        ByteBuf writtenData = channel.readOutbound();
-        byte[] connectionRequest = new byte[writtenData.readableBytes()];
-        writtenData.readBytes(connectionRequest);
-        // TODO: Check the content of the Iso TP connection request.
-
-        // Send an Iso TP connection response back to the pipeline.
-        byte[] connectionConfirm = toByteArray(
-            new int[] {
-                // ISO on TCP packet
-                0x03, 0x00, 0x00, 0x16,
-                // ISO TP packet
-                0x11,
-                TpduCode.CONNECTION_CONFIRM.getCode(),
-                0x00, 0x01, 0x00, 0x02,
-                ProtocolClass.CLASS_0.getCode(),
-
-                ParameterCode.CALLED_TSAP.getCode(), 0x02, DeviceGroup.PG_OR_PC.getCode(), 0x01,
-                ParameterCode.CALLING_TSAP.getCode(), 0x02, DeviceGroup.OTHERS.getCode(), 0x12,
-                ParameterCode.TPDU_SIZE.getCode(), 0x01, TpduSize.SIZE_512.getCode()
-            });
-        channel.writeInbound(Unpooled.wrappedBuffer(connectionConfirm));
-
-        // Read a S7 Setup Communication request.
-        writtenData = channel.readOutbound();
-        byte[] setupCommunicationRequest = new byte[writtenData.readableBytes()];
-        writtenData.readBytes(setupCommunicationRequest);
-        // TODO: Check the content of the S7 Setup Communication connection request.
-
-        // Send an S7 Setup Communication response back to the pipeline.
-        byte[] setupCommunicationResponse = toByteArray(
+    /*
+            byte[] setupCommunicationResponse = toByteArray(
             new int[] {
                 // ISO on TCP packet
                 0x03, 0x00,
@@ -91,6 +58,35 @@ public class S7PlcTestConnection extends S7PlcConnection {
                 // Parameters:
                 ParameterType.SETUP_COMMUNICATION.getCode(), 0x00, 0x00, 0x08, 0x00, 0x08, 0x01, 0x00
             });
+        dumpArrayToPcapFile("s7-setup-communication-response.pcap", setupCommunicationResponse);
+     */
+
+    @Override
+    protected void sendChannelCreatedEvent() {
+        EmbeddedChannel channel = (EmbeddedChannel) getChannel();
+
+        // Send an event to the pipeline telling the Protocol filters what's going on.
+        channel.pipeline().fireUserEventTriggered(new S7ConnectionEvent());
+
+        ByteBuf writtenData = channel.readOutbound();
+        byte[] connectionRequest = new byte[writtenData.readableBytes()];
+        writtenData.readBytes(connectionRequest);
+        // TODO: Check the content of the Iso TP connection request.
+
+        // Send an Iso TP connection response back to the pipeline.
+        byte[] connectionConfirm = readPcapFile(
+            "org/apache/plc4x/java/s7/connection/iso-tp-connect-response.pcap");
+        channel.writeInbound(Unpooled.wrappedBuffer(connectionConfirm));
+
+        // Read a S7 Setup Communication request.
+        writtenData = channel.readOutbound();
+        byte[] setupCommunicationRequest = new byte[writtenData.readableBytes()];
+        writtenData.readBytes(setupCommunicationRequest);
+        // TODO: Check the content of the S7 Setup Communication connection request.
+
+        // Send an S7 Setup Communication response back to the pipeline.
+        byte[] setupCommunicationResponse = readPcapFile(
+            "org/apache/plc4x/java/s7/connection/s7-setup-communication-response.pcap");
         channel.writeInbound(Unpooled.wrappedBuffer(setupCommunicationResponse));
     }
 
@@ -100,6 +96,94 @@ public class S7PlcTestConnection extends S7PlcConnection {
             out[i] = (byte) in[i];
         }
         return out;
+    }
+
+    public static void dumpArrayToPcapFile(String filename, byte[] data) {
+        byte[] pcapHeader = toByteArray(
+            new int[]{
+                // Magic Bytes (Big Endian)
+                0xa1, 0xb2, 0xc3, 0xd4,
+                // Version (2.4)
+                0x00, 0x02, 0x00, 0x04,
+                // Timestamp
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                // Max Length
+                0x00, 0x00, 0xFF, 0xFF,
+                // Link Layer: Ethernet
+                0x00, 0x00, 0x00, 0x01,
+                // Timestamp (Seconds)
+                0x00, 0x00, 0x00, 0x00,
+                // Timestamp (Microseconds)
+                0x00, 0x00, 0x00, 0x00,
+                // Packet Length
+                0x00, 0x00, 0x00, 0x00,
+                // Packet Length (duplicate)
+                0x00, 0x00, 0x00, 0x00, // 40
+                /////////////////////////////////////
+                // Ethernet Header
+                // Destination Address (Mac Address)
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                // Source Address (Mac Address)
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                // IPv4
+                0x08, 0x00,
+                /////////////////////////////////////
+                // IPv4 Header (type + 20 byte length)
+                0x45, 0x00,
+                // Length
+                0x00, 0x00, // 40 + payload
+                0x00, 0x00,
+                0x00, 0x00,
+                0x1e,
+                // Protocol (TCP)
+                0x06,
+                // Header Checksum
+                0x00, 0x00,
+                // Source IP
+                0x00, 0x00, 0x00, 0x00,
+                // Dest IP
+                0x00, 0x00, 0x00, 0x00, // 34
+                /////////////////////////////////////
+                // TCP Header
+                // Source Port
+                0x00, 0x66,
+                // Dest Port
+                0x10, 0xd1,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                // Header Length
+                0x50, 0x18,
+                0xFA, 0xDA,
+                0x00, 0x00,
+                0x00, 0x00 // 54
+            });
+
+        byte[] total = new byte[pcapHeader.length + data.length];
+        System.arraycopy(pcapHeader, 0, total, 0, pcapHeader.length);
+        System.arraycopy(data, 0, total, pcapHeader.length, data.length);
+        total[35] = (byte) (data.length + 54);
+        total[39] = (byte) (data.length + 54);
+        total[39+18] = (byte) (data.length + 40);
+
+        try {
+            File file = new File(filename);
+            System.out.println("Dumping to file: "  + file.getAbsolutePath());
+            FileUtils.writeByteArrayToFile(file, total);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] readPcapFile(String filename) {
+        try {
+            InputStream in = getClass().getClassLoader().getResourceAsStream(filename);
+            byte[] pcap = IOUtils.toByteArray(in);
+            byte[] data = new byte[pcap.length - 94];
+            System.arraycopy(pcap, 94, data, 0, pcap.length - 94);
+            return data;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
