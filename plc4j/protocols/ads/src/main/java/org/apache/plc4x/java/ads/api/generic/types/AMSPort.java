39fb481a559a6091605a184b37a7de9662653f12
Sebastian Rühl <sruehl@apache.org>
2018-03-08 15:12:28 +0100

    fix build

--- plc4j/protocols/ads/src/main/java/org/apache/plc4x/java/ads/api/generic/types/AMSPort.java
+++ plc4j/protocols/ads/src/main/java/org/apache/plc4x/java/ads/api/generic/types/AMSPort.java
@@ -26,28 +26,6 @@ import java.util.regex.Pattern;
 
 /**
  * The ADS devices in a TwinCAT message router are uniquely identified by a number referred to as the ADS-PortNr. For ADS devices this has a fixed specification, whereas pure ADS client applications (e.g. a visualisation system) are allocated a variable ADS port number when they first access the message router.
- * <p>
- * The following ADS port numbers are already assigned:
- * <p>
- * <table border="1">
- * <th><td>ADS-PortNr</td><td>ADS device description</td></th>
- * <tr><td>100<tr><td>Logger (only NT-Log)</td></tr>
- * <tr><td>110<tr><td>Eventlogger</td></tr>
- * <tr><td>300<tr><td>IO</td></tr>
- * <tr><td>301<tr><td>additional Task 1</td></tr>
- * <tr><td>302<tr><td>additional Task 2</td></tr>
- * <tr><td><tr><td></td></tr>
- * <tr><td>500<tr><td>NC</td></tr>
- * <tr><td>801<tr><td>PLC RuntimeSystem 1</td></tr>
- * <tr><td>811<tr><td>PLC RuntimeSystem 2</td></tr>
- * <tr><td>821<tr><td>PLC RuntimeSystem 3</td></tr>
- * <tr><td>831<tr><td>PLC RuntimeSystem 4</td></tr>
- * <tr><td><tr><td></td></tr>
- * <tr><td>900<tr><td>Camshaft controller</td></tr>
- * <tr><td>10000<tr><td>System Service</td></tr>
- * <tr><td>14000<tr><td>Scope</td></tr>
- * </table>
- * </p>
  *
  * @see <a href="https://infosys.beckhoff.com/content/1033/tcadscommon/html/tcadscommon_identadsdevice.htm?id=3991659524769593444">ADS device identification</a>
  */
@@ -89,5 +67,68 @@ public class AMSPort extends ByteValue {
     @Override
     public String toString() {
         return Integer.toString(getBytes()[1] << 8 | getBytes()[0] & 0xff);
+    }
+
+    public static class ReservedPorts {
+
+        private ReservedPorts() {
+            // Container class
+        }
+
+        /**
+         * Logger (only NT-Log)
+         */
+        public static final AMSPort logger = AMSPort.of(100);
+        /**
+         * Eventlogger
+         */
+        public static final AMSPort eventLogger = AMSPort.of(110);
+        /**
+         * IO
+         */
+        public static final AMSPort io = AMSPort.of(300);
+        /**
+         * additional Task 1
+         */
+        public static final AMSPort additionalTask1 = AMSPort.of(301);
+        /**
+         * additional Task 2
+         */
+        public static final AMSPort additionalTask2 = AMSPort.of(302);
+
+        /**
+         * NC
+         */
+        public static final AMSPort nc = AMSPort.of(500);
+        /**
+         * PLC RuntimeSystem 1
+         */
+        public static final AMSPort plcRuntimeSystem1 = AMSPort.of(801);
+        /**
+         * PLC RuntimeSystem 2
+         */
+        public static final AMSPort plcRuntimeSystem2 = AMSPort.of(811);
+        /**
+         * PLC RuntimeSystem 3
+         */
+        public static final AMSPort plcRuntimeSystem3 = AMSPort.of(821);
+        /**
+         * PLC RuntimeSystem 4
+         */
+        public static final AMSPort plcRuntimeSystem4 = AMSPort.of(831);
+
+        /**
+         * Camshaft controller
+         */
+        public static final AMSPort camShaftController = AMSPort.of(900);
+
+        /**
+         * System Service
+         */
+        public static final AMSPort systemService = AMSPort.of(900);
+        /**
+         * Scope
+         */
+        public static final AMSPort scope = AMSPort.of(900);
     }
 }
