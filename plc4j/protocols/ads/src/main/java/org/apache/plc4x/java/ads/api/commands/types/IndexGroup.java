201ddc65cd4066c130eb0609156c3576de234eec
Sebastian Rühl <sruehl@apache.org>
2018-03-09 14:55:14 +0100

    added reserved index groups

--- plc4j/protocols/ads/src/main/java/org/apache/plc4x/java/ads/api/commands/types/IndexGroup.java
+++ plc4j/protocols/ads/src/main/java/org/apache/plc4x/java/ads/api/commands/types/IndexGroup.java
@@ -56,4 +56,33 @@ public class IndexGroup extends UnsignedIntLEByteValue {
     public static IndexGroup of(ByteBuf byteBuf) {
         return new IndexGroup(byteBuf);
     }
+
+    public static class ReservedGroups {
+        public static final IndexGroup ADSIGRP_SYMTAB = IndexGroup.of(0xF000);
+        public static final IndexGroup ADSIGRP_SYMNAME = IndexGroup.of(0xF001);
+        public static final IndexGroup ADSIGRP_SYMVAL = IndexGroup.of(0xF002);
+        public static final IndexGroup ADSIGRP_SYM_HNDBYNAME = IndexGroup.of(0xF003);
+        public static final IndexGroup ADSIGRP_SYM_VALBYNAME = IndexGroup.of(0xF004);
+        public static final IndexGroup ADSIGRP_SYM_VALBYHND = IndexGroup.of(0xF005);
+        public static final IndexGroup ADSIGRP_SYM_RELEASEHND = IndexGroup.of(0xF006);
+        public static final IndexGroup ADSIGRP_SYM_INFOBYNAME = IndexGroup.of(0xF007);
+        public static final IndexGroup ADSIGRP_SYM_VERSION = IndexGroup.of(0xF008);
+        public static final IndexGroup ADSIGRP_SYM_INFOBYNAMEEX = IndexGroup.of(0xF009);
+        public static final IndexGroup ADSIGRP_SYM_DOWNLOAD = IndexGroup.of(0xF00A);
+        public static final IndexGroup ADSIGRP_SYM_UPLOAD = IndexGroup.of(0xF00B);
+        public static final IndexGroup ADSIGRP_SYM_UPLOADINFO = IndexGroup.of(0xF00C);
+        public static final IndexGroup ADSIGRP_SYMNOTE = IndexGroup.of(0xF010);
+        public static final IndexGroup ADSIGRP_IOIMAGE_RWIB = IndexGroup.of(0xF020);
+        public static final IndexGroup ADSIGRP_IOIMAGE_RWIX = IndexGroup.of(0xF021);
+        public static final IndexGroup ADSIGRP_IOIMAGE_RISIZE = IndexGroup.of(0xF025);
+        public static final IndexGroup ADSIGRP_IOIMAGE_RWOB = IndexGroup.of(0xF030);
+        public static final IndexGroup ADSIGRP_IOIMAGE_RWOX = IndexGroup.of(0xF031);
+        public static final IndexGroup ADSIGRP_IOIMAGE_RWOSIZE = IndexGroup.of(0xF035);
+        public static final IndexGroup ADSIGRP_IOIMAGE_CLEARI = IndexGroup.of(0xF040);
+        public static final IndexGroup ADSIGRP_IOIMAGE_CLEARO = IndexGroup.of(0xF050);
+        public static final IndexGroup ADSIGRP_IOIMAGE_RWIOB = IndexGroup.of(0xF060);
+        public static final IndexGroup ADSIGRP_DEVICE_DATA = IndexGroup.of(0xF100);
+        public static final IndexGroup ADSIOFFS_DEVDATA_ADSSTATE = IndexGroup.of(0x0000);
+        public static final IndexGroup ADSIOFFS_DEVDATA_DEVSTATE = IndexGroup.of(0x0002);
+    }
 }
