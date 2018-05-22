851838e455cfe2fc59eae2e490428596a836a48e
Christofer Dutz <christofer.dutz@c-ware.de>
2018-05-22 17:40:49 +0200

    Updated the iot-factory example to use Elasticsearch to save data which can then be displayed with Kibana (for example)
    (cherry picked from commit 93ae433)

---
+++ examples/iot-factory/src/main/java/org/apache/plc4x/java/examples/iotfactory/IotElasticsearchFactory.java
@@ -0,0 +1,224 @@
+package org.apache.plc4x.java.examples.iotfactory;
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+import org.apache.edgent.function.Supplier;
+import org.apache.edgent.providers.direct.DirectProvider;
+import org.apache.edgent.topology.TStream;
+import org.apache.edgent.topology.Topology;
+import org.apache.plc4x.edgent.PlcConnectionAdapter;
+import org.apache.plc4x.edgent.PlcFunctions;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.IndicesAdminClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.node.InternalSettingsPreparer;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeValidationException;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.transport.Netty4Plugin;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+public class IotElasticsearchFactory {
+
+    public enum ConveyorState {
+        STOPPED,
+        RUNNING_LEFT,
+        RUNNING_RIGHT
+    }
+
+    private ConveyorState conveyorState = ConveyorState.STOPPED;
+
+    private Node startElasticsearchNode() throws NodeValidationException {
+        Node node = new MyNode(Settings.builder()
+            .put("transport.type", "netty4")
+            .put("http.type", "netty4")
+            .put("http.enabled", "true")
+            .put("path.home", "elasticsearch-data")
+            .build(), Collections.singletonList(Netty4Plugin.class));
+        node.start();
+        return node;
+    }
+
+    private void prepareIndexes(Client esClient) throws Exception {
+        IndicesAdminClient indicesAdminClient = esClient.admin().indices();
+
+        // Check if the factory-data index exists and create it, if it doesn't.
+        IndicesExistsRequest factoryDataIndexExistsRequest =
+            indicesAdminClient.prepareExists("product-data").request();
+        if(!indicesAdminClient.exists(factoryDataIndexExistsRequest).actionGet().isExists()) {
+            CreateIndexRequest createIndexRequest = new CreateIndexRequest("factory-data");
+            createIndexRequest.mapping("FactoryData",
+                "{\n" +
+                    "            \"properties\": {\n" +
+                    "                \"time\": {\n" +
+                    "                    \"type\": \"date\"\n" +
+                    "                }\n" +
+                    "            }\n" +
+                    "        }", XContentType.JSON);
+            CreateIndexResponse createIndexResponse = indicesAdminClient.create(createIndexRequest).actionGet();
+            if(!createIndexResponse.isAcknowledged()) {
+                throw new Exception("Could not create index 'product-data'");
+            }
+        }
+
+        // Check if the product-data index exists and create it, if it doesn't.
+        IndicesExistsRequest productDataIndexExistsRequest =
+            indicesAdminClient.prepareExists("product-data").request();
+        if(!indicesAdminClient.exists(productDataIndexExistsRequest).actionGet().isExists()) {
+            CreateIndexRequest createIndexRequest = new CreateIndexRequest("product-data");
+            createIndexRequest.mapping("ProductData",
+                "{\n" +
+                    "            \"properties\": {\n" +
+                    "                \"time\": {\n" +
+                    "                    \"type\": \"date\"\n" +
+                    "                },\n" +
+                    "                \"type\": {\n" +
+                    "                    \"type\": \"keyword\"\n" +
+                    "                }\n" +
+                    "            }\n" +
+                    "        }", XContentType.JSON);
+            CreateIndexResponse createIndexResponse = indicesAdminClient.create(createIndexRequest).actionGet();
+            if(!createIndexResponse.isAcknowledged()) {
+                throw new Exception("Could not create index 'product-data'");
+            }
+        }
+    }
+
+    private void runFactory() throws Exception {
+        // Start an Elasticsearch node.
+        Node esNode = startElasticsearchNode();
+        Client esClient = esNode.client();
+        System.out.println("Started Elasticsearch node on port 9200");
+
+        // Make sure the indexes exist prior to writing to them.
+        prepareIndexes(esClient);
+
+        // Get a plc connection.
+        try (PlcConnectionAdapter plcAdapter = new PlcConnectionAdapter("s7://10.10.64.20/1/1")) {
+            // Initialize the Edgent core.
+            DirectProvider dp = new DirectProvider();
+            Topology top = dp.newTopology();
+
+            // Define the event stream.
+            // 1) PLC4X source generating a stream of bytes.
+            Supplier<Byte> plcSupplier = PlcFunctions.byteSupplier(plcAdapter, "OUTPUTS/0");
+            // 2) Use polling to get an item from the byte-stream in regular intervals.
+            TStream<Byte> plcOutputStates = top.poll(plcSupplier, 100, TimeUnit.MILLISECONDS);
+
+            // 3a) Create a stream that pumps all data into a 'factory-data' index.
+            TStream<XContentBuilder> factoryData = plcOutputStates.map(value -> {
+                boolean conveyorEntry = (value & 1) != 0;
+                boolean load = (value & 2) != 0;
+                boolean unload = (value & 4) != 0;
+                boolean transferLeft = (value & 8) != 0;
+                boolean transferRight = (value & 16) != 0;
+                boolean conveyorLeft = (value & 32) != 0;
+                boolean conveyorRight = (value & 64) != 0;
+
+                try {
+                    return XContentFactory.jsonBuilder()
+                        .startObject()
+                        .field("time", Calendar.getInstance().getTimeInMillis())
+                        .field("conveyorEntry", conveyorEntry)
+                        .field("load", load)
+                        .field( "unload", unload)
+                        .field( "transferLeft", transferLeft)
+                        .field( "transferRight", transferRight)
+                        .field( "conveyorLeft", conveyorLeft)
+                        .field( "conveyorRight", conveyorRight)
+                        .endObject();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            });
+            TStream<IndexResponse> factoryDataResponses = factoryData.map(
+                value -> esClient.prepareIndex("factory-data", "FactoryData").setSource(value).get());
+            factoryDataResponses.print();
+
+            // 3b) Create a stream that does some local analysis to detect big and small boxes and to only output
+            //     something to the 'product-data' index, if a new item is detected.
+            TStream<XContentBuilder> productData = plcOutputStates.map(value -> {
+                    boolean transferLeft = (value & 8) != 0;
+                    boolean transferRight = (value & 16) != 0;
+
+                    if (conveyorState == ConveyorState.STOPPED) {
+                        if (transferLeft | transferRight) {
+                            if (transferLeft) {
+                                conveyorState = ConveyorState.RUNNING_LEFT;
+                                try {
+                                    return XContentFactory.jsonBuilder()
+                                        .startObject()
+                                        .field("time", Calendar.getInstance().getTimeInMillis())
+                                        .field("type", "small")
+                                        .endObject();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            } else {
+                                conveyorState = ConveyorState.RUNNING_RIGHT;
+                                try {
+                                    return XContentFactory.jsonBuilder()
+                                        .startObject()
+                                        .field("time", Calendar.getInstance().getTimeInMillis())
+                                        .field("type", "large")
+                                        .endObject();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    } else if (!(transferLeft | transferRight)) {
+                        conveyorState = ConveyorState.STOPPED;
+                    }
+                    return null;
+                });
+            TStream<IndexResponse> productDataResponses = productData.map(
+                value -> esClient.prepareIndex("product-data", "ProductData").setSource(value).get());
+            productDataResponses.print();
+
+            // Submit the topology and hereby start the event streams.
+            dp.submit(top);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        IotElasticsearchFactory factory = new IotElasticsearchFactory();
+        factory.runFactory();
+    }
+
+    private static class MyNode extends Node {
+        private MyNode(Settings preparedSettings, Collection<Class<? extends Plugin>> classpathPlugins) {
+            super(InternalSettingsPreparer.prepareEnvironment(preparedSettings, null), classpathPlugins);
+        }
+    }
+
+}
