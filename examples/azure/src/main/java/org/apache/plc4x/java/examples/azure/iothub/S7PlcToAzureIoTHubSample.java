dd293bab023aa62f94096f0f2f4ae3446a87d9db
Sebastian Rühl <sruehl@apache.org>
2018-08-23 16:21:26 +0200

    cleanup fo azure example

--- examples/azure/src/main/java/org/apache/plc4x/java/examples/azure/iothub/S7PlcToAzureIoTHubSample.java
+++ examples/azure/src/main/java/org/apache/plc4x/java/examples/azure/iothub/S7PlcToAzureIoTHubSample.java
@@ -28,20 +28,22 @@ import org.apache.plc4x.java.api.model.Address;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 public class S7PlcToAzureIoTHubSample {
 
-    private static final Logger logger = LoggerFactory.getLogger(S7PlcToAzureIoTHubSample.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(S7PlcToAzureIoTHubSample.class);
 
     private static IotHubClientProtocol protocol = IotHubClientProtocol.MQTT;
     private static DeviceClient client;
 
     public static class Callback implements IotHubEventCallback {
 
+        private static final Logger LOGGER = LoggerFactory.getLogger(Callback.class);
+
         @Override
         public void execute(IotHubStatusCode iotHubStatusCode, Object ctx) {
-            System.out.println("Received status: " + iotHubStatusCode.name());
+            LOGGER.info("Received status: ", iotHubStatusCode);
         }
     }
 
@@ -50,46 +52,39 @@ public class S7PlcToAzureIoTHubSample {
      *
      * @param args Expected: [plc4x connection string, plc4x address, IoT-Hub connection string].
      */
-    public static void main(String[] args) {
-        logger.info("Connecting");
+    public static void main(String[] args) throws Exception {
+        LOGGER.info("Connecting");
         try (PlcConnection plcConnection = new PlcDriverManager().getConnection(args[0])) {
-            logger.info("Connected");
+            LOGGER.info("Connected");
 
             client = new DeviceClient(args[2], protocol);
             client.open();
 
-            Optional<PlcReader> reader = plcConnection.getReader();
+            PlcReader plcReader = plcConnection.getReader().orElseThrow(IllegalStateException::new);
 
-            if (reader.isPresent()) {
-                PlcReader plcReader = reader.get();
-                Address outputs = plcConnection.parseAddress(args[1]);
+            Address outputs = plcConnection.parseAddress(args[1]);
 
-                while (true) {
-                    // Simulate telemetry.
-                    TypeSafePlcReadResponse<Byte> plcReadResponse = plcReader.read(
-                        new TypeSafePlcReadRequest<>(Byte.class, outputs)).get();
+            while (!Thread.currentThread().isInterrupted()) {
+                // Simulate telemetry.
+                TypeSafePlcReadResponse<Byte> plcReadResponse = plcReader.read(
+                    new TypeSafePlcReadRequest<>(Byte.class, outputs)).get();
 
-                    System.out.println("Outputs: " + Long.toBinaryString(plcReadResponse.getResponseItem()
-                        .orElseThrow(() -> new IllegalStateException("No response available"))
-                        .getValues().get(0).longValue()));
-
-                    plcReadResponse.getResponseItem().map(byt -> {
-                            String result = Long.toBinaryString(byt.getValues().get(0).longValue());
+                plcReadResponse.getResponseItems().stream()
+                    .flatMap(readResponseItem -> readResponseItem.getValues().stream())
+                    .forEach(byteValue -> {
+                            String result = Long.toBinaryString(byteValue.longValue());
+                            LOGGER.info("Outputs {}", result);
                             Message msg = new Message("{ \"bits\" : \"" + result + "\"}");
                             // Send the message.
                             Callback callback = new Callback();
 
                             client.sendEventAsync(msg, callback, new Object());
-                            return byt;
                         }
                     );
 
-                    Thread.sleep(1000);
-                }
+                TimeUnit.SECONDS.sleep(1);
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
