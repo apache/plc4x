b430e28a55684762e8ea93212c210677f6cd2448
Andrey Skorikov <andrey.skorikov@codecentric.de>
2018-09-06 11:59:19 +0200

    added configuration options

--- integrations/apache-kafka/src/main/java/org/apache/plc4x/kafka/common/Plc4xConfig.java
+++ integrations/apache-kafka/src/main/java/org/apache/plc4x/kafka/common/Plc4xConfig.java
@@ -33,6 +33,15 @@ public class Plc4xConfig extends AbstractConfig {
     public static final String PLC_CONNECTION_STRING_DISPLAY = "PLC Connection String";
     public static final String PLC_CONNECTION_STRING_DOC = "Connection string used by PLC4X to connect to the PLC.";
 
+    public static final String PLC_TOPIC = "topic";
+    public static final String PLC_TOPIC_DOC = "Kafka topic to publish messages to.";
+
+    public static final String PLC_DATATYPE_CONFIG = "type";
+    public static final String PLC_DATATYPE_DOC = "Data type of values sent or received by PLC.";
+
+    public static final String PLC_ADDRESS = "address";
+    public static final String PLC_ADDRESS_DOC = "PLC address to sent to or receive data from.";
+
     public static ConfigDef baseConfigDef() {
         ConfigDef config = new ConfigDef();
         addPlcOptions(config);
@@ -44,7 +53,22 @@ public class Plc4xConfig extends AbstractConfig {
             PLC_CONNECTION_STRING_CONFIG,
             Type.STRING,
             Importance.HIGH,
-            PLC_CONNECTION_STRING_DOC);
+            PLC_CONNECTION_STRING_DOC)
+        .define(
+            PLC_DATATYPE_CONFIG,
+            Type.CLASS,
+            Importance.HIGH,
+            PLC_DATATYPE_DOC)
+        .define(
+            PLC_TOPIC,
+            Type.STRING,
+            Importance.HIGH,
+            PLC_TOPIC_DOC)
+        .define(
+            PLC_ADDRESS,
+            Type.STRING,
+            Importance.HIGH,
+            PLC_ADDRESS_DOC);
     }
 
     public static final ConfigDef CONFIG_DEF = baseConfigDef();
