2e0dd07a601568e2029389165ab2d51d24fa1870
Sebastian Rühl <sruehl@apache.org>
2018-10-25 17:42:28 +0200

    [plc4j-opm] cleanups: - updated copyrights - removed @author - fixed several small issues - added documentation

--- plc4j/utils/opm/src/main/java/org/apache/plc4x/java/opm/PlcEntityManager.java
+++ plc4j/utils/opm/src/main/java/org/apache/plc4x/java/opm/PlcEntityManager.java
@@ -1,3 +1,22 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
 package org.apache.plc4x.java.opm;
 
 import net.bytebuddy.ByteBuddy;
@@ -8,6 +27,7 @@ import org.apache.plc4x.java.api.connection.PlcConnection;
 import org.apache.plc4x.java.api.connection.PlcReader;
 import org.apache.plc4x.java.api.exceptions.PlcConnectionException;
 import org.apache.plc4x.java.api.exceptions.PlcInvalidFieldException;
+import org.apache.plc4x.java.api.exceptions.PlcRuntimeException;
 import org.apache.plc4x.java.api.messages.PlcReadRequest;
 import org.apache.plc4x.java.api.messages.PlcReadResponse;
 import org.slf4j.Logger;
@@ -26,39 +46,37 @@ import static net.bytebuddy.matcher.ElementMatchers.any;
 /**
  * Plc4x equivalent of Jpas EntityManager for implementing Object-Plc-Mapping.
  * This means that calls to a plc can be done by using plain POJOs with Annotations.
- *
+ * <p>
  * First, the necessary annotations are {@link PlcEntity} and {@link PlcField}.
- * For a class to be useable as PlcEntity it needs
+ * For a class to be usable as PlcEntity it needs
  * <ul>
- *     <li>be non-final (as proxiing has to be used in case of {@link #connect(Class)}</li>
- *     <li>a public no args constructor for instanciation</li>
- *     <li>Needs to be annotated with {@link PlcEntity} and has a valid value which is the connection string</li>
+ * <li>be non-final (as proxiing has to be used in case of {@link #connect(Class)}</li>
+ * <li>a public no args constructor for instanciation</li>
+ * <li>Needs to be annotated with {@link PlcEntity} and has a valid value which is the connection string</li>
  * </ul>
- *
+ * <p>
  * Basically, the {@link PlcEntityManager} has to operation "modes" represented by the methods {@link #read(Class)} and
  * {@link #connect(Class)}.
- *
+ * <p>
  * For a field to get Values from the Plc Injected it needs to be annotated with the {@link PlcField} annotation.
  * The value has to be the plc fields string (which is inserted in the {@link PlcReadRequest}).
  * The connection string is taken from the value of the {@link PlcEntity} annotation on the class.
- *
+ * <p>
  * The {@link #read(Class)} method has no direkt equivalent in JPA (as far as I know) as it only returns a "detached"
  * entity. This means it fetches all values from the plc that are annotated wiht the {@link PlcField} annotations.
- *
+ * <p>
  * The {@link #connect(Class)} method is more JPA-like as it returns a "connected" entity. This means, that each
  * time one of the getters on the returned entity is called a call is made to the plc (and the field value is changed
  * for this specific field).
  * Furthermore, if a method which is no getter is called, then all {@link PlcField}s are refreshed before doing the call.
  * Thus, all operations on fields that are annotated with {@link PlcField} are always done against the "live" values
  * from the PLC.
- *
+ * <p>
  * // TODO Add detach method
- *
- * @author julian
  */
 public class PlcEntityManager {
 
-    private static final Logger logger = LoggerFactory.getLogger(PlcEntityManager.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PlcEntityManager.class);
 
     private final PlcDriverManager driverManager;
 
@@ -74,15 +92,13 @@ public class PlcEntityManager {
         PlcEntity annotation = getPlcEntityAndCheckPreconditions(clazz);
         String source = annotation.value();
 
-        PlcReader reader;
-
         try (PlcConnection connection = driverManager.getConnection(source)) {
 
             if (!connection.getReader().isPresent()) {
                 throw new OPMException("Unable to get Reader for connection with url '" + source + "'");
             }
 
-            reader = connection.getReader().get();
+            PlcReader reader = connection.getReader().get();
 
             PlcReadRequest.Builder requestBuilder = reader.readRequestBuilder();
 
@@ -110,6 +126,7 @@ public class PlcEntityManager {
             // Perform the request
             PlcReadResponse<?> response;
             try {
+                // TODO: make configurable.
                 response = reader.read(request).get(1_000, TimeUnit.MILLISECONDS);
             } catch (InterruptedException | ExecutionException e) {
                 throw new OPMException("Request fetching not able", e);
@@ -135,13 +152,13 @@ public class PlcEntityManager {
     /**
      * Returns a connected proxy.
      *
-     * @param clazz
-     * @param <T>
-     * @return
-     * @throws OPMException
+     * @param clazz clazz to be connected.
+     * @param <T>   type of param {@code clazz}.
+     * @return a connected entity.
+     * @throws OPMException when proxy can't be build.
      */
     public <T> T connect(Class<T> clazz) throws OPMException {
-        PlcEntity annotation = getPlcEntityAndCheckPreconditions(clazz);
+        getPlcEntityAndCheckPreconditions(clazz);
         try {
             // Use Byte Buddy to generate a subclassed proxy that delegates all PlcField Methods
             // to the intercept method
@@ -167,7 +184,7 @@ public class PlcEntityManager {
         try {
             clazz.getConstructor();
         } catch (NoSuchMethodException e) {
-            throw new IllegalArgumentException("Cannot use PlcEntity without default constructor");
+            throw new IllegalArgumentException("Cannot use PlcEntity without default constructor", e);
         }
         return annotation;
     }
@@ -182,31 +199,39 @@ public class PlcEntityManager {
      * Basic Intersector for all methods on the proxy object.
      * It checks if the invoked method is a getter and if so, only retrieves the requested field, forwarding to
      * the {@link #fetchValueForGetter(Object, Method)} method.
-     *
+     * <p>
      * If the field is no getter, then all fields are refreshed by calling {@link #refetchAllFields(Object)}
      * and then, the method is invoked.
      *
-     * @param m Method that was intercepted
-     * @param c Callable to call the method after fetching the values
+     * @param o    Object to intercept
+     * @param m    Method that was intercepted
+     * @param c    Callable to call the method after fetching the values
      * @param that Reference to the proxy object
      * @return possible result of the original methods invocation
      * @throws OPMException Problems with plc / proxying
      */
     @RuntimeType
+    // TODO: avoid using single letter params.
     public Object intercept(@This Object o, @Origin Method m, @SuperCall Callable<?> c, @Super Object that) throws OPMException {
-        logger.trace("Invoked method {} on connected PlcEntity {}", m.getName(), that);
+        LOGGER.trace("Invoked method {} on connected PlcEntity {}", m.getName(), that);
 
-        // TODO enable getters starting with "is"
         if (m.getName().startsWith("get")) {
             // Fetch single value
-            logger.trace("Invoked method {} is getter, trying to find annotated field and return requested value",
+            LOGGER.trace("Invoked method {} is getter, trying to find annotated field and return requested value",
+                m.getName());
+            return fetchValueForGetter(that, m);
+        }
+
+        if (m.getName().startsWith("is") && (m.getReturnType() == boolean.class || m.getReturnType() == Boolean.class)) {
+            // Fetch single value
+            LOGGER.trace("Invoked method {} is boolean flag method, trying to find annotated field and return requested value",
                 m.getName());
             return fetchValueForGetter(that, m);
         }
 
         // Fetch all values, than invoke method
         try {
-            logger.trace("Invoked method is no getter, refetch all fields and invoke method {} then", m.getName());
+            LOGGER.trace("Invoked method is no getter, refetch all fields and invoke method {} then", m.getName());
             refetchAllFields(o);
             return c.call();
         } catch (Exception e) {
@@ -215,7 +240,6 @@ public class PlcEntityManager {
     }
 
     /**
-     *
      * Renews all values of all Fields that are annotated with {@link PlcEntity}.
      *
      * @param o Object to refresh he fields on.
@@ -235,7 +259,7 @@ public class PlcEntityManager {
                 // Check if the field has an annotation
                 PlcField plcField = field.getDeclaredAnnotation(PlcField.class);
                 if (plcField != null) {
-                    logger.trace("Adding field " + field.getName() + " to request as " + plcField.value());
+                    LOGGER.trace("Adding field " + field.getName() + " to request as " + plcField.value());
                     builder.addItem(field.getName(), plcField.value());
                 }
 
@@ -246,11 +270,11 @@ public class PlcEntityManager {
 
             // Fill all requested fields
             for (String fieldName : response.getFieldNames()) {
-                logger.trace("Value for field " + fieldName + " is " + response.getObject(fieldName));
+                LOGGER.trace("Value for field " + fieldName + " is " + response.getObject(fieldName));
                 try {
                     setField(o.getClass().getSuperclass(), o, response, fieldName);
                 } catch (NoSuchFieldException | IllegalAccessException e) {
-                    e.printStackTrace();
+                    throw new PlcRuntimeException(e);
                 }
             }
         } catch (Exception e) {
@@ -258,21 +282,22 @@ public class PlcEntityManager {
         }
     }
 
+    // TODO: why isn't o used?
     private Object fetchValueForGetter(Object o, Method m) throws OPMException {
         String s = m.getName().substring(3);
         // First char to lower
         String variable = s.substring(0, 1).toLowerCase().concat(s.substring(1));
-        logger.trace("Looking for field with name {} after invokation of getter {}", variable, m.getName());
-        PlcField annotation = null;
+        LOGGER.trace("Looking for field with name {} after invokation of getter {}", variable, m.getName());
+        PlcField annotation;
         try {
             annotation = m.getDeclaringClass().getDeclaredField(variable).getDeclaredAnnotation(PlcField.class);
         } catch (NoSuchFieldException e) {
             throw new OPMException("Unable to identify field annotated field for call to " + m.getName(), e);
         }
         PlcEntity plcEntity = m.getDeclaringClass().getAnnotation(PlcEntity.class);
-        try (PlcConnection connection = driverManager.getConnection(plcEntity.value())){
+        try (PlcConnection connection = driverManager.getConnection(plcEntity.value())) {
             // Catch the exception, if no reader present (see below)
-            PlcReader plcReader = connection.getReader().get();
+            PlcReader plcReader = connection.getReader().orElseThrow(IllegalStateException::new);
 
             // Assume to do the query here...
             PlcReadRequest request = plcReader.readRequestBuilder()
@@ -282,11 +307,9 @@ public class PlcEntityManager {
             PlcReadResponse<?> response;
             response = getPlcReadResponse(plcReader, request);
 
-            try {
-                return getTyped(m.getReturnType(), response, m.getName());
-            } catch (ClassCastException e) {
-                throw new OPMException("Unable to return response as suitable type", e);
-            }
+            return getTyped(m.getReturnType(), response, m.getName());
+        } catch (ClassCastException e) {
+            throw new OPMException("Unable to return response as suitable type", e);
         } catch (Exception e) {
             throw new OPMException("Problem during processing", e);
         }
@@ -297,8 +320,8 @@ public class PlcEntityManager {
      * This is one by looking for a field in the class and a response item
      * which is equal to the given fieldName parameter.
      *
-     * @param o Object to set the value on
-     * @param response Response to fetch the response from
+     * @param o         Object to set the value on
+     * @param response  Response to fetch the response from
      * @param fieldName Name of the field in the object and the response
      * @throws NoSuchFieldException
      * @throws IllegalAccessException
@@ -310,10 +333,10 @@ public class PlcEntityManager {
             field.set(o, getTyped(field.getType(), response, fieldName));
         } catch (ClassCastException e) {
             // TODO should we simply fail here?
-            logger.warn("Unable to assign return value {} to field {} with type {}", response.getObject(fieldName), fieldName, field.getType());
+            LOGGER.warn("Unable to assign return value {} to field {} with type {}", response.getObject(fieldName), fieldName, field.getType(), e);
         }
     }
-    
+
     private Object getTyped(Class<?> clazz, PlcReadResponse<?> response, String fieldName) {
         if (clazz.isPrimitive()) {
             if (clazz == byte.class) {
@@ -326,7 +349,7 @@ public class PlcEntityManager {
                 return response.getShort(fieldName);
             }
             // TODO this should fail on Short, Integer, ... because it always gets a Long
-        } else if (clazz.isAssignableFrom(response.getObject(fieldName).getClass())){
+        } else if (clazz.isAssignableFrom(response.getObject(fieldName).getClass())) {
             return response.getObject(fieldName);
         }
         // If nothing matched, throw
@@ -335,6 +358,7 @@ public class PlcEntityManager {
 
     /**
      * Fetch the request and do appropriate error handling
+     *
      * @param plcReader
      * @param request
      * @return
--- plc4j/utils/opm/src/test/java/org/apache/plc4x/java/opm/PlcEntityManagerTest.java
+++ plc4j/utils/opm/src/test/java/org/apache/plc4x/java/opm/PlcEntityManagerTest.java
@@ -1,3 +1,22 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
 package org.apache.plc4x.java.opm;
 
 import org.apache.commons.lang3.tuple.Pair;
@@ -131,12 +150,7 @@ public class PlcEntityManagerTest {
                 Map<String, Pair<PlcResponseCode, FieldItem>> map = readRequest.getFieldNames().stream()
                     .collect(Collectors.toMap(
                         Function.identity(),
-                        new Function<String, Pair<PlcResponseCode, FieldItem>>() {
-                            @Override
-                            public Pair<PlcResponseCode, FieldItem> apply(String s) {
-                                return Pair.of(PlcResponseCode.OK, responses.get(s));
-                            }
-                        }
+                        s -> Pair.of(PlcResponseCode.OK, responses.get(s))
                     ));
                 return CompletableFuture.completedFuture(new DefaultPlcReadResponse(((InternalPlcReadRequest) readRequest), map));
             }
@@ -152,76 +166,78 @@ public class PlcEntityManagerTest {
     }
 
     private PlcFieldHandler getFieldHandler() {
-        return new PlcFieldHandler() {
-            @Override
-            public org.apache.plc4x.java.api.model.PlcField createField(String fieldQuery) throws PlcInvalidFieldException {
-                return new org.apache.plc4x.java.api.model.PlcField() {
-                };
-            }
-
-            @Override
-            public FieldItem encodeBoolean(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeByte(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeShort(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeInteger(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeBigInteger(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeLong(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeFloat(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeDouble(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeString(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeTime(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeDate(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-
-            @Override
-            public FieldItem encodeDateTime(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
-                return null;
-            }
-        };
+        return new NoOpPlcFieldHandler();
     }
 
-    public static class NoEntity {
+    private static class NoOpPlcFieldHandler implements PlcFieldHandler {
+        @Override
+        public org.apache.plc4x.java.api.model.PlcField createField(String fieldQuery) throws PlcInvalidFieldException {
+            return new org.apache.plc4x.java.api.model.PlcField() {
+            };
+        }
+
+        @Override
+        public FieldItem encodeBoolean(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeByte(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeShort(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeInteger(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeBigInteger(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeLong(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeFloat(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeDouble(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeString(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeTime(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeDate(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+
+        @Override
+        public FieldItem encodeDateTime(org.apache.plc4x.java.api.model.PlcField field, Object[] values) {
+            return null;
+        }
+    }
+
+    private static class NoEntity {
 
     }
 
