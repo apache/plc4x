19552c62320865eaddead8e18d61be155b3b8981
Andrey Skorikov <andrey.skorikov@codecentric.de>
2018-10-26 12:02:13 +0200

    added UnsopportedOperationException

---
+++ plc4j/api/src/main/java/org/apache/plc4x/java/api/exceptions/PlcUnsupportedOperationException.java
@@ -0,0 +1,9 @@
+package org.apache.plc4x.java.api.exceptions;
+
+public class PlcUnsupportedOperationException extends PlcRuntimeException {
+
+    public PlcUnsupportedOperationException(String message) {
+        super(message);
+    }
+
+}
