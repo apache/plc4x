04f3e4768b18caea033b14ae2f56e2b603b20e51
Christofer Dutz <christofer.dutz@c-ware.de>
2018-11-01 12:49:42 +0100

    Moved the Hello OPM example into a dedicated module.

--- examples/hello-opm/src/main/java/org/apache/plc4x/java/examples/helloopm/HelloOpm.java
+++ examples/hello-opm/src/main/java/org/apache/plc4x/java/examples/helloopm/HelloOpm.java
@@ -7,7 +7,7 @@
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+     http://www.apache.org/licenses/LICENSE-2.0
 
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
@@ -17,7 +17,7 @@
  under the License.
  */
 
-package org.apache.plc4x.java.examples.helloplc4x;
+package org.apache.plc4x.java.examples.helloopm;
 
 import org.apache.plc4x.java.opm.OPMException;
 import org.apache.plc4x.java.opm.PlcEntity;
