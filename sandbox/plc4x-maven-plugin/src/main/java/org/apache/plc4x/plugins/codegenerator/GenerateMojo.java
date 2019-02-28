539521bd13b45e9f7ed090db216d56264ed873ac
Christofer Dutz <christofer.dutz@c-ware.de>
2019-02-28 12:30:12 +0100

    - Got a first version of the tests running ...

--- sandbox/plc4x-maven-plugin/src/main/java/org/apache/plc4x/plugins/codegenerator/GenerateMojo.java
+++ sandbox/plc4x-maven-plugin/src/main/java/org/apache/plc4x/plugins/codegenerator/GenerateMojo.java
@@ -1,5 +1,22 @@
-package org.apache.plc4x.plugins.codegenerator;
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
 
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+package org.apache.plc4x.plugins.codegenerator;
 
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
@@ -27,27 +44,43 @@ public class GenerateMojo extends AbstractMojo {
      * Where to generate the code.
      */
     @Parameter(defaultValue = "${project.build.directory}/generated-sources/plc4x/", property = "outputDir", required = true)
-    private File outputDirectory;
+    private File outputDir;
 
     /**
      * The path to the DFDL schema file in the modules classpath (Ideally as runtime dependency)
      */
     @Parameter(property = "dfdl-schema", required = true)
-    private File dfdlSchemaFile;
+    private String dfdlSchema;
 
     public void execute()
         throws MojoExecutionException {
 
         // Make sure the output directory exists.
-        if (!outputDirectory.exists()) {
-            if(!outputDirectory.mkdirs()) {
-                throw new MojoExecutionException("Could not generate output directory " + outputDirectory.getAbsolutePath());
+        if (!outputDir.exists()) {
+            if (!outputDir.mkdirs()) {
+                throw new MojoExecutionException("Could not generate output directory " + outputDir.getAbsolutePath());
             }
         }
 
         try {
-            for (String runtimeClasspathElement : project.getRuntimeClasspathElements()) {
-                getLog().info("Inspecting " + runtimeClasspathElement);
+            File schemaFile = new File(dfdlSchema);
+            if(!schemaFile.exists()) {
+                for (String runtimeClasspathElement : project.getRuntimeClasspathElements()) {
+                    File classPathElement = new File(runtimeClasspathElement);
+                    if (classPathElement.exists()) {
+                        // This is usually the "target/classes" directory.
+                        if (classPathElement.isDirectory()) {
+                            schemaFile = new File(classPathElement, dfdlSchema);
+                            if (schemaFile.exists()) {
+                                break;
+                            }
+                        }
+                        // These are usually jar files.
+                        else {
+                            // TODO: Implement ...
+                        }
+                    }
+                }
             }
         } catch (DependencyResolutionRequiredException e) {
             throw new MojoExecutionException("Error resolving dependencies", e);
--- sandbox/plc4x-maven-plugin/src/test/java/org/apache/plc4x/plugins/codegenerator/GenerateMojoTest.java
+++ sandbox/plc4x-maven-plugin/src/test/java/org/apache/plc4x/plugins/codegenerator/GenerateMojoTest.java
@@ -1,8 +1,27 @@
-package org.apache.plc4x.plugins.codegenerator;
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
 
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+package org.apache.plc4x.plugins.codegenerator;
 
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.WithoutMojo;
+import org.apache.maven.plugin.testing.resources.TestResources;
+import org.apache.maven.project.MavenProject;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -11,52 +30,38 @@ import java.io.File;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-public class GenerateMojoTest
-{
-    @Rule
-    public MojoRule rule = new MojoRule()
-    {
-        @Override
-        protected void before() throws Throwable 
-        {
-        }
+public class GenerateMojoTest {
 
-        @Override
-        protected void after()
-        {
-        }
-    };
+    @Rule
+    public MojoRule rule = new MojoRule();
+
+    @Rule
+    public TestResources testResources = new TestResources();
 
     /**
      * @throws Exception if any
      */
     @Test
-    public void testSomething()
-            throws Exception
-    {
-        File pom = new File( "target/test-classes/project-to-test/" );
-        assertNotNull( pom );
-        assertTrue( pom.exists() );
+    public void testSomething() throws Exception {
+        File baseDir = testResources.getBasedir("simple-embedded-schema");
+        MavenProject project = rule.readMavenProject(baseDir);
 
-        GenerateMojo myMojo = (GenerateMojo) rule.lookupConfiguredMojo( pom, "generate-driver" );
-        assertNotNull( myMojo );
-        myMojo.execute();
+        GenerateMojo generateMojo = (GenerateMojo) rule.lookupConfiguredMojo(project, "generate-driver");
+        assertNotNull(generateMojo);
+        generateMojo.execute();
 
-        File outputDirectory = ( File ) rule.getVariableValueFromObject( myMojo, "outputDirectory" );
-        assertNotNull( outputDirectory );
-        assertTrue( outputDirectory.exists() );
-
-        File touch = new File( outputDirectory, "touch.txt" );
-        assertTrue( touch.exists() );
-
+        File outputDirectory = (File) rule.getVariableValueFromObject(generateMojo, "outputDir");
+        assertNotNull(outputDirectory);
+        assertTrue(outputDirectory.exists());
     }
 
-    /** Do not need the MojoRule. */
+    /**
+     * Do not need the MojoRule.
+     */
     @WithoutMojo
     @Test
-    public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn()
-    {
-        assertTrue( true );
+    public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn() {
+        assertTrue(true);
     }
 
 }
