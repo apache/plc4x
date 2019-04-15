738f5aca78ec19df81bfa90e11b52c8388e449f4
Björn Höper <hoeper@ltsoft.de>
2019-04-15 21:06:19 +0200

    [PLC4X-110] Added core class implementation

---
+++ plc4net/plc4net.driver/IPlcDriver.cs
@@ -0,0 +1,10 @@
+namespace plc4net.driver
+{
+    /// <summary>
+    /// Interface for PLC drivers to be implemented
+    /// </summary>
+    public interface IPlcDriver
+    {
+        IPlcC
+    }
+}
