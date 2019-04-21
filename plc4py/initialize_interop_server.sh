de3a0c1adeb6cb15907e7ff7fb0674be26846ee8
julian <j.feinauer@pragmaticminds.de>
2019-04-21 14:18:49 +0200

    PLC4X-111 Fixed several scripts.

--- plc4py/initialize_interop_server.sh
+++ plc4py/initialize_interop_server.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Clean the directory
+rm -f lib/*.jar
+
 # Builds the interop server module with maven
 mvn clean package -f ../plc4j/utils/interop/pom.xml
 
 # Moves the server from the target folder here
-cp ../plc4j/utils/interop/target/apache-plc4x-incubating-* lib/
+cp ../plc4j/utils/interop/target/apache-plc4x-incubating-*-SNAPSHOT-jar-with-dependencies.jar lib/interop-server.jar
