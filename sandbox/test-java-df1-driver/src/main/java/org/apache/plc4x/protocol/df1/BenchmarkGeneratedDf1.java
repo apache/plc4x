cf3f001a2edc4313dbf9a99dabc3aa90f7cf127a
v.emmert <v.emmert@pragmaticminds.de>
2019-07-29 13:32:10 +0200

    DLC-207
    DF1 driver prototype

--- sandbox/test-java-df1-driver/src/main/java/org/apache/plc4x/protocol/df1/BenchmarkGeneratedDf1.java
+++ sandbox/test-java-df1-driver/src/main/java/org/apache/plc4x/protocol/df1/BenchmarkGeneratedDf1.java
@@ -17,55 +17,206 @@
  * under the License.
  */
 
-package org.apache.plc4x.protocol.df1;import org.apache.commons.codec.binary.Hex;
+package org.apache.plc4x.protocol.df1;
+
+import com.fazecast.jSerialComm.SerialPort;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.plc4x.java.df1.TPKTPacket;
 import org.apache.plc4x.java.df1.io.TPKTPacketIO;
 import org.apache.plc4x.java.utils.ReadBuffer;
 import org.apache.plc4x.java.utils.WriteBuffer;
+import purejavacomm.CommPortIdentifier;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.BitSet;
 
 public class BenchmarkGeneratedDf1 {
 
     public static void main(String[] args) throws Exception {
-        byte[] rData = Hex.decodeHex("0300006702f080320100000001005600000407120a10060001032b84000160120a10020001032b840001a0120a10010001032b840001a9120a10050001032b84000150120a10020001032b84000198120a10040001032b84000140120a10020001032b84000190");
-        long start = System.currentTimeMillis();
-        int numRunsParse = 2000000;
-        TPKTPacketIO tpktPacketIO = new TPKTPacketIO();
+//        byte[] rData = Hex.decodeHex("0300006702f080320100000001005600000407120a10060001032b84000160120a10020001032b840001a0120a10010001032b840001a9120a10050001032b84000150120a10020001032b84000198120a10040001032b84000140120a10020001032b84000190");
+//        long start = System.currentTimeMillis();
+//        int numRunsParse = 2000000;
+//        TPKTPacketIO tpktPacketIO = new TPKTPacketIO();
+//
+//
+//        CommPortIdentifier id = CommPortIdentifier.getPortIdentifier("");
+//
+//
+//        // Benchmark the parsing code
+//        TPKTPacket packet = null;
+//        for(int i = 0; i < numRunsParse; i++) {
+//            ReadBuffer rBuf = new ReadBuffer(rData);
+//            packet = tpktPacketIO.parse(rBuf);
+//        }
+//        long endParsing = System.currentTimeMillis();
+//
+//        System.out.println("Parsed " + numRunsParse + " packets in " + (endParsing - start) + "ms");
+//        System.out.println("That's " + ((float) (endParsing - start) / numRunsParse) + "ms per packet");
+//
+//        // Benchmark the serializing code
+//        int numRunsSerialize = 2000000;
+//        byte[] oData = null;
+//        for(int i = 0; i < numRunsSerialize; i++) {
+//            WriteBuffer wBuf = new WriteBuffer(packet.getLengthInBytes());
+//            tpktPacketIO.serialize(wBuf, packet);
+//            oData = wBuf.getData();
+//        }
+//        long endSerializing = System.currentTimeMillis();
+//
+//        System.out.println("Serialized " + numRunsSerialize + " packets in " + (endSerializing - endParsing) + "ms");
+//        System.out.println("That's " + ((float) (endSerializing - endParsing) / numRunsSerialize) + "ms per packet");
+//        if(!Arrays.equals(rData, oData)) {
+//            for(int i = 0; i < rData.length; i++) {
+//                if(rData[i] != oData[i]) {
+//                    System.out.println("Difference in byte " + i);
+//                }
+//            }
+//            System.out.println("Not equals");
+//        } else {
+//            System.out.println("Bytes equal");
+//        }
 
-        // Benchmark the parsing code
-        TPKTPacket packet = null;
-        for(int i = 0; i < numRunsParse; i++) {
-            ReadBuffer rBuf = new ReadBuffer(rData);
-            packet = tpktPacketIO.parse(rBuf);
-        }
-        long endParsing = System.currentTimeMillis();
 
-        System.out.println("Parsed " + numRunsParse + " packets in " + (endParsing - start) + "ms");
-        System.out.println("That's " + ((float) (endParsing - start) / numRunsParse) + "ms per packet");
+//         neu
 
-        // Benchmark the serializing code
-        int numRunsSerialize = 2000000;
-        byte[] oData = null;
-        for(int i = 0; i < numRunsSerialize; i++) {
-            WriteBuffer wBuf = new WriteBuffer(packet.getLengthInBytes());
-            tpktPacketIO.serialize(wBuf, packet);
-            oData = wBuf.getData();
-        }
-        long endSerializing = System.currentTimeMillis();
+//        int[] bccmsg = {0x08, 0x09, 0x06, 0x10, 0x10, 0x04, 0x03};
+//        int c_BCC = CalcBCC(bccmsg);
+//        System.out.println("hex: " + Integer.toHexString(c_BCC));
+//        int[] bccmsg2 = {0x08, 0x09, 0x06, 0x02, 0x04, 0x03};
+//        int c_BCC2 = CalcBCC(bccmsg2);
+//        System.out.println("hex: " + Integer.toHexString(c_BCC2));
+//
+//        System.exit(0);
 
-        System.out.println("Serialized " + numRunsSerialize + " packets in " + (endSerializing - endParsing) + "ms");
-        System.out.println("That's " + ((float) (endSerializing - endParsing) / numRunsSerialize) + "ms per packet");
-        if(!Arrays.equals(rData, oData)) {
-            for(int i = 0; i < rData.length; i++) {
-                if(rData[i] != oData[i]) {
-                    System.out.println("Difference in byte " + i);
-                }
+
+        SerialPort comPort = SerialPort.getCommPorts()[0];
+        comPort.openPort();
+
+        comPort.setComPortParameters(19200, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
+//        comPort.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0);
+
+        System.out.print(comPort.getSystemPortName() + " | ");
+        System.out.print(comPort.getPortDescription() + " | ");
+        System.out.print(comPort.getDescriptivePortName() + " | Baud rate: ");
+        System.out.print(comPort.getBaudRate());
+
+//        System.out.println(comPort.getReadTimeout());
+//        System.out.println(comPort.getWriteTimeout());
+
+
+        byte[] c_STX = {0x02};
+        byte[] c_SOH = {0x01};
+        byte[] c_ETX = {0x03};
+        byte[] c_EOT = {0x04};
+        byte[] c_ENQ = {0x05};
+        byte[] c_ACK = {0x06};
+        byte[] c_DLE = {0x10};
+        byte[] c_NAK = {0x0f};
+
+        byte[] c_DST = {0x09};
+        byte[] c_SRC = {0x00};
+        byte[] c_CMD = {0x01};
+        byte[] c_STS = {0x00};
+        byte[] c_TNS = {0x01, 0x00};
+        byte[] c_ADR = {0x11, 0x00};
+        byte[] c_SZE = {0x02};
+//        byte[] c_BCC = {-30};
+
+//        byte[] c_STN = {0x11};
+//        byte[] c_DST = {0x01};
+//        byte[] c_SRC = {0x00};
+//        byte[] c_SZE = {0x0c};
+//        byte[] c_TNS = {0x41, 0x00};
+//        byte[] c_ADR = {0x12, 0x00};
+//        byte[] c_CRC = {-49, 0x40};
+
+
+//        byte[] message = {0x10, 0x01, 0x11, 0x10, 0x02, 0x09, 0x00, 0x01, 0x00, 0x41, 0x00, 0x12, 0x00, 0x0c, 0x10, 0x03}; // halfduplex msg
+//        comPort.writeBytes(message, message.length);
+
+//        byte[] message = {0x10, 0x05, 0x11, -17};         // halfduplex poll
+//        comPort.writeBytes(message, message.length);
+
+
+        comPort.writeBytes(c_DLE, 1);     // fullduplex msg Seite 235
+        comPort.writeBytes(c_STX, 1);
+        comPort.writeBytes(c_DST, 1);
+        comPort.writeBytes(c_SRC, 1);
+        comPort.writeBytes(c_CMD, 1);
+        comPort.writeBytes(c_STS, 1);
+        comPort.writeBytes(c_TNS, 2);
+        comPort.writeBytes(c_ADR, 2);
+        comPort.writeBytes(c_SZE, 1);
+        comPort.writeBytes(c_DLE, 1);
+        comPort.writeBytes(c_ETX, 1);
+
+
+        int[] crcmsg = {c_DST[0], c_SRC[0], c_CMD[0], c_STS[0], c_TNS[0], c_TNS[1], c_ADR[0], c_ADR[1], c_SZE[0], c_ETX[0]}; // fullduplex CRC
+//        int[] crcmsg = {0x11, 0x02, 0x09, 0x00, 0x01, 0x00, 0x41, 0x00, 0x12, 0x00, 0x0c, 0x03}; // halfduplex CRC
+
+        int[] c_CRC = CalcCRC(crcmsg);
+        byte[] crc1 = {(byte)c_CRC[0]};
+        byte[] crc2 = {(byte)c_CRC[1]};
+        comPort.writeBytes(crc1, 1);
+        comPort.writeBytes(crc2, 1);
+
+
+        while (comPort.bytesAvailable() == 0) {
+            Thread.sleep(22); }
+
+        byte[] readBuffer = new byte[comPort.bytesAvailable()];
+        int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+        System.out.println("Read " + numRead + " bytes.");
+
+        for (byte c_RCV : readBuffer) {
+            System.out.print(Integer.toHexString(c_RCV) + " | "); }
+
+        if (numRead > 1) {
+            if (readBuffer[1] != 0x15) {
+                comPort.writeBytes(c_DLE, 1);
+                comPort.writeBytes(c_ACK, 1);
             }
-            System.out.println("Not equals");
-        } else {
-            System.out.println("Bytes equal");
         }
+
+        comPort.closePort();
+
     }
 
+        private static int[] CalcCRC(int[] crcBytes) {
+            int tmp = 0;
+            int crcL, crcR;
+
+            for (int newByte : crcBytes ) {
+                crcL = tmp >> 8;
+                crcR = tmp & 0xFF;
+                tmp = (crcL << 8) + (newByte ^ crcR);
+                for (int j=0; j<8; j++)
+                    if (tmp % 2 == 1) {     // check if LSB shifted out is 1 or 0
+                        tmp = tmp >> 1;
+                        tmp = tmp ^ 0xA001;
+                    } else {
+                        tmp = tmp >> 1;
+                    }
+            }
+
+//            return ((tmp & 0xFF) << 8) + (tmp >> 8);  // returns lowbyte|highbyte as one number, change return to non-array
+
+            int[] tmparray = {(tmp & 0xFF), (tmp >> 8)};
+            return tmparray;
+        }
+
+        private static int CalcBCC(int[] crcBytes) {
+            int tmp = 0;
+            int j = 0;
+
+            for (int newByte : crcBytes) {
+                tmp = tmp + newByte;
+                if (newByte == 0x10) {
+                    j = ++j; }
+            }
+            tmp = tmp - ((j/2) * 0x10);  // get rid of double DLE
+            return ((~tmp) & 0b11111111) + 1;
+        }
 }
