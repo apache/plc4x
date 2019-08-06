40dbc1bf73d239a6ab9d41e9ae75b5304b80b9ac
Christofer Dutz <christofer.dutz@c-ware.de>
2019-08-06 17:03:35 +0200

    - Tweaked the mspec for the DF1 protocol - Started implementing a PLC4X driver based on the generated code

---
+++ sandbox/test-java-df1-driver/src/main/java/org/apache/plc4x/java/df1/fields/DataType.java
@@ -0,0 +1,17 @@
+package org.apache.plc4x.java.df1.fields;
+
+public enum DataType {
+
+    BIT,
+    BIT_STRING,
+    BYTE_STRING,
+    INTEGER,
+    TIMER,
+    COUNTER,
+    GENERAL_COUNT_STRUCTURE,
+    FLOAT,
+    ARRAY,
+    ADDRESS,
+    BINARY_CODED_DECIMAL
+
+}
--- sandbox/test-java-df1-driver/src/main/java/org/apache/plc4x/protocol/df1/DF1Utils.java
+++ sandbox/test-java-df1-driver/src/main/java/org/apache/plc4x/protocol/df1/DF1Utils.java
@@ -16,13 +16,69 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-
 package org.apache.plc4x.protocol.df1;
+
+import org.apache.plc4x.java.df1.DF1ReadRequest;
+import org.apache.plc4x.java.df1.DF1Symbol;
+import org.apache.plc4x.java.df1.DF1SymbolMessageFrameStart;
+import org.apache.plc4x.java.utils.ParseException;
+import org.apache.plc4x.java.utils.WriteBuffer;
+
+import java.nio.ByteBuffer;
 
 public class DF1Utils {
 
     public static short CRCCheck(Object... args) {
+        DF1Symbol symbol = (DF1Symbol) args[1];
+        short messageType = (short) args[0];
+        if(symbol instanceof DF1SymbolMessageFrameStart) {
+            DF1SymbolMessageFrameStart messageFrameStart = (DF1SymbolMessageFrameStart) symbol;
+
+            short destinationAddress = messageFrameStart.getDestinationAddress();
+            short sourceAddress = messageFrameStart.getSourceAddress();
+            short commandDiscriminatorValues = (short) messageFrameStart.getCommand().getDiscriminatorValues()[0];
+            short status = messageFrameStart.getCommand().getStatus();
+            int   counter = messageFrameStart.getCommand().getTransactionCounter();
+            if(messageFrameStart.getCommand() instanceof DF1ReadRequest) {
+                DF1ReadRequest readRequestCommand = (DF1ReadRequest) messageFrameStart.getCommand();
+
+                try {
+                    WriteBuffer writeBuffer = new WriteBuffer(10, false);
+                    writeBuffer.writeUnsignedShort(8, destinationAddress);
+                    writeBuffer.writeUnsignedShort(8, sourceAddress);
+                    writeBuffer.writeUnsignedShort(8, commandDiscriminatorValues);
+                    writeBuffer.writeUnsignedShort(8, status);
+                    writeBuffer.writeUnsignedInt(16, (short) counter);
+                    writeBuffer.writeUnsignedInt(16, (short) readRequestCommand.getAddress());
+                    writeBuffer.writeUnsignedShort(8, (byte) readRequestCommand.getSize());
+                    writeBuffer.writeUnsignedShort(8, (byte) messageType);
+
+                    byte[] data = writeBuffer.getData();
+
+                    int tmp = 0;
+                    int crcL, crcR;
+
+                    for (int newByte : data) {
+                        crcL = tmp >> 8;
+                        crcR = tmp & 0xFF;
+                        tmp = (crcL << 8) + (newByte ^ crcR);
+                        for (int j=0; j<8; j++)
+                            if (tmp % 2 == 1) {     // check if LSB shifted out is 1 or 0
+                                tmp = tmp >> 1;
+                                tmp = tmp ^ 0xA001;
+                            } else {
+                                tmp = tmp >> 1;
+                            }
+                    }
+
+                    return (short) tmp;
+                } catch (ParseException e) {
+                    throw new RuntimeException("Something wen't wrong during the CRC check", e);
+                }
+            }
+        }
+
         return 0;
     }
+
 }
