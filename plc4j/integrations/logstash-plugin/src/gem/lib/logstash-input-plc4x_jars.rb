da4b3df7dead5397f51b5eee0d53d8c761ce9136
Till Voss <till.voss@codecentric.de>
2019-08-21 18:07:44 +0200

    version with ${current-full-version}

--- plc4j/integrations/logstash-plugin/src/gem/lib/logstash-input-plc4x_jars.rb
+++ plc4j/integrations/logstash-plugin/src/gem/lib/logstash-input-plc4x_jars.rb
@@ -19,4 +19,4 @@
 
 require 'jar_dependencies'
 # TODO replace 0.5.0 with version
-require_jar('${project.groupId}', 'logstash-input-plc4x', '0.5.0')
+require_jar('${project.groupId}', 'logstash-input-plc4x', '${current-full-version}')
