68476f402648c5ab16e6423cbeba85d8bf142659
Julian Feinauer <j.feinauer@pragmaticminds.de>
2019-10-15 21:35:46 +0200

    [OSGi] Added initial Implementation of a Exam Test.

--- plc4j/karaf-features/karaf-itest/src/test/java/org/apache/plc4x/karaf/FeatureITest.java
+++ plc4j/karaf-features/karaf-itest/src/test/java/org/apache/plc4x/karaf/FeatureITest.java
@@ -13,88 +13,119 @@
  */
 package org.apache.plc4x.karaf;
 
-import org.apache.karaf.itests.KarafTestSupport;
+import org.apache.karaf.bundle.core.BundleService;
+import org.apache.karaf.features.FeaturesService;
+import org.apache.plc4x.java.spi.PlcDriver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.ConfigurationManager;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.karaf.options.KarafDistributionOption;
-import org.ops4j.pax.exam.karaf.options.LogLevelOption;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
-import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
-import org.ops4j.pax.exam.spi.reactors.PerMethod;
+import org.ops4j.pax.exam.options.MavenUrlReference;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.stream.Stream;
+import javax.inject.Inject;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configureConsole;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
 
 @RunWith(PaxExam.class)
-@ExamReactorStrategy(PerMethod.class)
-public class FeatureITest extends KarafTestSupport {
+public class FeatureITest {
+
+    private static Logger LOG = LoggerFactory.getLogger(FeatureITest.class);
+
+    @Inject
+    private FeaturesService featuresService;
+
+    @Inject
+    private BundleContext bundleContext;
 
     @Configuration
     public Option[] config() {
-        Option[] options = new Option[]{
-            logLevel(LogLevelOption.LogLevel.DEBUG),
-            KarafDistributionOption.editConfigurationFilePut("etc/system.properties", "plc4x.version", System.getProperty("plc4x.version")),
-        };
-        return Stream.of(super.config(), options).flatMap(Stream::of).toArray(Option[]::new);
-    }
+        MavenArtifactUrlReference karafUrl = maven()
+            .groupId("org.apache.karaf")
+            .artifactId("apache-karaf")
+            .version(karafVersion())
+            .type("zip");
 
-    /**
-     * Checks:
-     * - the feature can be installed
-     * - the Driver Bundle is there and active
-     * - the Bundle contains the DS for PlcDriver
-     * - the feature is installed
-     */
-    @Test
-    public void installAndAssertFeature() throws Exception {
-        // Assemble Maven URL
-        final MavenArtifactUrlReference featureRepo = maven()
+        MavenUrlReference karafStandardRepo = maven()
+            .groupId("org.apache.karaf.features")
+            .artifactId("standard")
+            .version(karafVersion())
+            .classifier("features")
+            .type("xml");
+
+        final MavenArtifactUrlReference plc4xRepo = maven()
             .groupId("org.apache.plc4x")
             .artifactId("driver-s7-feature")
-            .version(System.getProperty("plc4x.version"))
-            .type("xml")
-            .classifier("features");
-        System.out.println("Installing feature repo " + featureRepo.getURL());
+            .version("0.5.0-SNAPSHOT")
+            .classifier("features")
+            .type("xml");
+        return new Option[] {
+            // KarafDistributionOption.debugConfiguration("5005", true),
+            karafDistributionConfiguration()
+                .frameworkUrl(karafUrl)
+                .unpackDirectory(new File("target", "exam"))
+                .useDeployFolder(false),
+            keepRuntimeFolder(),
+            configureConsole().ignoreLocalConsole(),
+            features(karafStandardRepo , "scr"),
+            features(plc4xRepo, "driver-s7-feature"),
+            mavenBundle()
+                .groupId("org.apache.plc4x")
+                .artifactId("plc4j-driver-s7")
+                .version("0.5.0-SNAPSHOT")
+                .start()
+        };
+    }
 
-        // Install the feature-repo
-        addFeaturesRepository(featureRepo.getURL());
+    public static String karafVersion() {
+        ConfigurationManager cm = new ConfigurationManager();
+        String karafVersion = cm.getProperty("pax.exam.karaf.version", "3.0.0");
+        return karafVersion;
+    }
 
-        // Install the feature
-        installAndAssertFeature("driver-s7-feature");
 
-        // Print Bundles and fetch result
-        String bundles = executeCommand("bundle:list -t 0");
+    @Test
+    public void checkFeatureInstalled() throws Exception {
+        assertTrue(
+            featuresService.isInstalled(featuresService.getFeature("driver-s7-feature"))
+        );
+    }
 
-        // Find that line
-        // 84 │ Active │  80 │ 0.5.0.SNAPSHOT │ PLC4J: Driver: S7
-        assertContains("PLC4J: Driver: S7", bundles);
-        System.out.println(bundles);
+    @Test
+    public void checkBundleStarted() throws Exception {
+        for (Bundle bundle : bundleContext.getBundles()) {
+            System.out.println(bundle.getSymbolicName());
+        }
 
-        // Find Bundle for more detailed check
-        final Bundle bundle = findBundleByName("org.apache.plc4x.plc4j-driver-s7");
+        // Try to find the bundle
+        final Optional<Bundle> optionalBundle = Arrays.stream(bundleContext.getBundles())
+            .filter(bundle -> "org.apache.plc4x.plc4j-driver-s7".equals(bundle.getSymbolicName()))
+            .findFirst();
 
-        // Bundle has to be ACTIVE
-        assertNotNull(bundle);
-        assertEquals(Bundle.ACTIVE, bundle.getState());
+        // Ensure that the bundle is resolved
+        assertTrue(optionalBundle.isPresent());
 
-        // Check declarative service is present
-        String services = executeCommand("services -p " + bundle.getBundleId());
-        System.out.println("Services: " + services);
-        assertContains("component.name = org.apache.plc4x.java.s7.S7PlcDriver", services);
-        assertContains("objectClass = [org.apache.plc4x.java.spi.PlcDriver]", services);
-
-        // Just for Debugging...
-        String features = executeCommand("feature:list -i");
-        System.out.print(features);
-        assertContains("driver-s7-feature", features);
+        // Check if the bundle is active
+        assertEquals(Bundle.ACTIVE, optionalBundle.get().getState());
     }
 
 }
