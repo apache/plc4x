b289fb9bc2bbc9dcac9f7a6d652196419ef61838
Julian Feinauer <j.feinauer@pragmaticminds.de>
2019-10-17 21:58:28 +0200

    [OSGi] Refactoring.

--- plc4j/karaf-features/karaf-itest/src/test/java/org/apache/plc4x/karaf/FeatureITest.java
+++ plc4j/karaf-features/karaf-itest/src/test/java/org/apache/plc4x/karaf/FeatureITest.java
@@ -31,8 +31,6 @@ import java.util.stream.Stream;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
 
 @RunWith(PaxExam.class)
@@ -44,88 +42,8 @@ public class FeatureITest extends KarafTestSupport {
         Option[] options = new Option[]{
             logLevel(LogLevelOption.LogLevel.DEBUG),
             KarafDistributionOption.editConfigurationFilePut("etc/system.properties", "plc4x.version", System.getProperty("plc4x.version")),
-//            features("scr"),
-//            mavenBundle()
-//                .groupId("org.ops4j.pax.exam.samples")
-//                .artifactId("pax-exam-sample8-ds")
-//                .version("4.13.1")
-//                .start()
         };
         return Stream.of(super.config(), options).flatMap(Stream::of).toArray(Option[]::new);
-
-//
-//        String httpPort = Integer.toString(getAvailablePort(Integer.parseInt(MIN_HTTP_PORT), Integer.parseInt(MAX_HTTP_PORT)));
-//        String rmiRegistryPort = Integer.toString(getAvailablePort(Integer.parseInt(MIN_RMI_REG_PORT), Integer.parseInt(MAX_RMI_REG_PORT)));
-//        String rmiServerPort = Integer.toString(getAvailablePort(Integer.parseInt(MIN_RMI_SERVER_PORT), Integer.parseInt(MAX_RMI_SERVER_PORT)));
-//        String sshPort = Integer.toString(getAvailablePort(Integer.parseInt(MIN_SSH_PORT), Integer.parseInt(MAX_SSH_PORT)));
-//        String localRepository = System.getProperty("org.ops4j.pax.url.mvn.localRepository");
-//        if (localRepository == null) {
-//            localRepository = "";
-//        }
-//
-//        if (JavaVersionUtil.getMajorVersion() >= 9) {
-//            return new Option[]{
-//                //KarafDistributionOption.debugConfiguration("8889", true),
-//                karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf").unpackDirectory(new File("target/exam")),
-//                // enable JMX RBAC security, thanks to the KarafMBeanServerBuilder
-//                configureSecurity().disableKarafMBeanServerBuilder(),
-//                // configureConsole().ignoreLocalConsole(),
-//                keepRuntimeFolder(),
-//                logLevel(LogLevelOption.LogLevel.INFO),
-//                mavenBundle().groupId("org.awaitility").artifactId("awaitility").versionAsInProject(),
-//                mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.hamcrest").versionAsInProject(),
-//                mavenBundle().groupId("org.apache.karaf.itests").artifactId("common").versionAsInProject(),
-//                editConfigurationFilePut("etc/org.ops4j.pax.web.cfg", "org.osgi.service.http.port", httpPort),
-//                editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiRegistryPort", rmiRegistryPort),
-//                editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiServerPort", rmiServerPort),
-//                editConfigurationFilePut("etc/org.apache.karaf.shell.cfg", "sshPort", sshPort),
-//                editConfigurationFilePut("etc/org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.localRepository", localRepository),
-//                new VMOption("--add-reads=java.xml=java.logging"),
-//                new VMOption("--add-exports=java.base/org.apache.karaf.specs.locator=java.xml,ALL-UNNAMED"),
-//                new VMOption("--patch-module"),
-//                new VMOption("java.base=lib/endorsed/org.apache.karaf.specs.locator-"
-//                    + System.getProperty("karaf.version") + ".jar"),
-//                new VMOption("--patch-module"),
-//                new VMOption("java.xml=lib/endorsed/org.apache.karaf.specs.java.xml-"
-//                    + System.getProperty("karaf.version") + ".jar"),
-//                new VMOption("--add-opens"),
-//                new VMOption("java.base/java.security=ALL-UNNAMED"),
-//                new VMOption("--add-opens"),
-//                new VMOption("java.base/java.net=ALL-UNNAMED"),
-//                new VMOption("--add-opens"),
-//                new VMOption("java.base/java.lang=ALL-UNNAMED"),
-//                new VMOption("--add-opens"),
-//                new VMOption("java.base/java.util=ALL-UNNAMED"),
-//                new VMOption("--add-opens"),
-//                new VMOption("java.naming/javax.naming.spi=ALL-UNNAMED"),
-//                new VMOption("--add-opens"),
-//                new VMOption("java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED"),
-//                new VMOption("--add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED"),
-//                new VMOption("--add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED"),
-//                new VMOption("--add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED"),
-//                new VMOption("--add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED"),
-//                new VMOption("-classpath"),
-//                new VMOption("lib/jdk9plus/*" + File.pathSeparator + "lib/boot/*")
-//            };
-//        } else {
-//            return new Option[]{
-//                //KarafDistributionOption.debugConfiguration("8889", true),
-//                karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf").unpackDirectory(new File("target/exam")),
-//                // enable JMX RBAC security, thanks to the KarafMBeanServerBuilder
-//                configureSecurity().disableKarafMBeanServerBuilder(),
-//                // configureConsole().ignoreLocalConsole(),
-//                keepRuntimeFolder(),
-//                logLevel(LogLevelOption.LogLevel.INFO),
-//                mavenBundle().groupId("org.awaitility").artifactId("awaitility").versionAsInProject(),
-//                mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.hamcrest").versionAsInProject(),
-//                mavenBundle().groupId("org.apache.karaf.itests").artifactId("common").versionAsInProject(),
-//                editConfigurationFilePut("etc/org.ops4j.pax.web.cfg", "org.osgi.service.http.port", httpPort),
-//                editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiRegistryPort", rmiRegistryPort),
-//                editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiServerPort", rmiServerPort),
-//                editConfigurationFilePut("etc/org.apache.karaf.shell.cfg", "sshPort", sshPort),
-//                editConfigurationFilePut("etc/org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.localRepository", localRepository)
-//            };
-//        }
     }
 
     /**
