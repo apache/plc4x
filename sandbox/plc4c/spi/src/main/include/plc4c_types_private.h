b1608ce1558c45ec4b7a000e9ee7907fdc8433e3
Otto Fowler <ottobackwards@gmail.com>
2020-04-20 09:34:10 -0400

    move structure defs into private header

---
+++ sandbox/plc4c/spi/src/main/include/plc4c_types_private.h
@@ -0,0 +1,27 @@
+//
+// Created by Otto Fowler on 4/20/20.
+//
+
+#ifndef PLC4C_SPI_SRC_MAIN_INCLUDE_PLC4C_TYPES_PRIVATE_H_
+#define PLC4C_SPI_SRC_MAIN_INCLUDE_PLC4C_TYPES_PRIVATE_H_
+
+struct plc4c_system_t {
+  /* drivers */
+
+  /* connections */
+
+  /* callbacks */
+};
+
+struct plc4c_driver_t {
+  /* name */
+
+  /* ??? */
+};
+
+struct plc4c_connection_t {
+  char* connection_string;
+  /* ???? */
+};
+
+#endif //PLC4C_SPI_SRC_MAIN_INCLUDE_PLC4C_TYPES_PRIVATE_H_
