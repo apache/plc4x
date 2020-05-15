d00e452bc573eec9cc213df413b3950d438ed150
Christofer Dutz <christofer.dutz@c-ware.de>
2020-05-15 16:51:29 +0200

    - Applied a fix for some tests.

--- plc4j/integrations/apache-camel/src/test/java/org/apache/plc4x/camel/Plc4XProducerTest.java
+++ plc4j/integrations/apache-camel/src/test/java/org/apache/plc4x/camel/Plc4XProducerTest.java
@@ -27,6 +27,9 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.mockito.Mockito.*;
@@ -51,11 +54,12 @@ public class Plc4XProducerTest {
         when(endpointMock.getConnection()).thenReturn(mockConnection);
         SUT = new Plc4XProducer(endpointMock);
         testExchange = mock(Exchange.class, RETURNS_DEEP_STUBS);
-
+        Map<String, Map<String,Object>> tags = new HashMap();
+        tags.put("test1", Collections.singletonMap("testAddress1",0));
+        tags.put("test1", Collections.singletonMap("testAddress2",true));
+        tags.put("test1", Collections.singletonMap("testAddress3","TestString"));
         when(testExchange.getIn().getBody())
-            .thenReturn(Arrays.asList(new TagData("testName","testAddress",0),
-                new TagData("testName2","testAddress2",true),
-                new TagData("testName3","testAddress3","testVal")));
+            .thenReturn(tags);
     }
 
     @Test
