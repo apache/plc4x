089e0cbc137ec31c9102712eca8ce25b9f40cc3d
Christofer Dutz <christofer.dutz@c-ware.de>
2020-12-03 14:52:08 +0100

    - Finish implementing the WriteBuffer

--- sandbox/plc4net/spi/spi/generation/WriteBuffer.cs
+++ sandbox/plc4net/spi/spi/generation/WriteBuffer.cs
@@ -17,72 +17,161 @@
 // under the License.
 //
 
+using System;
+using Ayx.BitIO;
+
 namespace org.apache.plc4net.spi.generation
 {
     public class WriteBuffer
     {
-        
+        private readonly BitWriter _writer;
+
+        public WriteBuffer()
+        {
+            _writer = new BitWriter();
+        }
+
         public int GetPos()
         {
-            return 0;
+            return _writer.Length;
         }
 
         public byte[] GetBytes()
         {
-            return null;
+            return _writer.GetBytes();
         }
     
         public int GetTotalBytes() 
         {
-            return 0;
+            return _writer.Length / 8 + (_writer.Length % 8 != 0 ? 1 : 0);
         }
 
         public void WriteBit(bool value)
         {
+            _writer.WriteBool(value);
         }
 
         public void WriteByte(int bitLength, byte value)
         {
+            if (bitLength > 8)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _writer.WriteByte(value, bitLength);
         }
         
         public void WriteUshort(int bitLength, ushort value)
         {
+            if (bitLength > 16)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _writer.WriteInt(value, bitLength);
         }
 
         public void WriteUint(int bitLength, uint value)
         {
+            if (bitLength > 32)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _writer.WriteInt((int) value, bitLength);
         }
 
         public void WriteUlong(int bitLength, ulong value)
         {
+            if (bitLength > 64)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            var highInt = (int) ((value & 0xFFFFFFFF00000000) >> 32);
+            var lowInt = (int) (value & 0xFFFFFFFF);
+            if (bitLength > 32)
+            {
+                _writer.WriteInt(highInt, bitLength - 32);
+                _writer.WriteInt(lowInt, 32);
+            }
+            else
+            {
+                _writer.WriteInt(lowInt, bitLength);
+            }
         }
 
         public void WriteSbyte(int bitLength, sbyte value)
         {
+            if (bitLength > 8)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _writer.WriteInt(value, bitLength);
         }
         
         public void WriteShort(int bitLength, short value)
         {
+            if (bitLength > 16)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _writer.WriteInt(value, bitLength);
         }
 
         public void WriteInt(int bitLength, int value)
         {
+            if (bitLength > 32)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            _writer.WriteInt(value, bitLength);
         }
 
         public void WriteLong(int bitLength, long value)
         {
+            if (bitLength > 64)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            WriteUlong(bitLength, (ulong) value);
         }
 
         public void WriteFloat(int bitLength, float value)
         {
+            if (bitLength == 32)
+            {
+                var bytes = BitConverter.GetBytes(value);
+                for (var i = 0; i < bytes.Length; i++)
+                {
+                    _writer.WriteByte(8, bytes[i]);
+                }
+            }
+            else if (bitLength == 16)
+            {
+                
+            }
+            else
+            {
+                throw new NotImplementedException("This encoding is currently not supported");
+            }
         }
 
         public void WriteDouble(int bitLength, double value)
         {
+            if (bitLength == 64)
+            {
+                var bytes = BitConverter.GetBytes(value);
+                for (var i = 0; i < bytes.Length; i++)
+                {
+                    _writer.WriteByte(8, bytes[i]);
+                }
+            }
+            else
+            {
+                throw new NotImplementedException("This encoding is currently not supported");
+            }
         }
 
         public void WriteString(int bitLength, string encoding, string value)
         {
+            throw new NotImplementedException("This encoding is currently not supported");
         }
 
     }
