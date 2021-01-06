392c2b59f9584a644d3c7171be85f3c119656056
cdutz <christofer.dutz@c-ware.de>
2021-01-06 19:30:50 +0100

    - Updated the XSLT to create more detailed Enums out of the KNX master data.

---
+++ plc4go/internal/plc4go/knxnetip/readwrite/model/KnxPropertyDataType.go
@@ -0,0 +1,525 @@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+package model
+
+import (
+	"github.com/apache/plc4x/plc4go/internal/plc4go/spi/utils"
+)
+
+type KnxPropertyDataType uint8
+
+type IKnxPropertyDataType interface {
+	SizeInBytes() uint8
+	Serialize(io utils.WriteBuffer) error
+}
+
+const (
+	KnxPropertyDataType_PDT_CONTROL             KnxPropertyDataType = 0
+	KnxPropertyDataType_PDT_CHAR                KnxPropertyDataType = 1
+	KnxPropertyDataType_PDT_UNSIGNED_CHAR       KnxPropertyDataType = 2
+	KnxPropertyDataType_PDT_INT                 KnxPropertyDataType = 3
+	KnxPropertyDataType_PDT_UNSIGNED_INT        KnxPropertyDataType = 4
+	KnxPropertyDataType_PDT_KNX_FLOAT           KnxPropertyDataType = 5
+	KnxPropertyDataType_PDT_DATE                KnxPropertyDataType = 6
+	KnxPropertyDataType_PDT_TIME                KnxPropertyDataType = 7
+	KnxPropertyDataType_PDT_LONG                KnxPropertyDataType = 8
+	KnxPropertyDataType_PDT_UNSIGNED_LONG       KnxPropertyDataType = 9
+	KnxPropertyDataType_PDT_FLOAT               KnxPropertyDataType = 10
+	KnxPropertyDataType_PDT_DOUBLE              KnxPropertyDataType = 11
+	KnxPropertyDataType_PDT_CHAR_BLOCK          KnxPropertyDataType = 12
+	KnxPropertyDataType_PDT_POLL_GROUP_SETTINGS KnxPropertyDataType = 13
+	KnxPropertyDataType_PDT_SHORT_CHAR_BLOCK    KnxPropertyDataType = 14
+	KnxPropertyDataType_PDT_DATE_TIME           KnxPropertyDataType = 15
+	KnxPropertyDataType_PDT_VARIABLE_LENGTH     KnxPropertyDataType = 16
+	KnxPropertyDataType_PDT_GENERIC_01          KnxPropertyDataType = 17
+	KnxPropertyDataType_PDT_GENERIC_02          KnxPropertyDataType = 18
+	KnxPropertyDataType_PDT_GENERIC_03          KnxPropertyDataType = 19
+	KnxPropertyDataType_PDT_GENERIC_04          KnxPropertyDataType = 20
+	KnxPropertyDataType_PDT_GENERIC_05          KnxPropertyDataType = 21
+	KnxPropertyDataType_PDT_GENERIC_06          KnxPropertyDataType = 22
+	KnxPropertyDataType_PDT_GENERIC_07          KnxPropertyDataType = 23
+	KnxPropertyDataType_PDT_GENERIC_08          KnxPropertyDataType = 24
+	KnxPropertyDataType_PDT_GENERIC_09          KnxPropertyDataType = 25
+	KnxPropertyDataType_PDT_GENERIC_10          KnxPropertyDataType = 26
+	KnxPropertyDataType_PDT_GENERIC_11          KnxPropertyDataType = 27
+	KnxPropertyDataType_PDT_GENERIC_12          KnxPropertyDataType = 28
+	KnxPropertyDataType_PDT_GENERIC_13          KnxPropertyDataType = 29
+	KnxPropertyDataType_PDT_GENERIC_14          KnxPropertyDataType = 30
+	KnxPropertyDataType_PDT_GENERIC_15          KnxPropertyDataType = 31
+	KnxPropertyDataType_PDT_GENERIC_16          KnxPropertyDataType = 32
+	KnxPropertyDataType_PDT_GENERIC_17          KnxPropertyDataType = 33
+	KnxPropertyDataType_PDT_GENERIC_18          KnxPropertyDataType = 34
+	KnxPropertyDataType_PDT_GENERIC_19          KnxPropertyDataType = 35
+	KnxPropertyDataType_PDT_GENERIC_20          KnxPropertyDataType = 36
+	KnxPropertyDataType_PDT_UTF_8               KnxPropertyDataType = 47
+	KnxPropertyDataType_PDT_VERSION             KnxPropertyDataType = 48
+	KnxPropertyDataType_PDT_ALARM_INFO          KnxPropertyDataType = 49
+	KnxPropertyDataType_PDT_BINARY_INFORMATION  KnxPropertyDataType = 50
+	KnxPropertyDataType_PDT_BITSET8             KnxPropertyDataType = 51
+	KnxPropertyDataType_PDT_BITSET16            KnxPropertyDataType = 52
+	KnxPropertyDataType_PDT_ENUM8               KnxPropertyDataType = 53
+	KnxPropertyDataType_PDT_SCALING             KnxPropertyDataType = 54
+	KnxPropertyDataType_PDT_NE_VL               KnxPropertyDataType = 60
+	KnxPropertyDataType_PDT_NE_FL               KnxPropertyDataType = 61
+	KnxPropertyDataType_PDT_FUNCTION            KnxPropertyDataType = 62
+	KnxPropertyDataType_PDT_ESCAPE              KnxPropertyDataType = 63
+)
+
+func (e KnxPropertyDataType) SizeInBytes() uint8 {
+	switch e {
+	case 0:
+		{ /* '0' */
+			return 10
+		}
+	case 1:
+		{ /* '1' */
+			return 1
+		}
+	case 10:
+		{ /* '10' */
+			return 4
+		}
+	case 11:
+		{ /* '11' */
+			return 8
+		}
+	case 12:
+		{ /* '12' */
+			return 10
+		}
+	case 13:
+		{ /* '13' */
+			return 3
+		}
+	case 14:
+		{ /* '14' */
+			return 5
+		}
+	case 15:
+		{ /* '15' */
+			return 8
+		}
+	case 16:
+		{ /* '16' */
+			return 0
+		}
+	case 17:
+		{ /* '17' */
+			return 1
+		}
+	case 18:
+		{ /* '18' */
+			return 2
+		}
+	case 19:
+		{ /* '19' */
+			return 3
+		}
+	case 2:
+		{ /* '2' */
+			return 1
+		}
+	case 20:
+		{ /* '20' */
+			return 4
+		}
+	case 21:
+		{ /* '21' */
+			return 5
+		}
+	case 22:
+		{ /* '22' */
+			return 6
+		}
+	case 23:
+		{ /* '23' */
+			return 7
+		}
+	case 24:
+		{ /* '24' */
+			return 8
+		}
+	case 25:
+		{ /* '25' */
+			return 9
+		}
+	case 26:
+		{ /* '26' */
+			return 10
+		}
+	case 27:
+		{ /* '27' */
+			return 11
+		}
+	case 28:
+		{ /* '28' */
+			return 12
+		}
+	case 29:
+		{ /* '29' */
+			return 13
+		}
+	case 3:
+		{ /* '3' */
+			return 2
+		}
+	case 30:
+		{ /* '30' */
+			return 14
+		}
+	case 31:
+		{ /* '31' */
+			return 15
+		}
+	case 32:
+		{ /* '32' */
+			return 16
+		}
+	case 33:
+		{ /* '33' */
+			return 17
+		}
+	case 34:
+		{ /* '34' */
+			return 18
+		}
+	case 35:
+		{ /* '35' */
+			return 19
+		}
+	case 36:
+		{ /* '36' */
+			return 20
+		}
+	case 4:
+		{ /* '4' */
+			return 2
+		}
+	case 47:
+		{ /* '47' */
+			return 0
+		}
+	case 48:
+		{ /* '48' */
+			return 2
+		}
+	case 49:
+		{ /* '49' */
+			return 6
+		}
+	case 5:
+		{ /* '5' */
+			return 2
+		}
+	case 50:
+		{ /* '50' */
+			return 1
+		}
+	case 51:
+		{ /* '51' */
+			return 1
+		}
+	case 52:
+		{ /* '52' */
+			return 2
+		}
+	case 53:
+		{ /* '53' */
+			return 1
+		}
+	case 54:
+		{ /* '54' */
+			return 1
+		}
+	case 6:
+		{ /* '6' */
+			return 3
+		}
+	case 60:
+		{ /* '60' */
+			return 0
+		}
+	case 61:
+		{ /* '61' */
+			return 0
+		}
+	case 62:
+		{ /* '62' */
+			return 0
+		}
+	case 63:
+		{ /* '63' */
+			return 0
+		}
+	case 7:
+		{ /* '7' */
+			return 3
+		}
+	case 8:
+		{ /* '8' */
+			return 4
+		}
+	case 9:
+		{ /* '9' */
+			return 4
+		}
+	default:
+		{
+			return 0
+		}
+	}
+}
+func KnxPropertyDataTypeValueOf(value uint8) KnxPropertyDataType {
+	switch value {
+	case 0:
+		return KnxPropertyDataType_PDT_CONTROL
+	case 1:
+		return KnxPropertyDataType_PDT_CHAR
+	case 10:
+		return KnxPropertyDataType_PDT_FLOAT
+	case 11:
+		return KnxPropertyDataType_PDT_DOUBLE
+	case 12:
+		return KnxPropertyDataType_PDT_CHAR_BLOCK
+	case 13:
+		return KnxPropertyDataType_PDT_POLL_GROUP_SETTINGS
+	case 14:
+		return KnxPropertyDataType_PDT_SHORT_CHAR_BLOCK
+	case 15:
+		return KnxPropertyDataType_PDT_DATE_TIME
+	case 16:
+		return KnxPropertyDataType_PDT_VARIABLE_LENGTH
+	case 17:
+		return KnxPropertyDataType_PDT_GENERIC_01
+	case 18:
+		return KnxPropertyDataType_PDT_GENERIC_02
+	case 19:
+		return KnxPropertyDataType_PDT_GENERIC_03
+	case 2:
+		return KnxPropertyDataType_PDT_UNSIGNED_CHAR
+	case 20:
+		return KnxPropertyDataType_PDT_GENERIC_04
+	case 21:
+		return KnxPropertyDataType_PDT_GENERIC_05
+	case 22:
+		return KnxPropertyDataType_PDT_GENERIC_06
+	case 23:
+		return KnxPropertyDataType_PDT_GENERIC_07
+	case 24:
+		return KnxPropertyDataType_PDT_GENERIC_08
+	case 25:
+		return KnxPropertyDataType_PDT_GENERIC_09
+	case 26:
+		return KnxPropertyDataType_PDT_GENERIC_10
+	case 27:
+		return KnxPropertyDataType_PDT_GENERIC_11
+	case 28:
+		return KnxPropertyDataType_PDT_GENERIC_12
+	case 29:
+		return KnxPropertyDataType_PDT_GENERIC_13
+	case 3:
+		return KnxPropertyDataType_PDT_INT
+	case 30:
+		return KnxPropertyDataType_PDT_GENERIC_14
+	case 31:
+		return KnxPropertyDataType_PDT_GENERIC_15
+	case 32:
+		return KnxPropertyDataType_PDT_GENERIC_16
+	case 33:
+		return KnxPropertyDataType_PDT_GENERIC_17
+	case 34:
+		return KnxPropertyDataType_PDT_GENERIC_18
+	case 35:
+		return KnxPropertyDataType_PDT_GENERIC_19
+	case 36:
+		return KnxPropertyDataType_PDT_GENERIC_20
+	case 4:
+		return KnxPropertyDataType_PDT_UNSIGNED_INT
+	case 47:
+		return KnxPropertyDataType_PDT_UTF_8
+	case 48:
+		return KnxPropertyDataType_PDT_VERSION
+	case 49:
+		return KnxPropertyDataType_PDT_ALARM_INFO
+	case 5:
+		return KnxPropertyDataType_PDT_KNX_FLOAT
+	case 50:
+		return KnxPropertyDataType_PDT_BINARY_INFORMATION
+	case 51:
+		return KnxPropertyDataType_PDT_BITSET8
+	case 52:
+		return KnxPropertyDataType_PDT_BITSET16
+	case 53:
+		return KnxPropertyDataType_PDT_ENUM8
+	case 54:
+		return KnxPropertyDataType_PDT_SCALING
+	case 6:
+		return KnxPropertyDataType_PDT_DATE
+	case 60:
+		return KnxPropertyDataType_PDT_NE_VL
+	case 61:
+		return KnxPropertyDataType_PDT_NE_FL
+	case 62:
+		return KnxPropertyDataType_PDT_FUNCTION
+	case 63:
+		return KnxPropertyDataType_PDT_ESCAPE
+	case 7:
+		return KnxPropertyDataType_PDT_TIME
+	case 8:
+		return KnxPropertyDataType_PDT_LONG
+	case 9:
+		return KnxPropertyDataType_PDT_UNSIGNED_LONG
+	}
+	return 0
+}
+
+func CastKnxPropertyDataType(structType interface{}) KnxPropertyDataType {
+	castFunc := func(typ interface{}) KnxPropertyDataType {
+		if sKnxPropertyDataType, ok := typ.(KnxPropertyDataType); ok {
+			return sKnxPropertyDataType
+		}
+		return 0
+	}
+	return castFunc(structType)
+}
+
+func (m KnxPropertyDataType) LengthInBits() uint16 {
+	return 8
+}
+
+func (m KnxPropertyDataType) LengthInBytes() uint16 {
+	return m.LengthInBits() / 8
+}
+
+func KnxPropertyDataTypeParse(io *utils.ReadBuffer) (KnxPropertyDataType, error) {
+	val, err := io.ReadUint8(8)
+	if err != nil {
+		return 0, nil
+	}
+	return KnxPropertyDataTypeValueOf(val), nil
+}
+
+func (e KnxPropertyDataType) Serialize(io utils.WriteBuffer) error {
+	err := io.WriteUint8(8, uint8(e))
+	return err
+}
+
+func (e KnxPropertyDataType) String() string {
+	switch e {
+	case KnxPropertyDataType_PDT_CONTROL:
+		return "PDT_CONTROL"
+	case KnxPropertyDataType_PDT_CHAR:
+		return "PDT_CHAR"
+	case KnxPropertyDataType_PDT_FLOAT:
+		return "PDT_FLOAT"
+	case KnxPropertyDataType_PDT_DOUBLE:
+		return "PDT_DOUBLE"
+	case KnxPropertyDataType_PDT_CHAR_BLOCK:
+		return "PDT_CHAR_BLOCK"
+	case KnxPropertyDataType_PDT_POLL_GROUP_SETTINGS:
+		return "PDT_POLL_GROUP_SETTINGS"
+	case KnxPropertyDataType_PDT_SHORT_CHAR_BLOCK:
+		return "PDT_SHORT_CHAR_BLOCK"
+	case KnxPropertyDataType_PDT_DATE_TIME:
+		return "PDT_DATE_TIME"
+	case KnxPropertyDataType_PDT_VARIABLE_LENGTH:
+		return "PDT_VARIABLE_LENGTH"
+	case KnxPropertyDataType_PDT_GENERIC_01:
+		return "PDT_GENERIC_01"
+	case KnxPropertyDataType_PDT_GENERIC_02:
+		return "PDT_GENERIC_02"
+	case KnxPropertyDataType_PDT_GENERIC_03:
+		return "PDT_GENERIC_03"
+	case KnxPropertyDataType_PDT_UNSIGNED_CHAR:
+		return "PDT_UNSIGNED_CHAR"
+	case KnxPropertyDataType_PDT_GENERIC_04:
+		return "PDT_GENERIC_04"
+	case KnxPropertyDataType_PDT_GENERIC_05:
+		return "PDT_GENERIC_05"
+	case KnxPropertyDataType_PDT_GENERIC_06:
+		return "PDT_GENERIC_06"
+	case KnxPropertyDataType_PDT_GENERIC_07:
+		return "PDT_GENERIC_07"
+	case KnxPropertyDataType_PDT_GENERIC_08:
+		return "PDT_GENERIC_08"
+	case KnxPropertyDataType_PDT_GENERIC_09:
+		return "PDT_GENERIC_09"
+	case KnxPropertyDataType_PDT_GENERIC_10:
+		return "PDT_GENERIC_10"
+	case KnxPropertyDataType_PDT_GENERIC_11:
+		return "PDT_GENERIC_11"
+	case KnxPropertyDataType_PDT_GENERIC_12:
+		return "PDT_GENERIC_12"
+	case KnxPropertyDataType_PDT_GENERIC_13:
+		return "PDT_GENERIC_13"
+	case KnxPropertyDataType_PDT_INT:
+		return "PDT_INT"
+	case KnxPropertyDataType_PDT_GENERIC_14:
+		return "PDT_GENERIC_14"
+	case KnxPropertyDataType_PDT_GENERIC_15:
+		return "PDT_GENERIC_15"
+	case KnxPropertyDataType_PDT_GENERIC_16:
+		return "PDT_GENERIC_16"
+	case KnxPropertyDataType_PDT_GENERIC_17:
+		return "PDT_GENERIC_17"
+	case KnxPropertyDataType_PDT_GENERIC_18:
+		return "PDT_GENERIC_18"
+	case KnxPropertyDataType_PDT_GENERIC_19:
+		return "PDT_GENERIC_19"
+	case KnxPropertyDataType_PDT_GENERIC_20:
+		return "PDT_GENERIC_20"
+	case KnxPropertyDataType_PDT_UNSIGNED_INT:
+		return "PDT_UNSIGNED_INT"
+	case KnxPropertyDataType_PDT_UTF_8:
+		return "PDT_UTF_8"
+	case KnxPropertyDataType_PDT_VERSION:
+		return "PDT_VERSION"
+	case KnxPropertyDataType_PDT_ALARM_INFO:
+		return "PDT_ALARM_INFO"
+	case KnxPropertyDataType_PDT_KNX_FLOAT:
+		return "PDT_KNX_FLOAT"
+	case KnxPropertyDataType_PDT_BINARY_INFORMATION:
+		return "PDT_BINARY_INFORMATION"
+	case KnxPropertyDataType_PDT_BITSET8:
+		return "PDT_BITSET8"
+	case KnxPropertyDataType_PDT_BITSET16:
+		return "PDT_BITSET16"
+	case KnxPropertyDataType_PDT_ENUM8:
+		return "PDT_ENUM8"
+	case KnxPropertyDataType_PDT_SCALING:
+		return "PDT_SCALING"
+	case KnxPropertyDataType_PDT_DATE:
+		return "PDT_DATE"
+	case KnxPropertyDataType_PDT_NE_VL:
+		return "PDT_NE_VL"
+	case KnxPropertyDataType_PDT_NE_FL:
+		return "PDT_NE_FL"
+	case KnxPropertyDataType_PDT_FUNCTION:
+		return "PDT_FUNCTION"
+	case KnxPropertyDataType_PDT_ESCAPE:
+		return "PDT_ESCAPE"
+	case KnxPropertyDataType_PDT_TIME:
+		return "PDT_TIME"
+	case KnxPropertyDataType_PDT_LONG:
+		return "PDT_LONG"
+	case KnxPropertyDataType_PDT_UNSIGNED_LONG:
+		return "PDT_UNSIGNED_LONG"
+	}
+	return ""
+}
--- protocols/knxnetip/src/main/xslt/knx-types.xsl
+++ protocols/knxnetip/src/main/xslt/knx-types.xsl
@@ -17,8 +17,9 @@
   specific language governing permissions and limitations
   under the License.
 -->
-<xsl:stylesheet version="1.0"
+<xsl:stylesheet version="2.0"
                 xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns:fn="http://www.w3.org/2005/xpath-functions"
                 xmlns:knx="http://knx.org/xml/project/20">
 
     <xsl:output
@@ -46,98 +47,135 @@
 // specific language governing permissions and limitations
 // under the License.
 //
-
-[enum uint 16 'KnxObjectType' [string 'text']<xsl:apply-templates select="knx:KNX/knx:MasterData/knx:InterfaceObjectTypes/knx:InterfaceObjectType"/>
+[enum uint 16 'KnxInterfaceObjectType' [string 'text']
+    ['65535' OT_GENERAL ['"General Object"']]
+    <xsl:apply-templates select="knx:KNX/knx:MasterData/knx:InterfaceObjectTypes/knx:InterfaceObjectType"/>
 ]
 
-[enum uint 8 'KnxObjectProperties' [string 'name', string 'dataTypeId', string 'text']<xsl:apply-templates select="knx:KNX/knx:MasterData/knx:InterfaceObjectProperties/knx:InterfaceObjectProperty"/>
+[enum uint 32 'KnxInterfaceObjectProperty' [uint16 'propertyId', KnxInterfaceObjectType 'objectType', KnxPropertyDataType 'propertyDataType', string 'text']
+    <xsl:apply-templates select="knx:KNX/knx:MasterData/knx:InterfaceObjectProperties/knx:InterfaceObjectProperty"/>
 ]
 
-[enum uint 16 'KnxManufacturers' [string 'text']<xsl:apply-templates select="knx:KNX/knx:MasterData/knx:Manufacturers/knx:Manufacturer"/>
+[enum uint 8 'KnxPropertyDataType' [uint 8 'sizeInBytes']
+    <xsl:apply-templates select="knx:KNX/knx:MasterData/knx:PropertyDataTypes/knx:PropertyDataType"/>
+]
+
+[enum uint 16 'KnxDatapointType' [string 'text', uint 8 'sizeInBits']
+    <xsl:apply-templates select="knx:KNX/knx:MasterData/knx:DatapointTypes/knx:DatapointType"/>
+]
+
+[enum uint 32 'KnxDatapointSubtype' [KnxDatapointType 'datapointType', string 'text']
+    <xsl:apply-templates select="knx:KNX/knx:MasterData/knx:DatapointTypes/knx:DatapointType/knx:DatapointSubtypes/knx:DatapointSubtype"/>
+]
+
+[enum uint 16 'KnxManufacturer' [string 'text']
+    <xsl:apply-templates select="knx:KNX/knx:MasterData/knx:Manufacturers/knx:Manufacturer"/>
 ]
     </xsl:template>
 
     <xsl:template match="knx:InterfaceObjectType">
         <xsl:variable name="objectTypeId">
-            <xsl:call-template name="clean-id-string">
+            <xsl:call-template name="getIdFromText">
                 <xsl:with-param name="text" select="@Name"/>
             </xsl:call-template>
         </xsl:variable>['<xsl:value-of select="@Number"/>' <xsl:value-of select="$objectTypeId"/> ['"<xsl:value-of select="@Text"/>"']]
     </xsl:template>
 
     <xsl:template match="knx:InterfaceObjectProperty">
-        <xsl:variable name="dataTypeId">
+        <xsl:variable name="objectTypeNumber">
             <xsl:choose>
-                <xsl:when test="contains(@PDT, ' ')">
-                    <xsl:call-template name="clean-id-string">
-                        <xsl:with-param name="text" select="substring-before(@PDT, ' ')"/>
-                    </xsl:call-template>
-                </xsl:when>
-                <xsl:when test="string-length(@PDT) &gt; 0">
-                    <xsl:call-template name="clean-id-string">
-                        <xsl:with-param name="text" select="@PDT"/>
-                    </xsl:call-template>
-                </xsl:when>
-                <xsl:otherwise>null</xsl:otherwise>
+                <xsl:when test="fn:starts-with(@Id, 'PID-G')">65535</xsl:when>
+                <xsl:otherwise>
+                    <xsl:value-of select="fn:substring-before(fn:substring-after(@Id, 'PID-'), '-')"/>
+                </xsl:otherwise>
             </xsl:choose>
         </xsl:variable>
-        <xsl:variable name="interfaceObjectPropertyId">
-            <xsl:call-template name="clean-id-string">
-                <xsl:with-param name="text">
-                    <xsl:call-template name="string-replace-all">
-                        <xsl:with-param name="text" select="@Id"/>
-                        <xsl:with-param name="replace" select="'-'"/>
-                        <xsl:with-param name="with" select="'_'"/>
-                    </xsl:call-template>
-                </xsl:with-param>
+        <xsl:variable name="objectPropertyValue" select="format-number(fn:number(concat($objectTypeNumber, format-number(@Number, '000'))), '#####0')"/>
+        <xsl:variable name="objectPropertyId">
+            <xsl:variable name="objectTypeId" select="@ObjectType"/>
+            <xsl:choose>
+                <xsl:when test="fn:starts-with(@Id, 'PID-G')">PID_GENERAL_<xsl:value-of select="fn:substring-after(@Name, 'PID_')"/></xsl:when>
+                <xsl:otherwise>PID_<xsl:value-of select="fn:substring-after(//knx:InterfaceObjectType[@Id = $objectTypeId]/@Name, 'OT_')"/>_<xsl:value-of select="fn:replace(fn:substring-after(@Name, '_'), '%', 'PERCENT')"/><xsl:if test="@Id = 'PID-417-120'">_2</xsl:if></xsl:otherwise>
+            </xsl:choose>
+        </xsl:variable>
+        <xsl:variable name="objectType">
+            <xsl:variable name="objectTypeId" select="@ObjectType"/>
+            <xsl:choose>
+                <xsl:when test="@ObjectType">KnxInterfaceObjectType.<xsl:value-of select="//knx:InterfaceObjectType[@Id = $objectTypeId]/@Name"/></xsl:when>
+                <xsl:otherwise>KnxInterfaceObjectType.OT_GENERAL</xsl:otherwise>
+            </xsl:choose>
+        </xsl:variable>['<xsl:value-of select="$objectPropertyValue"/>' <xsl:value-of select="$objectPropertyId"/> ['<xsl:value-of select="@Number"/>', '<xsl:value-of select="$objectType"/>', 'KnxPropertyDataType.PDT_CONTROL', '"<xsl:value-of select="@Text"/>"']]
+    </xsl:template>
+
+    <xsl:template match="knx:PropertyDataType">
+        <xsl:variable name="sizeInBytes">
+            <xsl:choose>
+                <xsl:when test="@Size"><xsl:value-of select="@Size"/></xsl:when>
+                <xsl:otherwise>0</xsl:otherwise>
+            </xsl:choose>
+        </xsl:variable>['<xsl:value-of select="@Number"/>' <xsl:value-of select="fn:replace(@Name, '-', '_')"/> ['<xsl:value-of select="$sizeInBytes"/>']]
+    </xsl:template>
+
+    <xsl:template match="knx:DatapointType">
+        <xsl:variable name="datapointTypeId">
+            <xsl:call-template name="getDatapointTypeId">
+                <xsl:with-param name="contextNode" select="."/>
             </xsl:call-template>
-        </xsl:variable>['<xsl:value-of select="@Number"/>' <xsl:value-of select="$interfaceObjectPropertyId"/> ['<xsl:value-of select="@Name"/>', '<xsl:value-of select="$dataTypeId"/>', '"<xsl:value-of select="@Text"/>"']]
+        </xsl:variable>['<xsl:value-of select="@Number"/>' <xsl:value-of select="$datapointTypeId"/> ['"<xsl:value-of select="@Text"/>"', '<xsl:value-of select="@SizeInBit"/>']]
+    </xsl:template>
+
+    <xsl:template match="knx:DatapointSubtype">
+        <xsl:variable name="datapointSubtypeValue" select="concat(../../@Number, format-number(@Number, '0000'))"/>
+        <xsl:variable name="datapointSubtypeId">
+            <xsl:choose>
+                <xsl:when test="fn:starts-with(@Name, 'DPT')">DPST_<xsl:value-of select="fn:substring-after(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(@Name, '\[', '_'), '\]', ''), '&#x00B3;', '_3'), '&#xB5;', 'y'), '/', ''), '-', '_'), 'DPT_')"/></xsl:when>
+                <xsl:otherwise>DPST_<xsl:for-each select="tokenize(@Name, ' ')"><xsl:value-of select="concat(upper-case(substring(.,1,1)), substring(., 2))"/><xsl:if test="position()!=last()">_</xsl:if></xsl:for-each></xsl:otherwise>
+            </xsl:choose>
+        </xsl:variable>
+        <xsl:variable name="datapointTypeId">
+            <xsl:call-template name="getDatapointTypeId">
+                <xsl:with-param name="contextNode" select="../.."/>
+            </xsl:call-template>
+        </xsl:variable>['<xsl:value-of select="$datapointSubtypeValue"/>' <xsl:value-of select="$datapointSubtypeId"/> ['KnxDatapointType.<xsl:value-of select="$datapointTypeId"/>', '"<xsl:value-of select="@Text"/>"']]
     </xsl:template>
 
     <xsl:template match="knx:Manufacturer">
         <xsl:variable name="manufacturerId">
-            <xsl:call-template name="clean-id-string">
-                <xsl:with-param name="text">
-                    <xsl:call-template name="string-replace-all">
-                        <xsl:with-param name="text" select="@Id"/>
-                        <xsl:with-param name="replace" select="'-'"/>
-                        <xsl:with-param name="with" select="'_'"/>
+            <xsl:choose>
+                <xsl:when test="@Name = 'Simon'">SIMON_<xsl:value-of select="@KnxManufacturerId"/></xsl:when>
+                <xsl:when test="@Name = 'Not Assigned'">NOT_ASSIGNED_<xsl:value-of select="@KnxManufacturerId"/></xsl:when>
+                <xsl:otherwise>
+                    <xsl:call-template name="getIdFromText">
+                        <xsl:with-param name="text" select="@Name"/>
                     </xsl:call-template>
-                </xsl:with-param>
-            </xsl:call-template>
-        </xsl:variable>['<xsl:value-of select="@KnxManufacturerId"/>' <xsl:value-of select="$manufacturerId"/> ['"<xsl:value-of select="@Name"/>"']]
+                </xsl:otherwise>
+            </xsl:choose>
+        </xsl:variable>['<xsl:value-of select="@KnxManufacturerId"/>' <xsl:value-of select="$manufacturerId"/> ['"<xsl:value-of select="fn:replace(normalize-space(@Name),'&#xa0;', ' ')"/>"']]
     </xsl:template>
 
-    <xsl:template name="clean-id-string">
-        <xsl:param name="text"/>
-        <xsl:call-template name="string-replace-all">
-            <xsl:with-param name="text" select="$text"/>
-            <xsl:with-param name="replace" select="'%'"/>
-            <xsl:with-param name="with" select="'PERCENT'"/>
-        </xsl:call-template>
+    <xsl:template name="getDatatypeId">
+        <xsl:param name="contextNode"/>
+        <xsl:value-of select="fn:replace(fn:replace(fn:replace(fn:replace(fn:upper-case($contextNode/@Text), ' ', '_'), '-', '_'), '&amp;', 'AND'), '/', '')"/>
     </xsl:template>
 
-    <xsl:template name="string-replace-all">
-        <xsl:param name="text"/>
-        <xsl:param name="replace"/>
-        <xsl:param name="with"/>
+    <xsl:template name="getDatapointTypeId">
+        <xsl:param name="contextNode"/>
         <xsl:choose>
-            <xsl:when test="$text = '' or $replace = ''or not($replace)">
-                <xsl:value-of select="$text"/>
-            </xsl:when>
-            <xsl:when test="contains($text, $replace)">
-                <xsl:value-of select="substring-before($text,$replace)"/>
-                <xsl:value-of select="$with"/>
-                <xsl:call-template name="string-replace-all">
-                    <xsl:with-param name="text" select="substring-after($text,$replace)"/>
-                    <xsl:with-param name="replace" select="$replace"/>
-                    <xsl:with-param name="with" select="$with"/>
-                </xsl:call-template>
-            </xsl:when>
-            <xsl:otherwise>
-                <xsl:value-of select="$text"/>
-            </xsl:otherwise>
+            <xsl:when test="$contextNode/@Number = 26">DPT_8_BIT_SET_2</xsl:when>
+            <xsl:when test="$contextNode/@Number = 237">DPT_CONFIGURATION_DIAGNOSTICS_16_BIT</xsl:when>
+            <xsl:when test="$contextNode/@Number = 238">DPT_CONFIGURATION_DIAGNOSTICS_8_BIT</xsl:when>
+            <xsl:when test="$contextNode/@Number = 241">DPT_STATUS_32_BIT</xsl:when>
+            <xsl:when test="$contextNode/@Number = 242">DPT_STATUS_48_BIT</xsl:when>
+            <xsl:when test="$contextNode/@Number = 250">DPT_STATUS_24_BIT</xsl:when>
+            <xsl:otherwise>DPT_<xsl:value-of select="fn:replace(fn:replace(fn:replace(fn:replace(fn:upper-case($contextNode/@Text), ' ', '_'), '-', '_'), '&amp;', 'AND'), '/', '')"/></xsl:otherwise>
         </xsl:choose>
+    </xsl:template>
+
+    <xsl:template name="getIdFromText">
+        <xsl:param name="text"/>
+        <xsl:variable name="cleanedText" select="fn:replace(fn:replace(fn:replace(fn:replace(fn:upper-case($text), '/', ''), '\(', ''), '\)', ''), '&#x2013;', '_')"/>
+        <xsl:variable name="cleanedText2" select="fn:replace(fn:replace($cleanedText, '&#x00B3;', '_3'), '&#xC9;', 'E')"/>
+        <xsl:value-of select="fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(fn:replace(normalize-space($cleanedText2),'&#xa0;', '_'), '&amp;', 'AND'), '-', '_'), ' ', '_'), '\.', '_'), ',', '_'), '\+', 'Plus'), '/', '_'), 'Ä', 'AE'), 'Ö', 'OE'), 'Ü', 'UE'), 'ß', 'SS'), ':', '_')"/>
     </xsl:template>
 
 </xsl:stylesheet>
