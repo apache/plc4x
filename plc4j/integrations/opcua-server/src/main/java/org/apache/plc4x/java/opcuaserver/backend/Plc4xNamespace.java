cd425c7a8adf16f7bd3d736628ff6a227fdba92a
hutcheb <ben.hutche@gmail.com>
2021-01-16 06:58:26 -0500

    Updated Notice and License files.
    To take into account for derivative works licenses under EPL 2.0

--- plc4j/integrations/opcua-server/src/main/java/org/apache/plc4x/java/opcuaserver/backend/Plc4xNamespace.java
+++ plc4j/integrations/opcua-server/src/main/java/org/apache/plc4x/java/opcuaserver/backend/Plc4xNamespace.java
@@ -1,20 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Copyright (c) 2019 the Eclipse Milo Authors
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * SPDX-License-Identifier: EPL-2.0
  */
 
 package org.apache.plc4x.java.opcuaserver.backend;
@@ -204,17 +195,13 @@ public class Plc4xNamespace extends ManagedNamespaceWithLifecycle {
 
                 node.setValue(new DataValue(variant));
 
-                AttributeLoggingFilter filter = new AttributeLoggingFilter();
-
                 node.getFilterChain().addLast(
-                    filter,
                     AttributeFilters.getValue(
                         ctx -> plc4xServer.getValue(ctx, tag, connectionString)
                     )
                 );
 
                 node.getFilterChain().addLast(
-                    filter,
                     AttributeFilters.setValue(
                         (ctx, value) -> {
                             if (length > 1) {
