fe70dc71ac75f2caee12a622690b7ef3d701ae13
Christofer Dutz <christofer.dutz@c-ware.de>
2021-02-05 18:55:53 +0100

    - Enabled a full KNX network scan instead of one limited to my address-ranges

--- plc4go/examples/discovery/hello_world_plc4go_knx_discovery.go
+++ plc4go/examples/discovery/hello_world_plc4go_knx_discovery.go
@@ -19,81 +19,81 @@
 package main
 
 import (
-    "github.com/apache/plc4x/plc4go/internal/plc4go/knxnetip"
-    "github.com/apache/plc4x/plc4go/internal/plc4go/spi/transports/udp"
-    "github.com/apache/plc4x/plc4go/pkg/plc4go"
-    "github.com/apache/plc4x/plc4go/pkg/plc4go/model"
-    log "github.com/sirupsen/logrus"
-    "os"
-    "time"
+	"github.com/apache/plc4x/plc4go/internal/plc4go/knxnetip"
+	"github.com/apache/plc4x/plc4go/internal/plc4go/spi/transports/udp"
+	"github.com/apache/plc4x/plc4go/pkg/plc4go"
+	"github.com/apache/plc4x/plc4go/pkg/plc4go/model"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"time"
 )
 
 func main() {
-    //log.SetFormatter(&log.JSONFormatter{})
-    log.SetOutput(os.Stdout)
-    log.SetLevel(log.DebugLevel)
+	//log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
 
-    driverManager := plc4go.NewPlcDriverManager()
-    driverManager.RegisterDriver(knxnetip.NewKnxNetIpDriver())
-    driverManager.RegisterTransport(udp.NewUdpTransport())
+	driverManager := plc4go.NewPlcDriverManager()
+	driverManager.RegisterDriver(knxnetip.NewKnxNetIpDriver())
+	driverManager.RegisterTransport(udp.NewUdpTransport())
 
-    // Try to auto-find KNX gateways via broadcast-message discovery
-    driverManager.Discover(func(event model.PlcDiscoveryEvent) {
-        connStr := event.ProtocolCode + /*":" + event.TransportCode +*/ "://" + event.TransportUrl.Host
-        crc := driverManager.GetConnection(connStr)
+	// Try to auto-find KNX gateways via broadcast-message discovery
+	driverManager.Discover(func(event model.PlcDiscoveryEvent) {
+		connStr := event.ProtocolCode + /*":" + event.TransportCode +*/ "://" + event.TransportUrl.Host
+		crc := driverManager.GetConnection(connStr)
 
-        // Wait for the driver to connect (or not)
-        connectionResult := <-crc
-        if connectionResult.Err != nil {
-            log.Errorf("error connecting to PLC: %s", connectionResult.Err.Error())
-            return
-        }
-        connection := connectionResult.Connection
-        defer connection.Close()
+		// Wait for the driver to connect (or not)
+		connectionResult := <-crc
+		if connectionResult.Err != nil {
+			log.Errorf("error connecting to PLC: %s", connectionResult.Err.Error())
+			return
+		}
+		connection := connectionResult.Connection
+		defer connection.Close()
 
-        // Try to find all KNX devices on the current network
-        browseRequestBuilder := connection.BrowseRequestBuilder()
-        //browseRequestBuilder.AddItem("allDevices", "[1-15].[1-15].[0-255]")
-        browseRequestBuilder.AddItem("allDevices", "[1-3].[1-6].[0-60]")
-        browseRequest, err := browseRequestBuilder.Build()
-        if err != nil {
-            log.Errorf("error creating browse request: %s", err.Error())
-            return
-        }
-        brr := browseRequest.ExecuteWithInterceptor(func(result model.PlcBrowseEvent) bool {
-            knxAddress := result.Result.Address
-            log.Info("Inspecting detected Device at KNX Address: " + knxAddress)
+		// Try to find all KNX devices on the current network
+		browseRequestBuilder := connection.BrowseRequestBuilder()
+		browseRequestBuilder.AddItem("allDevices", "[1-15].[1-15].[0-255]")
+		//browseRequestBuilder.AddItem("allDevices", "[1-3].[1-6].[0-60]")
+		browseRequest, err := browseRequestBuilder.Build()
+		if err != nil {
+			log.Errorf("error creating browse request: %s", err.Error())
+			return
+		}
+		brr := browseRequest.ExecuteWithInterceptor(func(result model.PlcBrowseEvent) bool {
+			knxAddress := result.Result.Address
+			log.Info("Inspecting detected Device at KNX Address: " + knxAddress)
 
-            // Try to get all the com-objects and the group addresses they are attached to.
-            browseRequestBuilder = connection.BrowseRequestBuilder()
-            browseRequestBuilder.AddItem("comObjects", knxAddress+"#com-obj")
-            browseRequest, err := browseRequestBuilder.Build()
-            if err != nil {
-                log.Errorf("error creating read request: %s", err.Error())
-                return false
-            }
-            brr := browseRequest.Execute()
-            browseResult := <-brr
-            if browseResult.Err != nil {
-                log.Errorf("error executing the browse request for com-objects: %s", browseResult.Err.Error())
-                return false
-            }
-            for _, result := range browseResult.Response.GetQueryResults("comObjects") {
-                log.Infof(" - %s", result.Address)
-            }
+			// Try to get all the com-objects and the group addresses they are attached to.
+			browseRequestBuilder = connection.BrowseRequestBuilder()
+			browseRequestBuilder.AddItem("comObjects", knxAddress+"#com-obj")
+			browseRequest, err := browseRequestBuilder.Build()
+			if err != nil {
+				log.Errorf("error creating read request: %s", err.Error())
+				return false
+			}
+			brr := browseRequest.Execute()
+			browseResult := <-brr
+			if browseResult.Err != nil {
+				log.Errorf("error executing the browse request for com-objects: %s", browseResult.Err.Error())
+				return false
+			}
+			for _, result := range browseResult.Response.GetQueryResults("comObjects") {
+				log.Infof(" - %s", result.Address)
+			}
 
-            return true
-        })
-        if brr == nil {
-            log.Errorf("error executing browse request")
-            return
-        }
-        select {
-        case browseRequestResult := <- brr:
-            log.Info(browseRequestResult)
-        }
-        return
-    })
+			return true
+		})
+		if brr == nil {
+			log.Errorf("error executing browse request")
+			return
+		}
+		select {
+		case browseRequestResult := <-brr:
+			log.Info(browseRequestResult)
+		}
+		return
+	})
 
-    time.Sleep(time.Second * 1000000)
+	time.Sleep(time.Second * 1000000)
 }
