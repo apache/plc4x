49337d9140e2b794f2708e94d77124277b8b2f88
Sebastian Rühl <sruehl@apache.org>
2021-03-31 12:01:47 +0200

    plc4go: ads first draft of symbolic addressing implemented + Test currently doesn't run due to issues on xml (un)marshalling

--- plc4go/cmd/main/drivers/tests/ads_driver_test.go
+++ plc4go/cmd/main/drivers/tests/ads_driver_test.go
@@ -30,7 +30,7 @@ func TestAdsDriver(t *testing.T) {
 	testutils.RunDriverTestsuite(t, ads.NewDriver(), "assets/testing/protocols/ads/DriverTestsuite.xml",
 		// TODO: tests assumes proper multi requests which is currently not implemented yet
 		"Multi-element direct read request",
-		// TODO: symbolic addressing not implemented yet
+		// TODO: xml marshaller/unmarshaler produces empty elements which break the tests
 		"Single element symbolic read request",
 		// TODO: symbolic addressing not implemented yet
 		"Single element symbolic read request (Address previously resolved)",
