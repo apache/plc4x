1f55c4eac07532f509b51f3baf069724f5b6451c
Sebastian Rühl <sruehl@apache.org>
2021-10-08 15:29:17 +0200

    fix(plc4j/spi): implement reserved field

--- plc4j/spi/src/main/java/org/apache/plc4x/java/spi/codegen/fields/FieldReaderReserved.java
+++ plc4j/spi/src/main/java/org/apache/plc4x/java/spi/codegen/fields/FieldReaderReserved.java
@@ -19,10 +19,13 @@
 package org.apache.plc4x.java.spi.codegen.fields;
 
 import org.apache.plc4x.java.spi.codegen.io.DataReader;
+import org.apache.plc4x.java.spi.generation.ParseAssertException;
 import org.apache.plc4x.java.spi.generation.ParseException;
 import org.apache.plc4x.java.spi.generation.WithReaderArgs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public class FieldReaderReserved<T> implements FieldReader<T> {
 
@@ -30,7 +33,15 @@ public class FieldReaderReserved<T> implements FieldReader<T> {
 
     @Override
     public T readField(String logicalName, DataReader<T> dataReader, WithReaderArgs... readerArgs) throws ParseException {
-        return switchByteOrderIfNecessary(() -> dataReader.read(logicalName, readerArgs), dataReader, extractByteOder(readerArgs).orElse(null));
+        throw new IllegalStateException("not possible with reserved field");
+    }
+
+    public T readAssertField(String logicalName, DataReader<T> dataReader, T referenceValue, WithReaderArgs... readerArgs) throws ParseException {
+        T reserved = dataReader.read(logicalName, readerArgs);
+        if (!Objects.equals(reserved, referenceValue)) {
+            LOGGER.info("Expected constant value {} but got {} for reserved field.", referenceValue, reserved);
+        }
+        return reserved;
     }
 
 }
