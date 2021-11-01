f7df8d1bc1f6f872841d08928f172e0edd528f0b
Sebastian Rühl <sruehl@apache.org>
2021-11-01 13:26:47 +0100

    fix(plc4go/codgen): fix merge messup

--- plc4go/internal/plc4go/s7/readwrite/model/COTPParameter.go
+++ plc4go/internal/plc4go/s7/readwrite/model/COTPParameter.go
@@ -126,7 +126,7 @@ func COTPParameterParse(readBuffer utils.ReadBuffer, rest uint8) (*COTPParameter
 	case parameterType == 0xC3: // COTPParameterChecksum
 		_parent, typeSwitchError = COTPParameterChecksumParse(readBuffer, rest)
 	case parameterType == 0xE0: // COTPParameterDisconnectAdditionalInformation
-		_parent, typeSwitchError = COTPParameterDisconnectAdditionalInformationParse(readBuffer)
+		_parent, typeSwitchError = COTPParameterDisconnectAdditionalInformationParse(readBuffer, rest)
 	default:
 		// TODO: return actual type
 		typeSwitchError = errors.New("Unmapped type")
