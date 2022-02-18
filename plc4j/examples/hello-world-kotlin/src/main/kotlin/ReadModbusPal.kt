2d3fd7c1e4e4c0d276dc9510a3b79df75bed2ee1
Sebastian Rühl <sruehl@apache.org>
2022-02-18 10:39:28 +0100

    refactor: slightly adjust kotlin example

--- plc4j/examples/hello-world-kotlin/src/main/kotlin/ReadModbusPal.kt
+++ plc4j/examples/hello-world-kotlin/src/main/kotlin/ReadModbusPal.kt
@@ -1,38 +1,42 @@
 import org.apache.plc4x.java.PlcDriverManager
 import org.apache.plc4x.java.api.types.PlcResponseCode
+import java.util.concurrent.TimeUnit
 import kotlin.system.exitProcess
 
 fun main() {
-    PlcDriverManager().getConnection("modbus://localhost:502").use { conn ->
-        if (!conn.metadata.canRead()) {
-            println("Cannot read!!")
-            return
-        }
+    PlcDriverManager()
+        .getConnection("modbus://localhost:502")
+        .use { conn ->
+            if (!conn.metadata.canRead()) {
+                println("Cannot read!!")
+                return
+            }
 
-        val builder = conn.readRequestBuilder()
-        builder.addItem("value-1", "coil:1")
-        builder.addItem("value-2", "coil:3[4]")
-        builder.addItem("value-3", "holding-register:1")
-        builder.addItem("value-4", "holding-register:3[4]")
-        val readRequest = builder.build()
+            val readRequest = conn.readRequestBuilder()
+                .addItem("value-1", "coil:1")
+                .addItem("value-2", "coil:3[4]")
+                .addItem("value-3", "holding-register:1")
+                .addItem("value-4", "holding-register:3[4]")
+                .build()
 
-        val response = readRequest.execute().get()
-        for (fieldName in response.fieldNames) {
-            if (response.getResponseCode(fieldName) === PlcResponseCode.OK) {
+            val response = readRequest.execute().get(1, TimeUnit.MINUTES)
+            response.fieldNames.forEach { fieldName ->
+                if (response.getResponseCode(fieldName) !== PlcResponseCode.OK) {
+                    println("Error[$fieldName]: $response.getResponseCode(fieldName).name")
+                    return
+                }
                 val numValues = response.getNumberOfValues(fieldName)
                 // If it's just one element, output just one single line.
                 if (numValues == 1) {
-                    println("Value[" + fieldName + "]: " + response.getObject(fieldName))
+                    println("Value[$fieldName]: response.getObject(fieldName)")
                 } else {
                     println("Value[$fieldName]:")
                     for (i in 0 until numValues) {
                         println(" - " + response.getObject(fieldName, i))
                     }
                 }
-            } else {
-                println("Error[" + fieldName + "]: " + response.getResponseCode(fieldName).name)
             }
         }
-    }
+
     exitProcess(0)
 }
