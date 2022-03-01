dc5ceda7f6a4007d4d6d819d2b38f229d809bf40
Sebastian Rühl <sruehl@apache.org>
2022-03-01 12:07:39 +0100

    test(protocols/c-bus): move tests to nested test

--- plc4j/drivers/c-bus/src/test/java/org/apache/plc4x/java/cbus/RandomPackagesTest.java
+++ plc4j/drivers/c-bus/src/test/java/org/apache/plc4x/java/cbus/RandomPackagesTest.java
@@ -22,6 +22,7 @@ import org.apache.commons.codec.binary.Hex;
 import org.apache.plc4x.java.cbus.readwrite.CBusCommand;
 import org.apache.plc4x.java.spi.generation.ReadBufferByteBased;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,85 +32,88 @@ public class RandomPackagesTest {
 
     static final String BACKSLASH = "5C";
 
-    // 4.2.9.1
-    @Test
-    void pointToPointCommand1() throws Exception {
-        byte[] bytes = Hex.decodeHex(BACKSLASH + "0603002102D4");
-        ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
-        CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
-        assertThat(cBusCommand)
-            .isNotNull();
-    }
+    // from: https://updates.clipsal.com/ClipsalSoftwareDownload/DL/downloads/OpenCBus/Serial%20Interface%20User%20Guide.pdf
+    @Nested
+    class ReferenceDocumentationTest {
+        // 4.2.9.1
+        @Test
+        void pointToPointCommand1() throws Exception {
+            byte[] bytes = Hex.decodeHex(BACKSLASH + "0603002102D4");
+            ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
+            CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
+            assertThat(cBusCommand)
+                .isNotNull();
+        }
 
-    // 4.2.9.1
-    @Test
-    void pointToPointCommand2() throws Exception {
-        byte[] bytes = Hex.decodeHex(BACKSLASH + "06420903210289");
-        ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
-        CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
-        assertThat(cBusCommand)
-            .isNotNull();
-    }
+        // 4.2.9.1
+        @Test
+        void pointToPointCommand2() throws Exception {
+            byte[] bytes = Hex.decodeHex(BACKSLASH + "06420903210289");
+            ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
+            CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
+            assertThat(cBusCommand)
+                .isNotNull();
+        }
 
-    // 4.2.9.2
-    @Test
-    void pointToMultiPointCommand1() throws Exception {
-        byte[] bytes = Hex.decodeHex(BACKSLASH + "0538000108BA");
-        ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
-        CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
-        assertThat(cBusCommand)
-            .isNotNull();
-    }
+        // 4.2.9.2
+        @Test
+        void pointToMultiPointCommand1() throws Exception {
+            byte[] bytes = Hex.decodeHex(BACKSLASH + "0538000108BA");
+            ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
+            CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
+            assertThat(cBusCommand)
+                .isNotNull();
+        }
 
-    // 4.2.9.2
-    @Test
-    void pointToMultiPointCommand2() throws Exception {
-        byte[] bytes = Hex.decodeHex(BACKSLASH + "05FF007A38004A");
-        ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
-        CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
-        assertThat(cBusCommand)
-            .isNotNull();
-    }
+        // 4.2.9.2
+        @Test
+        void pointToMultiPointCommand2() throws Exception {
+            byte[] bytes = Hex.decodeHex(BACKSLASH + "05FF007A38004A");
+            ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
+            CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
+            assertThat(cBusCommand)
+                .isNotNull();
+        }
 
-    // 4.2.9.3
-    @Test
-    void pointToPointToMultiPointCommand2() throws Exception {
-        byte[] bytes = Hex.decodeHex(BACKSLASH + "03420938010871");
-        ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
-        CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
-        assertThat(cBusCommand)
-            .isNotNull();
-    }
+        // 4.2.9.3
+        @Test
+        void pointToPointToMultiPointCommand2() throws Exception {
+            byte[] bytes = Hex.decodeHex(BACKSLASH + "03420938010871");
+            ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
+            CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
+            assertThat(cBusCommand)
+                .isNotNull();
+        }
 
-    // 4.3.3.1
-    @Test
-    void calReply1() throws Exception {
-        byte[] bytes = Hex.decodeHex(BACKSLASH + "0605002102");
-        ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
-        CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
-        assertThat(cBusCommand)
-            .isNotNull();
-    }
+        // 4.3.3.1
+        @Test
+        void calReply1() throws Exception {
+            byte[] bytes = Hex.decodeHex(BACKSLASH + "0605002102");
+            ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
+            CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
+            assertThat(cBusCommand)
+                .isNotNull();
+        }
 
-    // 4.3.3.1
-    @Test
-    void calReply2() throws Exception {
-        byte[] bytes = Hex.decodeHex(BACKSLASH + "86059300890231E22E363620207F");
-        ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
-        CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
-        assertThat(cBusCommand)
-            .isNotNull();
-    }
+        // 4.3.3.1
+        @Test
+        void calReply2() throws Exception {
+            byte[] bytes = Hex.decodeHex(BACKSLASH + "86059300890231E22E363620207F");
+            ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
+            CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
+            assertThat(cBusCommand)
+                .isNotNull();
+        }
 
-    // 4.3.3.2
-    @Test
-    void monitoredSal() throws Exception {
-        byte[] bytes = Hex.decodeHex(BACKSLASH + "0503380079083F");
-        ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
-        CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
-        assertThat(cBusCommand)
-            .isNotNull();
+        // 4.3.3.2
+        @Test
+        void monitoredSal() throws Exception {
+            byte[] bytes = Hex.decodeHex(BACKSLASH + "0503380079083F");
+            ReadBufferByteBased readBufferByteBased = new ReadBufferByteBased(bytes);
+            CBusCommand cBusCommand = CBusCommand.staticParse(readBufferByteBased, false);
+            assertThat(cBusCommand)
+                .isNotNull();
+        }
     }
-
 
 }
