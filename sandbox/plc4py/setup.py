f610ef782088284f9c41955e746454fe168ea8bb
Ben Hutcheson <ben.hutche@gmail.com>
2022-04-22 05:15:23 +1000

    Updated plc4py/pom.xml to use pipenv

--- sandbox/plc4py/setup.py
+++ sandbox/plc4py/setup.py
@@ -1,3 +1,22 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
 from setuptools import setup, find_packages
 
 setup(
@@ -20,11 +39,20 @@ setup(
     install_requires=[
         "pytest-asyncio>=0.18.3",
         "pip-tools",
-        "requires",
-        "pre-commit>=2.6.0",
-        "pytest-mock>=3.3.1",
-        "mock>=4.0.2",
-        "mypy>=0.942",
-        "flake8>=4.0.1",
     ],
+    extras_require={
+        "dev": [
+            "requires",
+            "pre-commit>=2.6.0",
+            "pytest-mock>=3.3.1",
+            "mock>=4.0.2",
+            "mypy>=0.942",
+            "flake8>=4.0.1",
+        ]
+    },
+    entry_points={
+        "plc4py.drivers": [
+            "modbus = plc4py.drivers.modbus.ModbusConnection:ModbusConnectionLoader"
+        ]
+    },
 )
