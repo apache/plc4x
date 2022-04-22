cb134be0caebbc010bed579c315c4665b449c7fe
Ben Hutcheson <ben.hutche@gmail.com>
2022-04-23 05:50:39 +1000

    Updated version numbe to 0.10a0. PEP440

--- sandbox/plc4py/setup.py
+++ sandbox/plc4py/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="plc4py",
-    version="0.1.0",
+    version="0.10a0",
     description="Plc4py The Python Industrial IOT Adapter",
     classifiers=[
         "Development Status :: 3 - Alpha",
