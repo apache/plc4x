93a1cff1b3ea6367652a2eedc04ecf922434a78c
Sebastian Rühl <sruehl@apache.org>
2022-04-28 15:10:18 +0200

    fix: fix build by switching to right test version

--- plc4j/drivers/bacnet/src/test/java/org/apache/plc4x/java/bacnetip/TrickyPackagesTest.java
+++ plc4j/drivers/bacnet/src/test/java/org/apache/plc4x/java/bacnetip/TrickyPackagesTest.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.stream.IntStream;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TrickyPackagesTest {
 
