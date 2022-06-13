446d74a45ece98a48a5f759b0436f4bdb4cf4540
cdutz <christofer.dutz@c-ware.de>
2022-06-13 15:50:06 +0200

    chore(plc4j): Updated the tests to work with real manually validated byte values instead of relying on the roundtrip.

--- plc4j/spi/src/test/java/org/apache/plc4x/java/spi/codegen/io/VintIoTest.java
+++ plc4j/spi/src/test/java/org/apache/plc4x/java/spi/codegen/io/VintIoTest.java
@@ -23,41 +23,43 @@ import org.apache.plc4x.java.spi.generation.ReadBufferByteBased;
 import org.apache.plc4x.java.spi.generation.WriteBufferByteBased;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 public class VintIoTest {
 
-    @ParameterizedTest
-    @ValueSource(longs = {0, 1, 23, 42, 126, 127})
-    void oneByteTest(long value) throws  Exception {
-        roundtrip(value, 1);
+    private static Stream<Arguments> inputData() {
+        return Stream.of(
+            Arguments.of(0,      new byte[]{0x00}),
+            Arguments.of(1,      new byte[]{0x01}),
+            Arguments.of(23,     new byte[]{0x17}),
+            Arguments.of(42,     new byte[]{0x2A}),
+            Arguments.of(126,    new byte[]{0x7E}),
+            Arguments.of(127,    new byte[]{0x7F}),
+            Arguments.of(128,    new byte[]{(byte) 0x81, 0x00}),
+            Arguments.of(1234,   new byte[]{(byte) 0x89, 0x52}),
+            Arguments.of(12345,  new byte[]{(byte) 0xE0, 0x39}),
+            Arguments.of(123456, new byte[]{(byte) 0x87, (byte) 0xC4, 0x40})
+        );
     }
 
     @ParameterizedTest
-    @ValueSource(longs = {128, 1234, 12345})
-    void twoByteTest(long value) throws  Exception {
-        roundtrip(value, 2);
-    }
-
-    @ParameterizedTest
-    @ValueSource(longs = {123456})
-    void threeByteTest(long value) throws  Exception {
-        roundtrip(value, 3);
-    }
-
-    private void roundtrip(long value, int expectedNumBytes) throws  Exception {
-        Assertions.assertEquals(expectedNumBytes, VintIo.getLengthInBytes(value));
+    @MethodSource("inputData")
+    void oneByteTest(long value, byte[] expectedBytes) throws  Exception {
+        Assertions.assertEquals(expectedBytes.length, VintIo.getLengthInBytes(value));
         WriteBufferByteBased writeBuffer = new WriteBufferByteBased(10);
         VintIo.serialize(value, writeBuffer);
-        Assertions.assertEquals(expectedNumBytes, writeBuffer.getPos());
-        ReadBuffer readBuffer = new ReadBufferByteBased(writeBuffer.getBytes());
-        Assertions.assertEquals(value, VintIo.parse(readBuffer));
-        // For the first bytes the top most bit must be set to 1.
-        for(int i = 0; i < (expectedNumBytes - 1); i++) {
-            Assertions.assertEquals(0x80, writeBuffer.getBytes()[i] & 0x80);
+        final byte[] serializedBytes = writeBuffer.getBytes();
+        Assertions.assertEquals(expectedBytes.length, serializedBytes.length);
+        // Compare the content of the byte buffer
+        for(int i = 0; i < expectedBytes.length; i++) {
+            Assertions.assertEquals(expectedBytes[i], serializedBytes[i]);
         }
-        // For the last byte the top most bit must be set to 0.
-        Assertions.assertEquals(0x00, writeBuffer.getBytes()[expectedNumBytes - 1] & 0x80);
+        ReadBuffer readBuffer = new ReadBufferByteBased(serializedBytes);
+        Assertions.assertEquals(value, VintIo.parse(readBuffer));
     }
 
 }
