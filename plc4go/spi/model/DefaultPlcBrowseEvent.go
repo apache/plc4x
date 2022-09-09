3493b1e95f9dd85d6397a731f7bb60b614028b7e
Sebastian Rühl <sruehl@apache.org>
2022-09-09 18:43:04 +0200

    feat(plc4go): added plc4xgenerator to generate the serialize functions
    + used for api and spi implementation

--- plc4go/spi/model/DefaultPlcBrowseEvent.go
+++ plc4go/spi/model/DefaultPlcBrowseEvent.go
@@ -21,10 +21,9 @@ package model
 
 import (
 	"github.com/apache/plc4x/plc4go/pkg/api/model"
-	"github.com/apache/plc4x/plc4go/spi/utils"
-	"github.com/pkg/errors"
 )
 
+//go:generate go run ../../tools/plc4xgenerator/gen.go -type=DefaultPlcBrowseEvent
 type DefaultPlcBrowseEvent struct {
 	Request   model.PlcBrowseRequest
 	FieldName string
@@ -50,62 +49,4 @@ func (d *DefaultPlcBrowseEvent) GetResult() model.PlcBrowseFoundField {
 
 func (d *DefaultPlcBrowseEvent) GetErr() error {
 	return d.Err
-}
-
-func (d *DefaultPlcBrowseEvent) Serialize(writeBuffer utils.WriteBuffer) error {
-	if err := writeBuffer.PushContext("PlcBrowseEvent"); err != nil {
-		return err
-	}
-
-	if d.Request != nil {
-		if err := writeBuffer.PushContext("Request"); err != nil {
-			return err
-		}
-		if serializableField, ok := d.Request.(utils.Serializable); ok {
-			if err := serializableField.Serialize(writeBuffer); err != nil {
-				return err
-			}
-		} else {
-			return errors.Errorf("Error serializing. Field %T doesn't implement Serializable", d.Request)
-		}
-		if err := writeBuffer.PopContext("Request"); err != nil {
-			return err
-		}
-	}
-	if err := writeBuffer.WriteString("fieldName", uint32(len(d.FieldName)*8), "UTF-8", d.FieldName); err != nil {
-		return err
-	}
-	if d.Result != nil {
-		if err := writeBuffer.PushContext("Result"); err != nil {
-			return err
-		}
-		if serializableField, ok := d.Result.(utils.Serializable); ok {
-			if err := serializableField.Serialize(writeBuffer); err != nil {
-				return err
-			}
-		} else {
-			return errors.Errorf("Error serializing. Field %T doesn't implement Serializable", d.Result)
-		}
-		if err := writeBuffer.PopContext("Result"); err != nil {
-			return err
-		}
-	}
-	if d.Err != nil {
-		if err := writeBuffer.WriteString("err", uint32(len(d.Err.Error())*8), "UTF-8", d.Err.Error()); err != nil {
-			return err
-		}
-	}
-
-	if err := writeBuffer.PopContext("PlcBrowseEvent"); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (d *DefaultPlcBrowseEvent) String() string {
-	writeBuffer := utils.NewWriteBufferBoxBasedWithOptions(true, true)
-	if err := writeBuffer.WriteSerializable(d); err != nil {
-		return err.Error()
-	}
-	return writeBuffer.GetBox().String()
 }
