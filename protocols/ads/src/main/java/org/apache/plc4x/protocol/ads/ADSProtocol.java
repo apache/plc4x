7fb2551e6053dfb43c5897d0b87f9be12bad2675
Ben Hutcheson <ben.hutche@gmail.com>
2023-02-11 06:40:52 +0100

    fix(plc4j/code-gen): remove TODO and add ads discovery mspec to validation checks

--- protocols/ads/src/main/java/org/apache/plc4x/protocol/ads/ADSProtocol.java
+++ protocols/ads/src/main/java/org/apache/plc4x/protocol/ads/ADSProtocol.java
@@ -25,11 +25,15 @@ import org.apache.plc4x.plugins.codegenerator.language.mspec.protocol.Validatabl
 import org.apache.plc4x.plugins.codegenerator.protocol.Protocol;
 import org.apache.plc4x.plugins.codegenerator.protocol.TypeContext;
 import org.apache.plc4x.plugins.codegenerator.types.exceptions.GenerationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ADSProtocol implements Protocol, ProtocolHelpers {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ADSProtocol.class);
 
     @Override
     public String getName() {
@@ -38,7 +42,15 @@ public class ADSProtocol implements Protocol, ProtocolHelpers {
 
     @Override
     public TypeContext getTypeContext() throws GenerationException {
-        ValidatableTypeContext typeContext = new MessageFormatParser().parse(getMspecStream());
+
+        ValidatableTypeContext typeContext;
+
+        LOGGER.info("Parsing: ads.mspec");
+        typeContext = new MessageFormatParser().parse(getMspecStream("ads"));
+
+        LOGGER.info("Parsing: ads-discovery.mspec");
+        typeContext = new MessageFormatParser().parse(getMspecStream("ads-discovery"), typeContext);
+
         typeContext.validate();
         return typeContext;
     }
