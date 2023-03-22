dece48b3b588d0a9d23e91b26a0ecbac311cc2f0
Sebastian Rühl <sruehl@apache.org>
2023-03-22 18:22:38 +0100

    fix(plc4go/plc4xbrowser): handle panics on actions

--- plc4go/tools/plc4xbrowser/ui/commands.go
+++ plc4go/tools/plc4xbrowser/ui/commands.go
@@ -22,6 +22,7 @@ package ui
 import (
 	"fmt"
 	"net/url"
+	"runtime/debug"
 	"strings"
 	"time"
 
@@ -277,6 +278,7 @@ var rootCommand = Command{
 			Name:        "browse-direct",
 			Description: "Builds a browse request with the supplied field",
 			action: func(c Command, connectionsStringAndFieldQuery string) error {
+				panic("asd")
 				split := strings.Split(connectionsStringAndFieldQuery, " ")
 				if len(split) != 2 {
 					return errors.Errorf("%s expects exactly three arguments [connection url] [fieldQuery]", c)
@@ -742,7 +744,15 @@ func Execute(commandText string) error {
 	return err
 }
 
-func (c Command) Execute(commandText string) error {
+func (c Command) Execute(commandText string) (err error) {
+	defer func() {
+		if recoveredErr := recover(); recoveredErr != nil {
+			if log.Debug().Enabled() {
+				log.Debug().Msgf("Panic '%v' stack:\n%s", recoveredErr, debug.Stack())
+			}
+			err = errors.Errorf("panic occurred: %v.", recoveredErr)
+		}
+	}()
 	plc4xBrowserLog.Debug().Msgf("%s executes %s", c, commandText)
 	if !c.acceptsCurrentText(commandText) {
 		return errors.Errorf("%s doesn't understand %s", c.Name, commandText)
