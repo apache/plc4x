874e2b9565945d0a01bfbeeb3717df5d2ccfb43c
Sebastian Rühl <sruehl@apache.org>
2023-03-24 16:57:33 +0100

    refactor(plc4go/cbus): move map encoded reply to message mapper

--- plc4go/internal/cbus/Reader.go
+++ plc4go/internal/cbus/Reader.go
@@ -21,8 +21,6 @@ package cbus
 
 import (
 	"context"
-	"fmt"
-	"strconv"
 	"sync"
 	"time"
 
@@ -31,8 +29,6 @@ import (
 	readWriteModel "github.com/apache/plc4x/plc4go/protocols/cbus/readwrite/model"
 	"github.com/apache/plc4x/plc4go/spi"
 	spiModel "github.com/apache/plc4x/plc4go/spi/model"
-	spiValues "github.com/apache/plc4x/plc4go/spi/values"
-
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 )
@@ -184,7 +180,7 @@ func (m *Reader) readSync(ctx context.Context, readRequest apiModel.PlcReadReque
 				log.Trace().Msg("Handling confirmed data")
 				// TODO: check if we can use a plcValueSerializer
 				encodedReply := embeddedReply.GetReply().(readWriteModel.ReplyEncodedReply).GetEncodedReply()
-				if err := m.mapEncodedReply(transaction, encodedReply, tagNameCopy, addResponseCode, addPlcValue); err != nil {
+				if err := MapEncodedReply(transaction, encodedReply, tagNameCopy, addResponseCode, addPlcValue); err != nil {
 					return errors.Wrap(err, "error encoding reply")
 				}
 				return transaction.EndRequest()
@@ -208,218 +204,4 @@ func (m *Reader) readSync(ctx context.Context, readRequest apiModel.PlcReadReque
 		Request:  readRequest,
 		Response: readResponse,
 	}
-}
-
-func (m *Reader) mapEncodedReply(transaction *spi.RequestTransaction, encodedReply readWriteModel.EncodedReply, tagName string, addResponseCode func(name string, responseCode apiModel.PlcResponseCode), addPlcValue func(name string, plcValue apiValues.PlcValue)) error {
-	switch reply := encodedReply.(type) {
-	case readWriteModel.EncodedReplyCALReplyExactly:
-		calData := reply.GetCalReply().GetCalData()
-		addResponseCode(tagName, apiModel.PlcResponseCode_OK)
-		switch calData := calData.(type) {
-		case readWriteModel.CALDataStatusExactly:
-			application := calData.GetApplication()
-			// TODO: verify application... this should be the same
-			_ = application
-			blockStart := calData.GetBlockStart()
-			// TODO: verify application... this should be the same
-			_ = blockStart
-			statusBytes := calData.GetStatusBytes()
-			addResponseCode(tagName, apiModel.PlcResponseCode_OK)
-			plcListValues := make([]apiValues.PlcValue, len(statusBytes)*4)
-			for i, statusByte := range statusBytes {
-				plcListValues[i*4+0] = spiValues.NewPlcSTRING(statusByte.GetGav0().String())
-				plcListValues[i*4+1] = spiValues.NewPlcSTRING(statusByte.GetGav1().String())
-				plcListValues[i*4+2] = spiValues.NewPlcSTRING(statusByte.GetGav2().String())
-				plcListValues[i*4+3] = spiValues.NewPlcSTRING(statusByte.GetGav3().String())
-			}
-			addPlcValue(tagName, spiValues.NewPlcStruct(map[string]apiValues.PlcValue{
-				"application": spiValues.NewPlcSTRING(application.PLC4XEnumName()),
-				"blockStart":  spiValues.NewPlcBYTE(blockStart),
-				"values":      spiValues.NewPlcList(plcListValues),
-			}))
-		case readWriteModel.CALDataStatusExtendedExactly:
-			coding := calData.GetCoding()
-			// TODO: verify coding... this should be the same
-			_ = coding
-			application := calData.GetApplication()
-			// TODO: verify application... this should be the same
-			_ = application
-			blockStart := calData.GetBlockStart()
-			// TODO: verify application... this should be the same
-			_ = blockStart
-			switch coding {
-			case readWriteModel.StatusCoding_BINARY_BY_THIS_SERIAL_INTERFACE:
-				fallthrough
-			case readWriteModel.StatusCoding_BINARY_BY_ELSEWHERE:
-				statusBytes := calData.GetStatusBytes()
-				addResponseCode(tagName, apiModel.PlcResponseCode_OK)
-				plcListValues := make([]apiValues.PlcValue, len(statusBytes)*4)
-				for i, statusByte := range statusBytes {
-					plcListValues[i*4+0] = spiValues.NewPlcSTRING(statusByte.GetGav0().String())
-					plcListValues[i*4+1] = spiValues.NewPlcSTRING(statusByte.GetGav1().String())
-					plcListValues[i*4+2] = spiValues.NewPlcSTRING(statusByte.GetGav2().String())
-					plcListValues[i*4+3] = spiValues.NewPlcSTRING(statusByte.GetGav3().String())
-				}
-				addPlcValue(tagName, spiValues.NewPlcStruct(map[string]apiValues.PlcValue{
-					"application": spiValues.NewPlcSTRING(application.PLC4XEnumName()),
-					"blockStart":  spiValues.NewPlcBYTE(blockStart),
-					"values":      spiValues.NewPlcList(plcListValues),
-				}))
-			case readWriteModel.StatusCoding_LEVEL_BY_THIS_SERIAL_INTERFACE:
-				fallthrough
-			case readWriteModel.StatusCoding_LEVEL_BY_ELSEWHERE:
-				levelInformation := calData.GetLevelInformation()
-				addResponseCode(tagName, apiModel.PlcResponseCode_OK)
-				plcListValues := make([]apiValues.PlcValue, len(levelInformation))
-				for i, levelInformation := range levelInformation {
-					switch levelInformation := levelInformation.(type) {
-					case readWriteModel.LevelInformationAbsentExactly:
-						plcListValues[i] = spiValues.NewPlcSTRING("is absent")
-					case readWriteModel.LevelInformationCorruptedExactly:
-						plcListValues[i] = spiValues.NewPlcSTRING("corrupted")
-					case readWriteModel.LevelInformationNormalExactly:
-						plcListValues[i] = spiValues.NewPlcUSINT(levelInformation.GetActualLevel())
-					default:
-						return transaction.FailRequest(errors.Errorf("Impossible case %v", levelInformation))
-					}
-				}
-				addPlcValue(tagName, spiValues.NewPlcList(plcListValues))
-			}
-		case readWriteModel.CALDataIdentifyReplyExactly:
-			switch identifyReplyCommand := calData.GetIdentifyReplyCommand().(type) {
-			case readWriteModel.IdentifyReplyCommandCurrentSenseLevelsExactly:
-				addPlcValue(tagName, spiValues.NewPlcRawByteArray(identifyReplyCommand.GetCurrentSenseLevels()))
-			case readWriteModel.IdentifyReplyCommandDelaysExactly:
-				addPlcValue(tagName, spiValues.NewPlcStruct(map[string]apiValues.PlcValue{
-					"reStrikeDelay": spiValues.NewPlcUSINT(identifyReplyCommand.GetReStrikeDelay()),
-					"terminalLevel": spiValues.NewPlcRawByteArray(identifyReplyCommand.GetTerminalLevels()),
-				}))
-			case readWriteModel.IdentifyReplyCommandDSIStatusExactly:
-				addPlcValue(tagName, spiValues.NewPlcStruct(map[string]apiValues.PlcValue{
-					"channelStatus1":          spiValues.NewPlcSTRING(identifyReplyCommand.GetChannelStatus1().String()),
-					"channelStatus2":          spiValues.NewPlcSTRING(identifyReplyCommand.GetChannelStatus2().String()),
-					"channelStatus3":          spiValues.NewPlcSTRING(identifyReplyCommand.GetChannelStatus3().String()),
-					"channelStatus4":          spiValues.NewPlcSTRING(identifyReplyCommand.GetChannelStatus4().String()),
-					"channelStatus5":          spiValues.NewPlcSTRING(identifyReplyCommand.GetChannelStatus5().String()),
-					"channelStatus6":          spiValues.NewPlcSTRING(identifyReplyCommand.GetChannelStatus6().String()),
-					"channelStatus7":          spiValues.NewPlcSTRING(identifyReplyCommand.GetChannelStatus7().String()),
-					"channelStatus8":          spiValues.NewPlcSTRING(identifyReplyCommand.GetChannelStatus8().String()),
-					"unitStatus":              spiValues.NewPlcSTRING(identifyReplyCommand.GetUnitStatus().String()),
-					"dimmingUCRevisionNumber": spiValues.NewPlcUSINT(identifyReplyCommand.GetDimmingUCRevisionNumber()),
-				}))
-			case readWriteModel.IdentifyReplyCommandExtendedDiagnosticSummaryExactly:
-				addPlcValue(tagName, spiValues.NewPlcStruct(map[string]apiValues.PlcValue{
-					"lowApplication":         spiValues.NewPlcSTRING(identifyReplyCommand.GetLowApplication().String()),
-					"highApplication":        spiValues.NewPlcSTRING(identifyReplyCommand.GetHighApplication().String()),
-					"area":                   spiValues.NewPlcUSINT(identifyReplyCommand.GetArea()),
-					"crc":                    spiValues.NewPlcUINT(identifyReplyCommand.GetCrc()),
-					"serialNumber":           spiValues.NewPlcUDINT(identifyReplyCommand.GetSerialNumber()),
-					"networkVoltage":         spiValues.NewPlcUSINT(identifyReplyCommand.GetNetworkVoltage()),
-					"unitInLearnMode":        spiValues.NewPlcBOOL(identifyReplyCommand.GetUnitInLearnMode()),
-					"networkVoltageLow":      spiValues.NewPlcBOOL(identifyReplyCommand.GetNetworkVoltageLow()),
-					"networkVoltageMarginal": spiValues.NewPlcBOOL(identifyReplyCommand.GetNetworkVoltageMarginal()),
-					"enableChecksumAlarm":    spiValues.NewPlcBOOL(identifyReplyCommand.GetEnableChecksumAlarm()),
-					"outputUnit":             spiValues.NewPlcBOOL(identifyReplyCommand.GetOutputUnit()),
-					"installationMMIError":   spiValues.NewPlcBOOL(identifyReplyCommand.GetInstallationMMIError()),
-					"EEWriteError":           spiValues.NewPlcBOOL(identifyReplyCommand.GetEEWriteError()),
-					"EEChecksumError":        spiValues.NewPlcBOOL(identifyReplyCommand.GetEEChecksumError()),
-					"EEDataError":            spiValues.NewPlcBOOL(identifyReplyCommand.GetEEDataError()),
-					"microReset":             spiValues.NewPlcBOOL(identifyReplyCommand.GetMicroReset()),
-					"commsTxError":           spiValues.NewPlcBOOL(identifyReplyCommand.GetCommsTxError()),
-					"internalStackOverflow":  spiValues.NewPlcBOOL(identifyReplyCommand.GetInternalStackOverflow()),
-					"microPowerReset":        spiValues.NewPlcBOOL(identifyReplyCommand.GetMicroPowerReset()),
-				}))
-			case readWriteModel.IdentifyReplyCommandSummaryExactly:
-				addPlcValue(tagName, spiValues.NewPlcStruct(map[string]apiValues.PlcValue{
-					"partName":        spiValues.NewPlcSTRING(identifyReplyCommand.GetPartName()),
-					"unitServiceType": spiValues.NewPlcUSINT(identifyReplyCommand.GetUnitServiceType()),
-					"version":         spiValues.NewPlcSTRING(identifyReplyCommand.GetVersion()),
-				}))
-			case readWriteModel.IdentifyReplyCommandFirmwareVersionExactly:
-				addPlcValue(tagName, spiValues.NewPlcSTRING(identifyReplyCommand.GetFirmwareVersion()))
-			case readWriteModel.IdentifyReplyCommandGAVPhysicalAddressesExactly:
-				addPlcValue(tagName, spiValues.NewPlcRawByteArray(identifyReplyCommand.GetValues()))
-			case readWriteModel.IdentifyReplyCommandGAVValuesCurrentExactly:
-				addPlcValue(tagName, spiValues.NewPlcRawByteArray(identifyReplyCommand.GetValues()))
-			case readWriteModel.IdentifyReplyCommandGAVValuesStoredExactly:
-				addPlcValue(tagName, spiValues.NewPlcRawByteArray(identifyReplyCommand.GetValues()))
-			case readWriteModel.IdentifyReplyCommandLogicalAssignmentExactly:
-				var plcValues []apiValues.PlcValue
-				for _, logicAssigment := range identifyReplyCommand.GetLogicAssigment() {
-					plcValues = append(plcValues, spiValues.NewPlcStruct(map[string]apiValues.PlcValue{
-						"greaterOfOrLogic": spiValues.NewPlcBOOL(logicAssigment.GetGreaterOfOrLogic()),
-						"reStrikeDelay":    spiValues.NewPlcBOOL(logicAssigment.GetReStrikeDelay()),
-						"assignedToGav16":  spiValues.NewPlcBOOL(logicAssigment.GetAssignedToGav16()),
-						"assignedToGav15":  spiValues.NewPlcBOOL(logicAssigment.GetAssignedToGav15()),
-						"assignedToGav14":  spiValues.NewPlcBOOL(logicAssigment.GetAssignedToGav14()),
-						"assignedToGav13":  spiValues.NewPlcBOOL(logicAssigment.GetAssignedToGav13()),
-					}))
-				}
-				addPlcValue(tagName, spiValues.NewPlcList(plcValues))
-			case readWriteModel.IdentifyReplyCommandManufacturerExactly:
-				addPlcValue(tagName, spiValues.NewPlcSTRING(identifyReplyCommand.GetManufacturerName()))
-			case readWriteModel.IdentifyReplyCommandMaximumLevelsExactly:
-				addPlcValue(tagName, spiValues.NewPlcRawByteArray(identifyReplyCommand.GetMaximumLevels()))
-			case readWriteModel.IdentifyReplyCommandMinimumLevelsExactly:
-				addPlcValue(tagName, spiValues.NewPlcRawByteArray(identifyReplyCommand.GetMinimumLevels()))
-			case readWriteModel.IdentifyReplyCommandNetworkTerminalLevelsExactly:
-				addPlcValue(tagName, spiValues.NewPlcRawByteArray(identifyReplyCommand.GetNetworkTerminalLevels()))
-			case readWriteModel.IdentifyReplyCommandNetworkVoltageExactly:
-				volts := identifyReplyCommand.GetVolts()
-				voltsFloat, err := strconv.ParseFloat(volts, 0)
-				if err != nil {
-					addResponseCode(tagName, apiModel.PlcResponseCode_INTERNAL_ERROR)
-					return transaction.FailRequest(errors.Wrap(err, "Error parsing volts"))
-				}
-				voltsDecimalPlace := identifyReplyCommand.GetVoltsDecimalPlace()
-				voltsDecimalPlaceFloat, err := strconv.ParseFloat(voltsDecimalPlace, 0)
-				if err != nil {
-					addResponseCode(tagName, apiModel.PlcResponseCode_INTERNAL_ERROR)
-					return transaction.FailRequest(errors.Wrap(err, "Error parsing volts decimal place"))
-				}
-				voltsFloat += voltsDecimalPlaceFloat / 10
-				addPlcValue(tagName, spiValues.NewPlcLREAL(voltsFloat))
-			case readWriteModel.IdentifyReplyCommandOutputUnitSummaryExactly:
-				unitFlags := identifyReplyCommand.GetUnitFlags()
-				structContent := map[string]apiValues.PlcValue{
-					"unitFlags": spiValues.NewPlcStruct(map[string]apiValues.PlcValue{
-						"assertingNetworkBurden": spiValues.NewPlcBOOL(unitFlags.GetAssertingNetworkBurden()),
-						"restrikeTimingActive":   spiValues.NewPlcBOOL(unitFlags.GetRestrikeTimingActive()),
-						"remoteOFFInputAsserted": spiValues.NewPlcBOOL(unitFlags.GetRemoteOFFInputAsserted()),
-						"remoteONInputAsserted":  spiValues.NewPlcBOOL(unitFlags.GetRemoteONInputAsserted()),
-						"localToggleEnabled":     spiValues.NewPlcBOOL(unitFlags.GetLocalToggleEnabled()),
-						"localToggleActiveState": spiValues.NewPlcBOOL(unitFlags.GetLocalToggleActiveState()),
-						"clockGenerationEnabled": spiValues.NewPlcBOOL(unitFlags.GetClockGenerationEnabled()),
-						"unitGeneratingClock":    spiValues.NewPlcBOOL(unitFlags.GetUnitGeneratingClock()),
-					}),
-					"timeFromLastRecoverOfMainsInSeconds": spiValues.NewPlcUSINT(identifyReplyCommand.GetTimeFromLastRecoverOfMainsInSeconds()),
-				}
-				if gavStoreEnabledByte1 := identifyReplyCommand.GetGavStoreEnabledByte1(); gavStoreEnabledByte1 != nil {
-					structContent["gavStoreEnabledByte1"] = spiValues.NewPlcUSINT(*gavStoreEnabledByte1)
-				}
-				if gavStoreEnabledByte2 := identifyReplyCommand.GetGavStoreEnabledByte2(); gavStoreEnabledByte2 != nil {
-					structContent["gavStoreEnabledByte2"] = spiValues.NewPlcUSINT(*gavStoreEnabledByte2)
-				}
-				addPlcValue(tagName, spiValues.NewPlcStruct(structContent))
-			case readWriteModel.IdentifyReplyCommandTerminalLevelsExactly:
-				addPlcValue(tagName, spiValues.NewPlcRawByteArray(identifyReplyCommand.GetTerminalLevels()))
-			case readWriteModel.IdentifyReplyCommandTypeExactly:
-				addPlcValue(tagName, spiValues.NewPlcSTRING(identifyReplyCommand.GetUnitType()))
-			default:
-				addResponseCode(tagName, apiModel.PlcResponseCode_INVALID_DATA)
-				return transaction.FailRequest(errors.Errorf("Unmapped type %T", identifyReplyCommand))
-			}
-		default:
-			wbpcb := spiValues.NewWriteBufferPlcValueBased()
-			if err := calData.SerializeWithWriteBuffer(context.Background(), wbpcb); err != nil {
-				log.Warn().Err(err).Msgf("Unmapped cal data type %T. Returning raw to string", calData)
-				addPlcValue(tagName, spiValues.NewPlcSTRING(fmt.Sprintf("%s", calData)))
-			} else {
-				addPlcValue(tagName, wbpcb.GetPlcValue())
-			}
-		}
-	default:
-		return transaction.FailRequest(errors.Errorf("All types should be mapped here. Not mapped: %T", reply))
-	}
-	return nil
 }
