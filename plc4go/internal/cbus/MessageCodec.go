60955cc3ce89b534fb6247542740d75bae9e1b98
Sebastian Rühl <sruehl@apache.org>
2023-04-05 11:55:39 +0200

    test(plc4go/cbus): improved test for MessageCodec

--- plc4go/internal/cbus/MessageCodec.go
+++ plc4go/internal/cbus/MessageCodec.go
@@ -63,7 +63,10 @@ func (m *MessageCodec) GetCodec() spi.MessageCodec {
 func (m *MessageCodec) Send(message spi.Message) error {
 	log.Trace().Msg("Sending message")
 	// Cast the message to the correct type of struct
-	cbusMessage := message.(readWriteModel.CBusMessage)
+	cbusMessage, ok := message.(readWriteModel.CBusMessage)
+	if !ok {
+		return errors.Errorf("Invalid message type %T", message)
+	}
 
 	// Set the right request context
 	m.requestContext = CreateRequestContext(cbusMessage)
