4f4712feba947788173c39e6424492176e4b93e4
Sebastian Rühl <sruehl@apache.org>
2023-05-03 16:51:09 +0200

    fix(plc4j/spi): fix error message

--- plc4j/utils/test-utils/src/main/java/org/apache/plc4x/test/driver/internal/ConnectionManager.java
+++ plc4j/utils/test-utils/src/main/java/org/apache/plc4x/test/driver/internal/ConnectionManager.java
@@ -62,7 +62,7 @@ public class ConnectionManager {
         ChannelExposingConnection connection = (ChannelExposingConnection) plcConnection;
         Channel channel = connection.getChannel();
         if (!(channel instanceof Plc4xEmbeddedChannel)) {
-            throw new PlcRuntimeException("Expecting EmbeddedChannel. Actual "+ channel.getClass());
+            throw new PlcRuntimeException("Expecting Plc4xEmbeddedChannel. Actual "+ channel.getClass());
         }
         return (Plc4xEmbeddedChannel) channel;
     }
