ba9a5b8c632a6efb29677bba873fd7350aefe41c
Christofer Dutz <cdutz@apache.org>
2023-12-04 14:33:47 +0100

    chore: Removed an unneeded import.

--- plc4j/spi/src/main/java/org/apache/plc4x/java/spi/Plc4xNettyWrapper.java
+++ plc4j/spi/src/main/java/org/apache/plc4x/java/spi/Plc4xNettyWrapper.java
@@ -18,7 +18,6 @@
  */
 package org.apache.plc4x.java.spi;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
