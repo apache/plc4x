5a22ad1520503681d756fba25d3900a1af322c5c
Christofer Dutz <cdutz@apache.org>
2024-01-20 11:56:58 +0100

    fix: Made the spring boot application runnable from within the IDE. Upgraded derby to avoid errors in the build.

--- plc4j/tools/ui/frontend/frontend/src/generated/plc4j-tools-ui-frontend.ts
+++ plc4j/tools/ui/frontend/frontend/src/generated/plc4j-tools-ui-frontend.ts
@@ -19,7 +19,7 @@
  */
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2024-01-20 11:37:19.
+// Generated using typescript-generator version 3.2.1263 on 2024-01-20 11:48:31.
 
 export interface Device {
     id: number;
