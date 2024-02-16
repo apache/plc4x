8e5f659f54cdadd7e1a4bd12bf43f51f33c3e4ac
Christofer Dutz <cdutz@apache.org>
2024-02-16 08:24:49 +0100

    build: Made the release-prepare operation run on the host and not in docker.

--- tools/release-2-prepare-release.sh
+++ tools/release-2-prepare-release.sh
@@ -31,8 +31,8 @@ RELEASE_VERSION=${PROJECT_VERSION%"-SNAPSHOT"}
 IFS='.' read -ra VERSION_SEGMENTS <<< "$RELEASE_VERSION"
 NEW_VERSION="${VERSION_SEGMENTS[0]}.${VERSION_SEGMENTS[1]}.$((VERSION_SEGMENTS[2] + 1))-SNAPSHOT"
 
-# 1. Do a simple release-prepare command with pushChanges=false (inside the Docker container)
-docker compose run --rm releaser bash /ws/mvnw -e -P with-c,with-dotnet,with-go,with-java,with-python,with-sandbox -Dmaven.repo.local=/ws/out/.repository release:prepare -DautoVersionSubmodules=true -DpushChanges=false -DreleaseVersion="$RELEASE_VERSION" -DdevelopmentVersion="$NEW_VERSION" -Dtag="v$RELEASE_VERSION"
+# 1. Do a simple release-prepare command
+../mvnw -f ../pom.xml -e -P with-c,with-dotnet,with-go,with-java,with-python,with-sandbox -Dmaven.repo.local=../out/.repository release:prepare -DautoVersionSubmodules=true -DreleaseVersion="$RELEASE_VERSION" -DdevelopmentVersion="$NEW_VERSION" -Dtag="v$RELEASE_VERSION"
 if [ $? -ne 0 ]; then
     echo "Got non-0 exit code from docker compose, aborting."
     exit 1
