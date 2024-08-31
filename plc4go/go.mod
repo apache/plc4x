aa5b24ea1fcc18decc9dedb9a4f8878547122e26
Sebastian Rühl <sruehl@apache.org>
2024-08-31 23:05:02 +0200

    fix(plc4go/spi): FieldReaderVirtual uses a conversion lib

--- plc4go/go.mod
+++ plc4go/go.mod
@@ -24,6 +24,7 @@ go 1.23.0
 require (
 	github.com/IBM/netaddr v1.5.0
 	github.com/ajankovic/xdiff v0.0.1
+	github.com/cstockton/go-conv v1.0.0
 	github.com/google/uuid v1.6.0
 	github.com/gopacket/gopacket v1.3.0
 	github.com/icza/bitio v1.1.0
--- plc4go/spi/codegen/fields/FieldReaderVirtual.go
+++ plc4go/spi/codegen/fields/FieldReaderVirtual.go
@@ -21,7 +21,9 @@ package fields
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/cstockton/go-conv"
 	"github.com/rs/zerolog"
 
 	"github.com/apache/plc4x/plc4go/spi/codegen"
@@ -38,15 +40,56 @@ func NewFieldReaderVirtual[T any](logger zerolog.Logger) *FieldReaderVirtual[T] 
 	return &FieldReaderVirtual[T]{log: logger}
 }
 
-func (f *FieldReaderVirtual[T]) ReadVirtualField(ctx context.Context, logicalName string, klass any, valueExpression any, readerArgs ...utils.WithReaderArgs) (T, error) {
+func (f *FieldReaderVirtual[T]) ReadVirtualField(ctx context.Context, logicalName string, klass any, valueExpression any, readerArgs ...utils.WithReaderArgs) (value T, err error) {
 	f.log.Debug().Str("logicalName", logicalName).Msg("reading field")
 	switch klass.(type) {
 	case *bool:
-		return valueExpression.(T), nil
-	case *byte:
-		panic(valueExpression)
-		return valueExpression.(T), nil
-	default:
-		panic("asd")
+		return f.toT(conv.Bool(valueExpression))
+	case *int8:
+		return f.toT(conv.Int8(valueExpression))
+	case *uint8:
+		return f.toT(conv.Uint8(valueExpression))
+	case *int16:
+		return f.toT(conv.Int16(valueExpression))
+	case *uint16:
+		return f.toT(conv.Uint16(valueExpression))
+	case *int32:
+		return f.toT(conv.Int32(valueExpression))
+	case *uint32:
+		return f.toT(conv.Uint32(valueExpression))
+	case *int64:
+		return f.toT(conv.Int64(valueExpression))
+	case *uint64:
+		return f.toT(conv.Uint64(valueExpression))
+	case *int:
+		return f.toT(conv.Int(valueExpression))
+	case *uint:
+		return f.toT(conv.Uint(valueExpression))
+	//case *uintptr:
+	//	return f.toT(conv.Uintptr(valueExpression))
+	case *float32:
+		return f.toT(conv.Float32(valueExpression))
+	case *float64:
+		return f.toT(conv.Float64(valueExpression))
+	//case *complex64:
+	//	return f.toT(conv.Complex64(valueExpression))
+	//case *complex128:
+	//	return f.toT(conv.Complex128(valueExpression))
+	case *string:
+		return any(fmt.Sprintf("%v", valueExpression)).(T), nil
 	}
+	return valueExpression.(T), nil
+}
+
+// EnsureType is used to convert from one (any,error) to (T, error)
+func (f *FieldReaderVirtual[T]) toT(r any, err error) (T, error) {
+	if err != nil {
+		var zero T
+		return zero, err
+	}
+	var zero T
+	if r == nil {
+		return zero, nil
+	}
+	return r.(T), nil
 }
